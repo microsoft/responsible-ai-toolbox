22fb436a22eb2a475a0e825076052e47104c463a
Ke Xu <xuke@microsoft.com>
2020-08-03 15:06:14 -0700

    comment out unused function
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- libs/interpret/src/lib/MLIDashboard/Controls/DatasetExplorerTab/DatasetExplorerTab.tsx
+++ libs/interpret/src/lib/MLIDashboard/Controls/DatasetExplorerTab/DatasetExplorerTab.tsx
@@ -758,38 +758,38 @@ export class DatasetExplorerTab extends React.PureComponent<IDatasetExplorerTabP
         };
     }
 
-    private scatterSelection = (guid: string, selections: string[], plotlyProps: IPlotlyProperty): void => {
-        const selectedPoints =
-            selections.length === 0
-                ? null
-                : plotlyProps.data.map(trace => {
-                      const selectedIndexes: number[] = [];
-                      if ((trace as any).customdata) {
-                          ((trace as any).customdata as any[]).forEach((dict, index) => {
-                              if (selections.indexOf(dict[JointDataset.IndexLabel]) !== -1) {
-                                  selectedIndexes.push(index);
-                              }
-                          });
-                      }
-                      return selectedIndexes;
-                  });
-        Plotly.restyle(guid, "selectedpoints" as any, selectedPoints as any);
-        const newLineWidths =
-            selections.length === 0
-                ? [0]
-                : plotlyProps.data.map(trace => {
-                      if ((trace as any).customdata) {
-                          const customData = (trace as any).customdata as string[];
-                          const newWidths: number[] = new Array(customData.length).fill(0);
-                          customData.forEach((id, index) => {
-                              if (selections.indexOf(id) !== -1) {
-                                  newWidths[index] = 2;
-                              }
-                          });
-                          return newWidths;
-                      }
-                      return [0];
-                  });
-        Plotly.restyle(guid, "marker.line.width" as any, newLineWidths as any);
-    };
+    // private scatterSelection = (guid: string, selections: string[], plotlyProps: IPlotlyProperty): void => {
+    //     const selectedPoints =
+    //         selections.length === 0
+    //             ? null
+    //             : plotlyProps.data.map(trace => {
+    //                   const selectedIndexes: number[] = [];
+    //                   if ((trace as any).customdata) {
+    //                       ((trace as any).customdata as any[]).forEach((dict, index) => {
+    //                           if (selections.indexOf(dict[JointDataset.IndexLabel]) !== -1) {
+    //                               selectedIndexes.push(index);
+    //                           }
+    //                       });
+    //                   }
+    //                   return selectedIndexes;
+    //               });
+    //     Plotly.restyle(guid, "selectedpoints" as any, selectedPoints as any);
+    //     const newLineWidths =
+    //         selections.length === 0
+    //             ? [0]
+    //             : plotlyProps.data.map(trace => {
+    //                   if ((trace as any).customdata) {
+    //                       const customData = (trace as any).customdata as string[];
+    //                       const newWidths: number[] = new Array(customData.length).fill(0);
+    //                       customData.forEach((id, index) => {
+    //                           if (selections.indexOf(id) !== -1) {
+    //                               newWidths[index] = 2;
+    //                           }
+    //                       });
+    //                       return newWidths;
+    //                   }
+    //                   return [0];
+    //               });
+    //     Plotly.restyle(guid, "marker.line.width" as any, newLineWidths as any);
+    // };
 }
