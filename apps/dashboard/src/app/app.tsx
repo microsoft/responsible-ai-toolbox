cbd44f165883f2da529688e0f0ce928febbf14eb
Ke Xu <xuke@microsoft.com>
2020-08-02 20:05:41 -0700

    fix yarn start
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- apps/dashboard/src/app/app.tsx
+++ apps/dashboard/src/app/app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { App as Interpret } from "../interpret/App";
 import { App as Fairlearn } from "../fairlearn/App";
-import { Route, Link, BrowserRouter } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 
 export default class App extends React.Component {
     public render(): React.ReactNode {
@@ -10,17 +10,15 @@ export default class App extends React.Component {
                 <div role="navigation">
                     <ul>
                         <li>
-                            <Link to="/fairlearn">Fair Learn</Link>
+                            <Link to="/fairlearn">Fairlearn</Link>
                         </li>
                         <li>
                             <Link to="/interpret">Interpret</Link>
                         </li>
                     </ul>
                 </div>
-                <BrowserRouter>
-                    <Route path="/fairlearn" exact component={Fairlearn} />
-                    <Route path="/interpret" exact component={Interpret} />
-                </BrowserRouter>
+                <Route path="/fairlearn" exact component={Fairlearn} />
+                <Route path="/interpret" exact component={Interpret} />
             </div>
         );
     }
