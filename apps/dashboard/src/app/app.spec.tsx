3073706beb88a94c94ec4449a7ea0138c2e45fe6
Ke Xu <xuke@microsoft.com>
2020-08-10 14:52:12 -0700

    add ut
    Signed-off-by: Ke Xu <xuke@microsoft.com>

---
+++ apps/dashboard/src/app/app.spec.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import App from "./App";
+
+describe("FairnessWizard", () => {
+  it("should render successfully", () => {
+    const tree = shallow(<App />);
+    expect(tree).toMatchSnapshot();
+  });
+});
