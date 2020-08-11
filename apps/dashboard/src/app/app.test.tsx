26ef294c3bb97bd964c65eaf3f2d6bfc42dc506b
Ke Xu <xuke@microsoft.com>
2020-08-10 17:02:04 -0700

    fix imclude
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- apps/dashboard/src/app/app.test.tsx
+++ apps/dashboard/src/app/app.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import { App } from "./App";
+import { App } from "./app";
 
 describe("FairnessWizard", () => {
   it("should render successfully", () => {
