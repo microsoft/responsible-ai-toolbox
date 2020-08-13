1cae79df3b983e946b0b782c25a0f24a2a54b72e
Ke Xu <xuke@microsoft.com>
2020-08-13 08:55:06 -0700

    update violin
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- libs/interpret/src/lib/MLIDashboard/Controls/FeatureImportance/Violin.style.ts
+++ libs/interpret/src/lib/MLIDashboard/Controls/FeatureImportance/Violin.style.ts
@@ -1,55 +1,75 @@
-.aggregate-chart {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  .top-controls {
-    display: flex;
-    padding: 3px 15px;
-    .feature-slider {
-      flex: 1;
-    }
-    .path-selector {
-      margin: 0 5px 0 0;
-    }
-    .selector-label {
-      display: flex;
-      flex-direction: row;
-      span {
-        padding-top: 5px;
-        font-family: "Segoe UI", "Segoe UI Web (West European)", "Segoe UI",
-          -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue",
-          sans-serif;
-      }
-    }
-    .slider-control {
-      flex: 1;
-      padding: 0 4px;
-      .slider-label {
-        display: flex;
-        flex-direction: row;
-        .label-text {
-          line-height: 14px;
-          margin: 7px 0 0 4px;
-          font-size: 14px;
-          font-family: "Segoe UI", "Segoe UI Web (West European)", "Segoe UI",
-            -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue",
-            sans-serif;
-        }
-      }
-    }
-  }
+import {
+  IStyle,
+  mergeStyleSets,
+  IProcessedStyleSet
+} from "office-ui-fabric-react";
+
+export interface IICEPlotStyles {
+  iceWrapper: IStyle;
+  loading: IStyle;
+  featurePicker: IStyle;
+  rangeView: IStyle;
+  parameterSet: IStyle;
+  secondWrapper: IStyle;
+  chartWrapper: IStyle;
 }
 
-.callout-info {
-  display: flex;
-  flex-direction: column;
-  max-width: 300px;
-  padding: 30px;
-  font-family: "Segoe UI", "Segoe UI Web (West European)", "Segoe UI",
-    -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif;
+export const iCEPlotStyles: IProcessedStyleSet<IICEPlotStyles> = mergeStyleSets<
+  IICEPlotStyles
+>({});
 
-  button {
-    max-width: 100px;
-  }
-}
+// .aggregate-chart {
+//   width: 100%,
+//   height: 100%,
+//   display: flex,
+//   flex-direction: column,
+//   .top-controls {
+//     display: flex,
+//     padding: 3px 15px,
+//     .feature-slider {
+//       flex: 1,
+//     }
+//     .path-selector {
+//       margin: 0 5px 0 0,
+//     }
+//     .selector-label {
+//       display: flex,
+//       flex-direction: row,
+//       span {
+//         padding-top: 5px,
+//         font-family: "Segoe UI", "Segoe UI Web (West European)", "Segoe UI",
+//           -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue",
+//           sans-serif,
+//       }
+//     }
+//     .slider-control {
+//       flex: 1,
+//       padding: 0 4px,
+//       .slider-label {
+//         display: flex,
+//         flex-direction: row,
+//         .label-text {
+//           line-height: 14px,
+//           margin: 7px 0 0 4px,
+//           font-size: 14px,
+//           font-family: "Segoe UI", "Segoe UI Web (West European)", "Segoe UI",
+//             -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue",
+//             sans-serif,
+//         }
+//       }
+//     }
+//   }
+// }
+
+// .callout-info {
+//   display: flex,
+//   flex-direction: column,
+//   max-width: 300px,
+//   padding: 30px,
+//   font-family: "Segoe UI", "Segoe UI Web (West European)", "Segoe UI",
+//     -apple-system, BlinkMacSystemFont, Roboto, "Helvetica Neue", sans-serif,
+
+//   button {
+//     max-width: 100px,
+//   }
+// }
