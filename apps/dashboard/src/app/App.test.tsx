ee7e2e5023c1ccb26fc9e47d9862d6d78a73d22d
Ke Xu <xuke@microsoft.com>
2020-08-14 00:51:30 -0700

    add file name lint
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- apps/dashboard/src/app/App.test.tsx
+++ apps/dashboard/src/app/App.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import { App } from "./app";
+import { App } from "./App";
 
 describe("FairnessWizard", () => {
   it("should render successfully", () => {
--- libs/interpret/src/lib/MLIDashboard/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./ExplanationDashboard";
-export * from "./Interfaces";
-export * from "./Controls";
-export * from "./NewExplanationDashboard";
