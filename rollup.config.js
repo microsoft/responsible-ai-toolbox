68788cc4e595353a60d177b98c3045be3ee0e310
Ke Xu <xuke@microsoft.com>
2020-08-20 14:05:06 -0700

    change language to json
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
-const nrwlConfig = require("@nrwl/react/plugins/bundle-rollup"); // require the main @nrwl/react/plugins/webpack configuration function.
+const nrwlConfig = require("@nrwl/react/plugins/bundle-rollup");
+const json = require("@rollup/plugin-json");
 
 module.exports = (config) => {
   config = nrwlConfig(config);
 
   config.context = "window";
-
+  config.plugins.push(json());
   return config;
 };
