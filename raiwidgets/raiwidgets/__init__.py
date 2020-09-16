2abebbd6c9e8d9b6b892abe3fa45612273796d8e
Roman Lutz <rolutz@microsoft.com>
2020-09-16 15:56:04 -0700

    fairlearnboard -> raiwidgets
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

---
+++ raiwidgets/raiwidgets/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Microsoft Corporation and contributors.
+# Licensed under the MIT License.
+
+"""Package for the fairness Dashboard widget."""
+
+from .fairness_dashboard import FairnessDashboard
+
+
+__version__ = "0.1.0-dev"
+
+__all__ = ['FairnessDashboard']
