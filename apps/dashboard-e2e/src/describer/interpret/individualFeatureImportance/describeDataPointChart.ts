372e4f65da03151650f6f33cce9e100b49f4588f
Ke Xu <xuke@microsoft.com>
2020-10-10 01:50:00 -0700

    fix test
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- apps/dashboard-e2e/src/describer/interpret/individualFeatureImportance/describeDataPointChart.ts
+++ apps/dashboard-e2e/src/describer/interpret/individualFeatureImportance/describeDataPointChart.ts
@@ -25,30 +25,26 @@ export function describeDataPointChart(dataShape: IInterpretData): void {
     describe("Scatter chart clickable", () => {
       it("should select none by default", () => {
         cy.get(
-          '#IndividualFeatureContainer  div[class^="legendAndText"] div[class^="clickTarget"]'
+          '#IndividualFeatureContainer div[class^="legendAndText"] div[class^="clickTarget"]'
         ).should("not.exist");
       });
       it("should show message on sub chart", () => {
         const message =
           !dataShape.noLocalImportance && !dataShape.noFeatureImportance
-            ? "Select a point to view feature importance"
+            ? "Select a point to see its local importance"
             : "Provide local feature importances to see how each feature impacts individual predictions.";
         const containerId =
           !dataShape.noLocalImportance && !dataShape.noFeatureImportance
             ? "#noPointSelectedInfo"
             : "#noFeatureImportanceInfo";
-        cy.get(
-          `${containerId}  div[class^="missingParametersPlaceholderSpacer"]`
-        ).should("contain.text", message);
+        cy.get(`${containerId}`).should("contain.text", message);
       });
       it("should select the first point", () => {
         props.chart.clickNthPoint(0);
         cy.get(
           '#IndividualFeatureContainer  div[class^="legendAndText"] div[class^="clickTarget"]'
         ).should("contain.text", "Row");
-        cy.get(
-          '#noPointSelectedInfo  div[class^="missingParametersPlaceholderSpacer"]'
-        ).should("not.exist");
+        cy.get("#noPointSelectedInfo").should("not.exist");
       });
     });
 
