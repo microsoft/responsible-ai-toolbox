feb34f122e050832d03cdf68bcce1c46e2337ef2
Roman Lutz <rolutz@microsoft.com>
2020-10-16 18:01:38 -0700

    Fairness: get existing parity metrics set up properly across all tasks (#108)
    * add all variations of currently used metrics, write descriptions, and add restrictions based on ML task
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * eslintrc ignore parity identifiers in underscore notation
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- libs/fairness/src/lib/util/ParityMetrics.ts
+++ libs/fairness/src/lib/util/ParityMetrics.ts
@@ -17,33 +17,66 @@ export enum ParityModes {
 }
 
 export const parityOptions: { [key: string]: IParityOption } = {
-  recall_score: {
+  demographic_parity_difference: {
     description:
-      localization.Fairness.Metrics.equalOpportunityDifferenceDescription,
-    key: "recall_score",
-    parityMetric: "recall_score",
-    parityMode: ParityModes.Difference,
-    title: localization.Fairness.Metrics.equalOpportunityDifference
-  },
-  selection_rate: {
-    description: localization.Fairness.Metrics.parityDifferenceDescription,
-    key: "selection_rate",
+      localization.Fairness.Metrics.demographicParityDifferenceDescription,
+    key: "demographic_parity_difference",
     parityMetric: "selection_rate",
     parityMode: ParityModes.Difference,
-    title: localization.Fairness.Metrics.parityDifference
+    title: localization.Fairness.Metrics.demographicParityDifference
   },
-  selection_rate_ratio: {
-    description: localization.Fairness.Metrics.parityRatioDescription,
-    key: "selection_rate_ratio",
+  demographic_parity_ratio: {
+    description:
+      localization.Fairness.Metrics.demographicParityRatioDescription,
+    key: "demographic_parity_ratio",
     parityMetric: "selection_rate",
     parityMode: ParityModes.Ratio,
-    title: localization.Fairness.Metrics.parityRatio
+    title: localization.Fairness.Metrics.demographicParityRatio
   },
-  zero_one_loss: {
+  // zero_one_loss is the error rate for binary classification, while
+  // mean_absolute_error is the error rate for probabilistic classification and regression
+  error_rate_difference_binary_classification: {
     description: localization.Fairness.Metrics.errorRateDifferenceDescription,
-    key: "zero_one_loss",
+    key: "error_rate_difference_binary_classification",
     parityMetric: "zero_one_loss",
     parityMode: ParityModes.Difference,
     title: localization.Fairness.Metrics.errorRateDifference
+  },
+  error_rate_difference_regression: {
+    description: localization.Fairness.Metrics.errorRateDifferenceDescription,
+    key: "error_rate_difference_regression",
+    parityMetric: "mean_absolute_error",
+    parityMode: ParityModes.Difference,
+    title: localization.Fairness.Metrics.errorRateDifference
+  },
+  error_rate_ratio_binary_classification: {
+    description: localization.Fairness.Metrics.errorRateRatioDescription,
+    key: "error_rate_ratio_binary_classification",
+    parityMetric: "zero_one_loss",
+    parityMode: ParityModes.Ratio,
+    title: localization.Fairness.Metrics.errorRateRatio
+  },
+  error_rate_ratio_regression: {
+    description: localization.Fairness.Metrics.errorRateRatioDescription,
+    key: "error_rate_ratio_regression",
+    parityMetric: "mean_absolute_error",
+    parityMode: ParityModes.Ratio,
+    title: localization.Fairness.Metrics.errorRateRatio
+  },
+  true_positive_rate_difference: {
+    description:
+      localization.Fairness.Metrics.truePositiveRateDifferenceDescription,
+    key: "true_positive_rate_difference",
+    parityMetric: "recall_score",
+    parityMode: ParityModes.Difference,
+    title: localization.Fairness.Metrics.truePositiveRateDifference
+  },
+  true_positive_rate_ratio: {
+    description:
+      localization.Fairness.Metrics.truePositiveRateDifferenceDescription,
+    key: "true_positive_rate_ratio",
+    parityMetric: "recall_score",
+    parityMode: ParityModes.Ratio,
+    title: localization.Fairness.Metrics.truePositiveRateRatio
   }
 };
