4b440355c9bd6a443f1caeda1accb0ede4e4d7a3
Ke Xu <xuke@microsoft.com>
2020-11-20 15:10:45 -0800

    Revert "make panel scoped"
    This reverts commit 34304434b8247e629a502ee2d8e3b8b643ff1e7d.

--- libs/error-analysis/src/lib/ErrorAnalysisDashboard/Controls/CohortInfo/CohortInfo.tsx
+++ libs/error-analysis/src/lib/ErrorAnalysisDashboard/Controls/CohortInfo/CohortInfo.tsx
@@ -3,11 +3,9 @@
 
 import { JointDataset } from "@responsible-ai/interpret";
 import {
-  Customizer,
   DefaultButton,
   IFocusTrapZoneProps,
   IStackTokens,
-  LayerHost,
   Panel,
   Stack
 } from "office-ui-fabric-react";
@@ -38,113 +36,99 @@ const alignmentStackTokens: IStackTokens = {
 };
 
 export class CohortInfo extends React.PureComponent<ICohortInfoProps> {
-  private scopedPanelHostId = "scopedPanelHostId";
   public render(): React.ReactNode {
     const classNames = cohortInfoStyles();
 
     return (
-      <>
-        <LayerHost id={this.scopedPanelHostId} />
-        <Customizer
-          scopedSettings={{
-            Layer: {
-              hostId: this.scopedPanelHostId
-            }
-          }}
-        >
-          <Panel
-            headerText="Cohort Information"
-            isOpen={this.props.isOpen}
-            focusTrapZoneProps={focusTrapZoneProps}
-            // You MUST provide this prop! Otherwise screen readers will just say "button" with no label.
-            closeButtonAriaLabel="Close"
-            // layerProps={{ hostId: this.props.hostId }}
-            isBlocking={false}
-            onDismiss={this.props.onDismiss}
-          >
-            <div className={classNames.divider}></div>
-            <div className={classNames.section}>
-              <div className={classNames.subsection}>
-                <DefaultButton
-                  text="Save Cohort"
-                  onClick={(): any => this.props.onSaveCohortClick()}
-                />
-              </div>
-              <div className={classNames.section}></div>
-              <div className={classNames.subsection}>
-                <div className={classNames.header}>Basic Information</div>
-                {this.props.currentCohort.cohort.name !== "All data" && (
-                  <div>{this.props.currentCohort.cohort.name}</div>
-                )}
-                <div>
-                  All data ({this.props.currentCohort.cohort.filters.length}{" "}
-                  filters)
+      <Panel
+        headerText="Cohort Information"
+        isOpen={this.props.isOpen}
+        focusTrapZoneProps={focusTrapZoneProps}
+        // You MUST provide this prop! Otherwise screen readers will just say "button" with no label.
+        closeButtonAriaLabel="Close"
+        // layerProps={{ hostId: this.props.hostId }}
+        isBlocking={false}
+        onDismiss={this.props.onDismiss}
+      >
+        <div className={classNames.divider}></div>
+        <div className={classNames.section}>
+          <div className={classNames.subsection}>
+            <DefaultButton
+              text="Save Cohort"
+              onClick={(): any => this.props.onSaveCohortClick()}
+            />
+          </div>
+          <div className={classNames.section}></div>
+          <div className={classNames.subsection}>
+            <div className={classNames.header}>Basic Information</div>
+            {this.props.currentCohort.cohort.name !== "All data" && (
+              <div>{this.props.currentCohort.cohort.name}</div>
+            )}
+            <div>
+              All data ({this.props.currentCohort.cohort.filters.length}{" "}
+              filters)
+            </div>
+          </div>
+        </div>
+        <div className={classNames.divider}></div>
+        <div className={classNames.section}>
+          <div className={classNames.subsection}>
+            <div>Instances in base cohort</div>
+            <Stack>
+              <Stack horizontal tokens={alignmentStackTokens}>
+                <div className={classNames.tableData}>Total</div>
+                <div className={classNames.tableData}>
+                  {this.props.currentCohort.totalAll}
                 </div>
-              </div>
-            </div>
-            <div className={classNames.divider}></div>
-            <div className={classNames.section}>
-              <div className={classNames.subsection}>
-                <div>Instances in base cohort</div>
-                <Stack>
-                  <Stack horizontal tokens={alignmentStackTokens}>
-                    <div className={classNames.tableData}>Total</div>
-                    <div className={classNames.tableData}>
-                      {this.props.currentCohort.totalAll}
-                    </div>
-                  </Stack>
-                  <Stack horizontal tokens={alignmentStackTokens}>
-                    <div className={classNames.tableData}>Correct</div>
-                    <div className={classNames.tableData}>
-                      {this.props.currentCohort.totalCorrect}
-                    </div>
-                  </Stack>
-                  <Stack horizontal tokens={alignmentStackTokens}>
-                    <div className={classNames.tableData}>Incorrect</div>
-                    <div className={classNames.tableData}>
-                      {this.props.currentCohort.totalIncorrect}
-                    </div>
-                  </Stack>
-                </Stack>
-              </div>
-            </div>
-            <div className={classNames.section}>
-              <div className={classNames.subsection}>
-                <div>Instances in the selected cohort</div>
-                <Stack>
-                  <Stack horizontal tokens={alignmentStackTokens}>
-                    <div className={classNames.tableData}>Total</div>
-                    <div className={classNames.tableData}>
-                      {this.props.currentCohort.totalCohort}
-                    </div>
-                  </Stack>
-                  <Stack horizontal tokens={alignmentStackTokens}>
-                    <div className={classNames.tableData}>Correct</div>
-                    <div className={classNames.tableData}>
-                      {this.props.currentCohort.totalCohortCorrect}
-                    </div>
-                  </Stack>
-                  <Stack horizontal tokens={alignmentStackTokens}>
-                    <div className={classNames.tableData}>Incorrect</div>
-                    <div className={classNames.tableData}>
-                      {this.props.currentCohort.totalCohortIncorrect}
-                    </div>
-                  </Stack>
-                </Stack>
-              </div>
-            </div>
-            <div className={classNames.divider}></div>
-            <div className={classNames.section}>
-              <div className={classNames.subsection}>
-                <div className={classNames.header}>
-                  Prediction path (filters)
+              </Stack>
+              <Stack horizontal tokens={alignmentStackTokens}>
+                <div className={classNames.tableData}>Correct</div>
+                <div className={classNames.tableData}>
+                  {this.props.currentCohort.totalCorrect}
                 </div>
-                <PredictionPath temporaryCohort={this.props.currentCohort} />
-              </div>
-            </div>
-          </Panel>
-        </Customizer>
-      </>
+              </Stack>
+              <Stack horizontal tokens={alignmentStackTokens}>
+                <div className={classNames.tableData}>Incorrect</div>
+                <div className={classNames.tableData}>
+                  {this.props.currentCohort.totalIncorrect}
+                </div>
+              </Stack>
+            </Stack>
+          </div>
+        </div>
+        <div className={classNames.section}>
+          <div className={classNames.subsection}>
+            <div>Instances in the selected cohort</div>
+            <Stack>
+              <Stack horizontal tokens={alignmentStackTokens}>
+                <div className={classNames.tableData}>Total</div>
+                <div className={classNames.tableData}>
+                  {this.props.currentCohort.totalCohort}
+                </div>
+              </Stack>
+              <Stack horizontal tokens={alignmentStackTokens}>
+                <div className={classNames.tableData}>Correct</div>
+                <div className={classNames.tableData}>
+                  {this.props.currentCohort.totalCohortCorrect}
+                </div>
+              </Stack>
+              <Stack horizontal tokens={alignmentStackTokens}>
+                <div className={classNames.tableData}>Incorrect</div>
+                <div className={classNames.tableData}>
+                  {this.props.currentCohort.totalCohortIncorrect}
+                </div>
+              </Stack>
+            </Stack>
+          </div>
+        </div>
+        <div className={classNames.divider}></div>
+        <div className={classNames.section}>
+          <div className={classNames.subsection}>
+            <div className={classNames.header}>Prediction path (filters)</div>
+            <PredictionPath temporaryCohort={this.props.currentCohort} />
+          </div>
+        </div>
+      </Panel>
     );
   }
 }
