7b043ab5cacd18d38fdc24cb3e005a9090216526
xuke444 <40614413+xuke444@users.noreply.github.com>
2020-11-23 14:16:54 -0800

    move initializeIcons(); (#152)
    * move initializeIcons();
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>
    
    * fix icon
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>
    
    * remove init icon
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>
    
    * change ci sequence
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>
    
    * make panel scoped
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>
    
    * Revert "make panel scoped"
    
    This reverts commit 34304434b8247e629a502ee2d8e3b8b643ff1e7d.

--- apps/widget/src/main.tsx
+++ apps/widget/src/main.tsx
@@ -1,10 +1,12 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import { initializeIcons } from "office-ui-fabric-react";
 import React from "react";
 import ReactDOM from "react-dom";
 
 import { App } from "./app/App";
+initializeIcons();
 
 ReactDOM.render(
   <React.StrictMode>
