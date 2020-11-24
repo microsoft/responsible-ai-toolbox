127a3c52f3f5126beb90927a4d74a3d2a8a93d21
Ilya Matiach <ilmat@microsoft.com>
2020-11-24 01:11:48 -0500

    removed unused widget code (#156)

--- raiwidgets/raiwidgets/error-analysis/python/error_analysis/widget/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) Microsoft Corporation
-# Licensed under the MIT License.
-
-"""Module for Error Analysis Dashboard widget."""
-
-from .error_analysis import ErrorAnalysisDashboard
-
-__all__ = ['ErrorAnalysisDashboard']
