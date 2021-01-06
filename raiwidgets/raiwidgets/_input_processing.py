0b2b6f42bfce865ffce9fcda026a340351f12c09
Richard Edgar <riedgar@microsoft.com>
2021-01-06 13:19:01 -0500

    Handle DataFrames
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>

--- raiwidgets/raiwidgets/_input_processing.py
+++ raiwidgets/raiwidgets/_input_processing.py
@@ -7,6 +7,8 @@ from scipy.sparse import issparse
 from sklearn.utils import check_consistent_length
 from typing import Dict, List
 
+_DF_COLUMN_BAD_NAME = "DataFrame column names must be strings. Name '{0}' is of type {1}"
+
 
 def _convert_to_list(array):
     if issparse(array):
@@ -34,5 +36,14 @@ def _convert_to_string_list_dict(
             result[ys.name] = _convert_to_list(ys)
         else:
             result[base_name_format.format(0)] = _convert_to_list(ys)
+    elif isinstance(ys, pd.DataFrame):
+        for i in range(len(ys.columns)):
+            col_name = ys.columns[i]
+            if not isinstance(col_name, str):
+                msg = _DF_COLUMN_BAD_NAME.format(col_name, type(col_name))
+                raise ValueError(msg)
+            column = ys.iloc[:, i]
+            check_consistent_length(column, sample_array)
+            result[col_name] = _convert_to_list(column)
 
     return result
--- raiwidgets/tests/test_input_processing.py
+++ raiwidgets/tests/test_input_processing.py
@@ -13,6 +13,7 @@ class TestConvertToStringListDict:
         sample_array = [4, 5, 6]
         result = _convert_to_string_list_dict("Base {0}", input, sample_array)
         assert isinstance(result, dict)
+        assert len(result) == 1
         assert "Base 0" in result
         arr = result["Base 0"]
         assert isinstance(arr, list)
@@ -23,7 +24,23 @@ class TestConvertToStringListDict:
         sample_array = [4, 5, 6]
         result = _convert_to_string_list_dict("Base {0}", input, sample_array)
         assert isinstance(result, dict)
+        assert len(result) == 1
         assert "Something" in result
         arr = result["Something"]
         assert isinstance(arr, list)
         assert np.array_equal(arr, [1, 3, 5])
+
+    def test_dataframe(self):
+        input = pd.DataFrame.from_dict({"a": [0, 1, 2], "b": [4, 5, 6]})
+        sample_array = [3, 6, 9]
+        result = _convert_to_string_list_dict("Base {0}", input, sample_array)
+        assert isinstance(result, dict)
+        assert len(result) == 2
+        assert "a" in result
+        arr = result["a"]
+        assert isinstance(arr, list)
+        assert np.array_equal(arr, [0, 1, 2])
+        assert "b" in result
+        arr = result["b"]
+        assert isinstance(arr, list)
+        assert np.array_equal(arr, [4, 5, 6])
