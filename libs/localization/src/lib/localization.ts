f5a06a422e7956cb2320d7ac896ba0207e7c0dc5
Dawei Li <daweil@microsoft.com>
2021-01-12 15:45:36 -0800

    Fix build

--- libs/localization/src/lib/localization.ts
+++ libs/localization/src/lib/localization.ts
@@ -1,26 +1,44 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+import Cs from "./cs.json";
+import De from "./de.json";
+import En from "./en.json";
+import Es from "./es.json";
+import Fr from "./fr.json";
 import { getLocalization } from './getLocalization';
+import Hu from "./hu.json";
+import It from "./it.json";
+import Ja from "./ja.json";
+import Ko from "./ko.json";
 import { Language } from './Language';
+import Nl from "./nl.json";
+import Pl from "./pl.json";
+import PtBR from "./pt-BR.json";
+import PtPT from "./pt-PT.json";
+import Ru from "./ru.json";
+import Sv from "./sv.json";
+import Tr from "./tr.json";
+import ZhHans from "./zh-Hans.json";
+import ZhHant from "./zh-Hant.json";
 
 export const localization = getLocalization({
-  [Language.En]: import("./en.json"),
-  [Language.Es]: () => import("./es.json"),
-  [Language.Cs]: () => import("./cs.json"),
-  [Language.De]: () => import("./de.json"),
-  [Language.Fr]: () => import("./fr.json"),
-  [Language.It]: () => import("./it.json"),
-  [Language.Ja]: () => import("./ja.json"),
-  [Language.Ko]: () => import("./ko.json"),
-  [Language.PtBR]: () => import("./pt-BR.json"),
-  [Language.Ru]: () => import("./ru.json"),
-  [Language.ZhHans]: () => import("./zh-Hans.json"),
-  [Language.ZhHant]: () => import("./zh-Hant.json"),
-  [Language.Nl]: () => import("./nl.json"),
-  [Language.Hu]: () => import("./hu.json"),
-  [Language.PtPT]: () => import("./pt-PT.json"),
-  [Language.Pl]: () => import("./pl.json"),
-  [Language.Sv]: () => import("./sv.json"),
-  [Language.Tr]: () => import("./tr.json")
+  [Language.En]: En,
+  [Language.Es]: Es,
+  [Language.Cs]: Cs,
+  [Language.De]: De,
+  [Language.Fr]: Fr,
+  [Language.It]: It,
+  [Language.Ja]: Ja,
+  [Language.Ko]: Ko,
+  [Language.PtBR]: PtBR,
+  [Language.Ru]: Ru,
+  [Language.ZhHans]: ZhHans,
+  [Language.ZhHant]: ZhHant,
+  [Language.Nl]: Nl,
+  [Language.Hu]: Hu,
+  [Language.PtPT]: PtPT,
+  [Language.Pl]: Pl,
+  [Language.Sv]: Sv,
+  [Language.Tr]: Tr
 });
