ebb048e1e3fa8ee9215589a5df25670eeb1ccf4b
Dawei Li <daweil@microsoft.com>
2021-01-12 16:13:48 -0800

    Rename

--- libs/localization/src/lib/localization.ts
+++ libs/localization/src/lib/localization.ts
@@ -1,24 +1,24 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import Cs from "./en-us.cs.json";
-import De from "./en-us.de.json";
-import Es from "./en-us.es.json";
-import Fr from "./en-us.fr.json";
-import Hu from "./en-us.hu.json";
-import It from "./en-us.it.json";
-import Ja from "./en-us.ja.json";
-import En from "./en-us.json";
-import Ko from "./en-us.ko.json";
-import Nl from "./en-us.nl.json";
-import Pl from "./en-us.pl.json";
-import PtBR from "./en-us.pt-BR.json";
-import PtPT from "./en-us.pt-PT.json";
-import Ru from "./en-us.ru.json";
-import Sv from "./en-us.sv.json";
-import Tr from "./en-us.tr.json";
-import ZhHans from "./en-us.zh-Hans.json";
-import ZhHant from "./en-us.zh-Hant.json";
+import Cs from "./en.cs.json";
+import De from "./en.de.json";
+import Es from "./en.es.json";
+import Fr from "./en.fr.json";
+import Hu from "./en.hu.json";
+import It from "./en.it.json";
+import Ja from "./en.ja.json";
+import En from "./en.json";
+import Ko from "./en.ko.json";
+import Nl from "./en.nl.json";
+import Pl from "./en.pl.json";
+import PtBR from "./en.pt-BR.json";
+import PtPT from "./en.pt-PT.json";
+import Ru from "./en.ru.json";
+import Sv from "./en.sv.json";
+import Tr from "./en.tr.json";
+import ZhHans from "./en.zh-Hans.json";
+import ZhHant from "./en.zh-Hant.json";
 import { getLocalization } from "./getLocalization";
 import { Language } from "./Language";
 
