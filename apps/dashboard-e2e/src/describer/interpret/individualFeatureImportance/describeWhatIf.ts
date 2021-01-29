d349545b0e3c7ebf64c2e2a65828f7a68c3672c6
Bo Zhang <boz2@uci.edu>
2021-01-29 20:07:43 +0800

    Update describeWhatIf.ts

--- apps/dashboard-e2e/src/describer/interpret/individualFeatureImportance/describeWhatIf.ts
+++ apps/dashboard-e2e/src/describer/interpret/individualFeatureImportance/describeWhatIf.ts
@@ -9,17 +9,26 @@ export function describeWhatIf(datasetShape: IInterpretData): void {
     beforeEach(() => {
       getMenu("Individual feature importance", "#DashboardPivot").click();
     });
-    if (!datasetShape.noY) {
-      it("should delete selected data point", () => {
-        // cy.get("#what-if-expand-btn").click();
+    it("should have no datapoint selected by default", () => {
+      cy.get("#IndividualFeatureContainer").should(
+        "contain.text",
+        "None created yet"
+      );
+    });
+    if (
+      !datasetShape.noY &&
+      !datasetShape.noPredict &&
+      !datasetShape.errorMessage
+    ) {
+      it("should save data point from dropdown option", () => {
+        cy.get("#what-if-expand-btn").click();
+        cy.get("#indexSelector").click();
+        cy.get('button:contains("Row 1")').last().click();
+        cy.get("#whatIfNameLabel").should("have.value", "Copy of row 1");
+
         cy.get('button:contains("Save as new point")').click();
         cy.get("#IndividualFeatureContainer").should(
           "not.contain",
-          "None created yet"
-        );
-        cy.get("#iterative-container button.ms-Button").last().click();
-        cy.get("#IndividualFeatureContainer").should(
-          "contain.text",
           "None created yet"
         );
       });
