8bd1821304f2dface2a2eb9e3f30115df5150c4c
Roman Lutz <rolutz@microsoft.com>
2021-02-01 16:40:57 -0800

    Updates for release v0.2.0 (#298)
    * remove erroneous -dev
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * readme replacement
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * copy README earlier
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * rc1
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * move readme replacement step to a later point
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * attempt at fixing README formatting for pypi
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * update release to include pictures in README
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * rc3
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * replace images with links, bump rai-core-flask req to latest
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * rc4
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * rc5
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * rc6
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * sed before cp, rc7
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * v0.2.0
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- raiwidgets/raiwidgets/__init__.py
+++ raiwidgets/raiwidgets/__init__.py
@@ -9,7 +9,7 @@ from .error_analysis_dashboard import ErrorAnalysisDashboard
 from .model_performance_dashboard import ModelPerformanceDashboard
 
 
-__version__ = "0.2.0-dev"
+__version__ = "0.2.0"
 
 __all__ = ['FairnessDashboard', 'ExplanationDashboard',
            'ErrorAnalysisDashboard', 'ExplanationDashboard',
