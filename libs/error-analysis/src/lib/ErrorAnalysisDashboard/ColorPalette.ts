33ce62cd06f2ab943b7d9990f412dca41c8ef371
Ilya Matiach <ilmat@microsoft.com>
2021-02-07 15:45:56 -0500

    fixed comments

--- libs/error-analysis/src/lib/ErrorAnalysisDashboard/ColorPalette.ts
+++ libs/error-analysis/src/lib/ErrorAnalysisDashboard/ColorPalette.ts
@@ -4,5 +4,9 @@
 export enum ColorPalette {
   MinColor = "#F4D1D2",
   MaxColor = "#8d2323",
-  ErrorAvgColor = "#b2b7bd"
+  ErrorAvgColor = "#b2b7bd",
+  FillStyle = "#d2d2d2",
+  SelectedLineColor = "#089acc",
+  UnselectedLineColor = "#e8eaed",
+  LinkLabelOutline = "#089acc"
 }
