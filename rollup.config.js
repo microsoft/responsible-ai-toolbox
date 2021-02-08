dec046d6375577ce06ebaec6b59115abae814704
Ke Xu <xuke@microsoft.com>
2021-02-08 12:05:33 -0800

    error out on circular dependency
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- rollup.config.js
+++ rollup.config.js
@@ -8,5 +8,23 @@ module.exports = (config) => {
   config.context = "window";
   config.plugins.push(json());
   config.plugins.push(svgr.default());
+  config.onwarn = (warning, warn) => {
+    // ignore
+    if (
+      warning.code === "THIS_IS_UNDEFINED" ||
+      warning.code === "CIRCULAR_DEPENDENCY" ||
+      warning.code === "NAMESPACE_CONFLICT"
+    ) {
+      throw new Error(warning);
+    }
+
+    warn(warning);
+  };
+
+  if (process.env.debug) {
+    require("fs-extra").writeJSONSync("./rollup.json", config, {
+      spaces: 2
+    });
+  }
   return config;
 };
