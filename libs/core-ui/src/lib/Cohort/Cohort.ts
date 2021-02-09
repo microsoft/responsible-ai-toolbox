64d5359de7bc8ef7b18e85e54bd22d0d007eb5b8
Roman Lutz <rolutz@microsoft.com>
2021-02-08 21:35:31 -0800

    fix merge conflicts
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- libs/core-ui/src/lib/Cohort/Cohort.ts
+++ libs/core-ui/src/lib/Cohort/Cohort.ts
@@ -1,8 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { JointDataset } from "@responsible-ai/core-ui";
-
 import {
   IFilter,
   FilterMethods,
--- libs/error-analysis/src/lib/ErrorAnalysisDashboard/Controls/MatrixArea/MatrixArea.tsx
+++ libs/error-analysis/src/lib/ErrorAnalysisDashboard/Controls/MatrixArea/MatrixArea.tsx
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { JointDataset } from "@responsible-ai/core-ui";
 import {
   IFilter,
   ICompositeFilter,
