463292a3b11e299ef43b2e252452cfa50b02830f
Ke Xu <xuke@microsoft.com>
2021-02-24 11:19:48 -0800

    fix flake
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- raiwidgets/raiwidgets/__version__.py
+++ raiwidgets/raiwidgets/__version__.py
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft Corporation
 # Licensed under the MIT License.
 
-# Version will be populated during release from version.cfg file 
+# Version will be populated during release from version.cfg file
 # by running `yarn auto-version`
 version = "0.0.0"
