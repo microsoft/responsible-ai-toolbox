0925a432806d6d3ea6e619dbc58780effeff9d99
Ke Xu <xuke@microsoft.com>
2021-02-26 00:38:42 -0800

    rollback version
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- raiwidgets/raiwidgets/__version__.py
+++ raiwidgets/raiwidgets/__version__.py
@@ -1,1 +1,6 @@
-version = "0.3.0-233"
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+# Version will be populated during release from version.cfg file
+# by running `yarn auto-version`
+version = "0.0.0"
