d9afda32cd87b4d101473e58c17df88a91077cdd
Roman Lutz <rolutz@microsoft.com>
2021-03-03 19:57:50 -0800

    fix widget code
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- apps/widget/src/app/ModelAssessment.tsx
+++ apps/widget/src/app/ModelAssessment.tsx
@@ -2,6 +2,12 @@
 // Licensed under the MIT License.
 
 import { ModelAssessmentDashboard } from "@responsible-ai/model-assessment";
+import {
+  IDataset,
+  IMetricRequest,
+  IMetricResponse,
+  IModelExplanationData
+} from "@responsible-ai/core-ui";
 import React from "react";
 
 import { callFlaskService } from "./callFlaskService";
@@ -14,6 +20,7 @@ export class ModelAssessment extends React.Component {
     let requestMatrixMethod = undefined;
     let requestDebugMLMethod = undefined;
     let requestImportancesMethod = undefined;
+    let requestMetricsMethod = undefined;
     if (config.baseUrl !== undefined) {
       requestPredictionsMethod = async (data: any[]): Promise<any[]> => {
         return callFlaskService(data, "/predict");
@@ -27,32 +34,50 @@ export class ModelAssessment extends React.Component {
       requestImportancesMethod = async (data: any[]): Promise<any[]> => {
         return callFlaskService(data, "/importances");
       };
+      requestMetricsMethod = async (
+        data: IMetricRequest
+      ): Promise<IMetricResponse> => {
+        return callFlaskService(data, "/metrics") as Promise<IMetricResponse>;
+      };
     }
+
+    const dataset: IDataset = {
+      categoricalMap: modelData.categoricalMap,
+      classNames: modelData.classNames,
+      featureNames: modelData.featureNames,
+      features: modelData.features,
+      sensitiveFeatures: modelData.sensitiveFeatures,
+      trueY: modelData.trueY
+    };
+
+    const modelExplanationData: IModelExplanationData = {
+      modelClass: "modelClass" in modelData ? modelData.modelClass : "blackbox",
+      predictedY: modelData.predictedY,
+      probabilityY: modelData.probabilityY,
+      precomputedExplanations: {
+        ebmGlobalExplanation: modelData.ebmData,
+        globalFeatureImportance: modelData.globalExplanation,
+        localFeatureImportance: modelData.localExplanations
+      }
+    };
 
     return (
       <ModelAssessmentDashboard
-        modelInformation={{ modelClass: "blackbox" }}
-        dataSummary={{
-          categoricalMap: modelData.categoricalMap,
-          classNames: modelData.classNames,
-          featureNames: modelData.featureNames
-        }}
-        testData={modelData.trainingData}
-        predictedY={modelData.predictedY}
-        probabilityY={modelData.probabilityY}
-        trueY={modelData.trueY}
-        precomputedExplanations={{
-          ebmGlobalExplanation: modelData.ebmData,
-          globalFeatureImportance: modelData.globalExplanation,
-          localFeatureImportance: modelData.localExplanations
-        }}
+        dataset={dataset}
+        modelExplanationData={modelExplanationData}
         requestPredictions={requestPredictionsMethod}
         requestDebugML={requestDebugMLMethod}
         requestMatrix={requestMatrixMethod}
+        requestMetrics={requestMetricsMethod}
         requestImportances={requestImportancesMethod}
         localUrl={modelData.localUrl}
         locale={modelData.locale}
-        features={modelData.featureNames}
+        theme={modelData.theme}
+        supportedBinaryClassificationPerformanceKeys={
+          modelData.classification_methods
+        }
+        supportedRegressionPerformanceKeys={modelData.regression_methods}
+        supportedProbabilityPerformanceKeys={modelData.probability_methods}
       />
     );
   }
--- libs/core-ui/src/lib/Interfaces/IModelExplanationData.ts
+++ libs/core-ui/src/lib/Interfaces/IModelExplanationData.ts
@@ -1,11 +1,7 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import {
-  IMultiClassLocalFeatureImportance,
-  IPrecomputedExplanations,
-  ISingleClassLocalFeatureImportance
-} from "./ExplanationInterfaces";
+import { IPrecomputedExplanations } from "./ExplanationInterfaces";
 
 export interface IModelExplanationData {
   modelClass: "Tree" | "EBM" | "blackbox";
@@ -14,7 +10,4 @@ export interface IModelExplanationData {
   probabilityY?: number[][];
   explanationMethod?: string;
   precomputedExplanations?: IPrecomputedExplanations;
-  localExplanations:
-    | IMultiClassLocalFeatureImportance
-    | ISingleClassLocalFeatureImportance;
 }
