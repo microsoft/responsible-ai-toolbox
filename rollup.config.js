1a0da3563fe3bd48abdfa402c53df7a81cdd2efd
xuke444 <40614413+xuke444@users.noreply.github.com>
2021-05-06 21:55:34 -0700

    fix localization build (#500)
    * fix localization build
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>
    
    * remove junk
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,10 @@
 const nrwlConfig = require("@nrwl/react/plugins/bundle-rollup");
-const json = require("@rollup/plugin-json");
 const svgr = require("@svgr/rollup");
 
 module.exports = (config) => {
   config = nrwlConfig(config);
 
   config.context = "window";
-  config.plugins.push(json());
   config.plugins.push(svgr.default());
   config.onwarn = (warning, warn) => {
     if (
