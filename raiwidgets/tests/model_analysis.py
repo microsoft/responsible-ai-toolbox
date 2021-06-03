2c362f1e4f0ad1f0ad1138691467ec80457377ed
Ke Xu <xuke@microsoft.com>
2021-06-02 17:07:53 -0700

    add counter factual
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- raiwidgets/tests/model_analysis.py
+++ raiwidgets/tests/model_analysis.py
@@ -10,8 +10,9 @@ import pandas as pd
 x, y = shap.datasets.adult()
 y = [1 if r else 0 for r in y]
 
+
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-    x, y, test_size=0.2, random_state=7)
+    x, y, test_size=0.02, random_state=7)
 
 knn = sklearn.neighbors.KNeighborsClassifier()
 knn.fit(x_train, y_train)
@@ -23,15 +24,17 @@ test = pd.merge(x_test, pd.DataFrame(y_test, columns=[
                 "income"]), left_index=True, right_index=True)
 
 ma = ModelAnalysis(knn, train, test, "income", "classification",
-                   categorical_features=[])
-ma.explainer.add()
-ma.counterfactual.add(['Age', 'Workclass', 'Education-Num', 'Marital Status',
-                       'Occupation', 'Relationship', 'Race', 'Sex',
+                   categorical_features=['Workclass', 'Education-Num',
+                                         'Marital Status',
+                                         'Occupation', 'Relationship', 'Race',
+                                         'Sex', 'Country'])
+# ma.explainer.add()
+ma.counterfactual.add(['Age',
                        'Capital Gain', 'Capital Loss',
-                       'Hours per week', 'Country'], 10,
+                       'Hours per week'], 10,
                       desired_class="opposite")
-ma.error_analysis.add()
-ma.causal.add()
+# ma.error_analysis.add()
+# ma.causal.add()
 ma.compute()
 
 ModelAnalysisDashboard(ma)
