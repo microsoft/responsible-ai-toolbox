f197f3d0db8fae2cad1ab9927194c6151c4fbd78
Ke Xu <xuke@microsoft.com>
2021-06-02 21:08:58 -0700

    rollback comments
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- raiwidgets/tests/model_analysis.py
+++ raiwidgets/tests/model_analysis.py
@@ -12,7 +12,7 @@ y = [1 if r else 0 for r in y]
 
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(
-    x, y, test_size=0.02, random_state=7)
+    x, y, test_size=0.2, random_state=7)
 
 knn = sklearn.neighbors.KNeighborsClassifier()
 knn.fit(x_train, y_train)
@@ -28,13 +28,13 @@ ma = ModelAnalysis(knn, train, test, "income", "classification",
                                          'Marital Status',
                                          'Occupation', 'Relationship', 'Race',
                                          'Sex', 'Country'])
-# ma.explainer.add()
+ma.explainer.add()
 ma.counterfactual.add(['Age',
                        'Capital Gain', 'Capital Loss',
                        'Hours per week'], 10,
                       desired_class="opposite")
-# ma.error_analysis.add()
-# ma.causal.add()
+ma.error_analysis.add()
+ma.causal.add()
 ma.compute()
 
 ModelAnalysisDashboard(ma)
