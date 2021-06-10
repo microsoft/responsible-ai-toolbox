2bc4f3c633d8a17857a9c916d970e1f671faa014
Roman Lutz <rolutz@microsoft.com>
2021-06-09 18:35:39 -0700

    Add cards around components in model assessment dashboard (#589)
    * add cards around components
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * lintfix
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * move style into separate fiile
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>
    
    * lintfix
    
    Signed-off-by: Roman Lutz <rolutz@microsoft.com>

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/AddTabButton.tsx
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/AddTabButton.tsx
@@ -61,7 +61,7 @@ export class AddTabButton extends React.Component<
   public render(): React.ReactNode {
     const style = addTabButtonStyles();
     return (
-      <>
+      <div className={style.buttonSection}>
         <div className={style.splitter} />
         <Stack horizontal horizontalAlign={"center"}>
           <IconButton
@@ -92,7 +92,7 @@ export class AddTabButton extends React.Component<
             </Callout>
           )}
         </Stack>
-      </>
+      </div>
     );
   }
 
