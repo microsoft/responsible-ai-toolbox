9293564c22581d2fdff96798811e59096ff63fec
Ke Xu <xuke@microsoft.com>
2021-06-11 21:06:14 -0700

    fix test
    Signed-off-by: Ke Xu <xuke@microsoft.com>

--- responsibleai/tests/counterfactual_manager_validator.py
+++ responsibleai/tests/counterfactual_manager_validator.py
@@ -6,6 +6,8 @@ from responsibleai import ModelTask
 from responsibleai.exceptions import (
     DuplicateManagerConfigException, UserConfigValidationException
 )
+from dice_ml.utils.exception import UserConfigValidationException\
+    as DiceException
 
 
 def validate_counterfactual(cf_analyzer, X_train, target_column,
@@ -43,16 +45,13 @@ def validate_counterfactual(cf_analyzer, X_train, target_column,
     assert len(cf_analyzer.counterfactual.get()) == 2
 
     # Add a bad configuration
-    cf_analyzer.counterfactual.add(continuous_features=continuous_features,
-                                   total_CFs=-1,
-                                   method='random',
-                                   desired_class=desired_class,
-                                   desired_range=desired_range)
-    cf_analyzer.counterfactual.compute()
-    assert cf_analyzer.counterfactual.get() is not None
-    assert isinstance(cf_analyzer.counterfactual.get(), list)
-    assert len(cf_analyzer.counterfactual.get()) == 2
-    assert len(cf_analyzer.counterfactual.get(failed_to_compute=True)) == 1
+    with pytest.raises(DiceException):
+        cf_analyzer.counterfactual.add(continuous_features=continuous_features,
+                                       total_CFs=-1,
+                                       method='random',
+                                       desired_class=desired_class,
+                                       desired_range=desired_range)
+        cf_analyzer.counterfactual.compute()
 
     task_type = cf_analyzer.task_type
     if task_type == ModelTask.REGRESSION:
