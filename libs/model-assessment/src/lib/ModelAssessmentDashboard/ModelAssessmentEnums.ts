83ceb26487927d4278d736e0164d291bc4723d3e
Yongjia Li <yongjiali@microsoft.com>
2021-09-03 22:51:20 -0700

    rename the component names in the dashboard setting
    Signed-off-by: Yongjia Li <yongjiali@microsoft.com>

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/ModelAssessmentEnums.ts
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/ModelAssessmentEnums.ts
@@ -2,11 +2,11 @@
 // Licensed under the MIT License.
 
 export enum GlobalTabKeys {
-  DataExplorerTab = "DataExplorerTab",
-  FeatureImportancesTab = "FeatureImportancesTab",
-  ErrorAnalysisTab = "ErrorAnalysisTab",
-  FairnessTab = "FairnessTab",
-  ModelStatisticsTab = "ModelStatisticsTab",
-  CausalAnalysisTab = "CausalAnalysisTab",
-  CounterfactualsTab = "CounterfactualsTab"
+  DataExplorerTab = "Data Explorer",
+  FeatureImportancesTab = "Feature Importances",
+  ErrorAnalysisTab = "Error Analysis",
+  FairnessTab = "Fairness",
+  ModelStatisticsTab = "Model Statistics",
+  CausalAnalysisTab = "Causal Analysis",
+  CounterfactualsTab = "Counterfactuals"
 }
