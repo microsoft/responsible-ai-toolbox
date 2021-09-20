86ef1630ccc4f9abe0b7498332747b12694172bc
Jenna Ellis <jeel@microsoft.com>
2021-09-20 09:45:18 -0700

    signed

--- apps/widget/src/app/Fairness.tsx
+++ apps/widget/src/app/Fairness.tsx
@@ -11,7 +11,7 @@ import { modelData } from "./modelData";
 
 export class Fairness extends React.Component {
   public render(): React.ReactNode {
-    console.log("testing");
+    console.log("testing_two");
     let requestMethod = undefined;
     if (config.baseUrl !== undefined) {
       requestMethod = async (
