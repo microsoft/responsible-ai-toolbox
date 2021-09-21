a1369530cbfa0b5bf91c393701f3f2bab3987e60
zhb000 <zhb@microsoft.com>
2021-09-21 09:55:03 -0700

    update

--- libs/core-ui/src/index.ts
+++ libs/core-ui/src/index.ts
@@ -34,7 +34,6 @@ export * from "./lib/util/Never";
 export * from "./lib/util/PartialRequired";
 export * from "./lib/util/nameof";
 export * from "./lib/util/rowErrorSize";
-export * from "./lib/util/getFeatureOptions";
 export * from "./lib/util/string";
 export * from "./lib/util/toScientific";
 export * from "./lib/util/ITelemetryMessage";
