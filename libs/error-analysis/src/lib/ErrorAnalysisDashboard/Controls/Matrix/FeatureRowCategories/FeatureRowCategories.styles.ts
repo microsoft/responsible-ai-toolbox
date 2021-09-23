3d08f31c4b7b36f327f0ba170d929ed2af1b32cb
Ilya Matiach <ilmat@microsoft.com>
2021-09-23 16:18:27 -0400

    add tooltip for heatmap categories and left-justify the heatmap

---
+++ libs/error-analysis/src/lib/ErrorAnalysisDashboard/Controls/Matrix/FeatureRowCategories/FeatureRowCategories.styles.ts
@@ -0,0 +1,46 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+  IStyle,
+  mergeStyleSets,
+  IProcessedStyleSet
+} from "office-ui-fabric-react";
+
+export interface IFeatureRowCategoriesStyles {
+  matrixCellPivot1Categories: IStyle;
+  matrixCol: IStyle;
+  matrixRow: IStyle;
+}
+
+export const featureRowCategoriesStyles: () => IProcessedStyleSet<IFeatureRowCategoriesStyles> =
+  () => {
+    return mergeStyleSets<IFeatureRowCategoriesStyles>({
+      matrixCellPivot1Categories: {
+        alignItems: "center",
+        border: "none",
+        display: "flex",
+        fontSize: "12px",
+        justifyContent: "flex-end",
+        lineHeight: "16px",
+        margin: "2px",
+        maxWidth: "180px",
+        minWidth: "20px",
+        overflow: "hidden",
+        paddingLeft: "2px"
+      },
+      matrixCol: {
+        display: "flex",
+        flexDirection: "column",
+        fontStyle: "normal",
+        fontWeight: "normal"
+      },
+      matrixRow: {
+        display: "flex",
+        flexDirection: "row",
+        fontStyle: "normal",
+        fontWeight: "normal",
+        height: "50px"
+      }
+    });
+  };
