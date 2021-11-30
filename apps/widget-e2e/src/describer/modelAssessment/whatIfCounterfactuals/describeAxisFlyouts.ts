43c01e6e40f030e1851c068411ea6aeb81f0adfa
vinutha karanth <vinutha.karanth@gmail.com>
2021-11-30 11:59:38 -0800

    add tests

--- apps/widget-e2e/src/describer/modelAssessment/whatIfCounterfactuals/describeAxisFlyouts.ts
+++ apps/widget-e2e/src/describer/modelAssessment/whatIfCounterfactuals/describeAxisFlyouts.ts
@@ -12,36 +12,75 @@ export function describeAxisFlyouts(dataShape: IModelAssessmentData): void {
           .click()
           .get(Locators.WhatIfAxisPanel)
           .should("exist");
-        cy.get(Locators.WhatIfScatterChartYAxisFlyoutCancel).click();
+        cy.get(Locators.WhatIfScatterChartFlyoutCancel).click();
         cy.get(Locators.WhatIfAxisPanel).should("not.exist");
       });
       it("should have dropdown with feature list", () => {
         cy.get(Locators.WhatIfScatterChartYAxis).click();
-        cy.get(Locators.WhatIfYAxisFeatureDropdown).click();
+        cy.get(Locators.WhatIfAxisFeatureDropdown).click();
         cy.get(Locators.WhatIfYAxisFeatureDropdownOption).should(
           "have.length",
           dataShape.featureNames?.length
         );
-        cy.get(Locators.WhatIfScatterChartYAxisFlyoutCancel).click();
+        cy.get(Locators.WhatIfScatterChartFlyoutCancel).click();
       });
       it("should be able to select different feature", () => {
         cy.get(Locators.WhatIfScatterChartYAxis).click();
-        cy.get(Locators.WhatIfYAxisFeatureDropdownCurrentOption).should(
+        cy.get(Locators.WhatIfAxisFeatureDropdownCurrentOption).should(
           "attr",
           "value",
           "age"
         );
-        cy.get(Locators.WhatIfYAxisFeatureDropdown).click();
+        cy.get(Locators.WhatIfAxisFeatureDropdown).click();
         cy.get(Locators.WhatIfYAxisFeatureDropdownOccupationOption).click();
-        cy.get(Locators.WhatIfScatterChartYAxisFlyoutSelect).click();
+        cy.get(Locators.WhatIfScatterChartFlyoutSelect).click();
         cy.get(Locators.WhatIfScatterChartYAxisLabelUpdated).should("exist");
       });
 
       it("should be able to select axis value", () => {
         cy.get(Locators.WhatIfScatterChartYAxis).click();
         cy.get(Locators.WhatIfYAxisAxisValueNewValue).click();
-        cy.get(Locators.WhatIfScatterChartYAxisFlyoutSelect).click();
+        cy.get(Locators.WhatIfScatterChartFlyoutSelect).click();
         cy.get(Locators.WhatIfScatterChartYAxisLabelUpdated2).should("exist");
+      });
+    });
+
+    describe.only("X Axis functionalities", () => {
+      it("should be able cancel the flyout", () => {
+        cy.get(Locators.WhatIfScatterChartXAxis)
+          .click()
+          .get(Locators.WhatIfAxisPanel)
+          .should("exist");
+        cy.get(Locators.WhatIfScatterChartFlyoutCancel).click();
+        cy.get(Locators.WhatIfAxisPanel).should("not.exist");
+      });
+      it("should have dropdown with class", () => {
+        cy.get(Locators.WhatIfScatterChartXAxis).click();
+        cy.get(Locators.WhatIfAxisFeatureDropdown).click();
+        cy.get(Locators.WhatIfYAxisFeatureDropdownOption).should(
+          "have.length",
+          2
+        );
+        cy.get(Locators.WhatIfScatterChartFlyoutCancel).click();
+      });
+      it("should be able to select different class", () => {
+        cy.get(Locators.WhatIfScatterChartXAxis).click();
+        cy.get(Locators.WhatIfAxisFeatureDropdownCurrentOption).should(
+          "attr",
+          "value",
+          "Probability : <=50K"
+        );
+        cy.get(Locators.WhatIfAxisFeatureDropdown).click();
+        cy.get(Locators.WhatIfXAxisFeatureDropdownOccupationOption).click();
+        cy.get(Locators.WhatIfScatterChartFlyoutSelect).click();
+        cy.get(Locators.WhatIfScatterChartXAxisLabelUpdated).should("exist");
+      });
+
+      it("should be able to select axis value", () => {
+        cy.get(Locators.WhatIfScatterChartXAxis).click();
+        cy.get(Locators.WhatIfXAxisAxisValueNewValue).click();
+        cy.get(Locators.WhatIfScatterChartFlyoutSelect).click();
+        cy.get(Locators.WhatIfScatterChartXAxisLabelUpdated2).should("exist");
       });
     });
   });
