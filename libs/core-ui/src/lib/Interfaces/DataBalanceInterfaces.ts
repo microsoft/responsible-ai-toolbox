0f1e10017fb76ab43aac9ffd6943f445fd7b5850
Kashyap Patel <kapat@microsoft.com>
2022-01-04 18:50:04 -0500

    Printing all types of data balance measures

--- libs/core-ui/src/lib/Interfaces/DataBalanceInterfaces.ts
+++ libs/core-ui/src/lib/Interfaces/DataBalanceInterfaces.ts
@@ -2,26 +2,73 @@
 // Licensed under the MIT License.
 
 export interface IDataBalanceMeasures {
-  featureBalanceMeasures?: IFeatureBalanceMeasures;
-  distributionBalanceMeasures?: IDistributionBalanceMeasures;
   aggregateBalanceMeasures?: IAggregateBalanceMeasures;
-}
-
-export interface IFeatureBalanceMeasures {
-  measures?: { [key: string]: { [key: string]: number } };
-}
-
-export interface IFeatureBalanceMeasure {
-  // TODO: Make this interface an indexable type of something like featureName::classA::classB
-  featureName?: string;
-  classA?: string;
-  classB?: string;
-}
-
-export interface IDistributionBalanceMeasures {
-  measures?: { [key: string]: { [key: string]: number } };
+  distributionBalanceMeasures?: IDistributionBalanceMeasures;
+  featureBalanceMeasures?: IFeatureBalanceMeasures;
 }
 
 export interface IAggregateBalanceMeasures {
-  measures?: { [key: string]: number };
+  measures?: { [measureName: string]: number };
+}
+
+export function getAggregateBalanceMeasures(measures: {
+  [measureName: string]: number;
+}): { [measureName: string]: number } {
+  return measures;
+}
+
+export interface IDistributionBalanceMeasures {
+  measures?: { [featureName: string]: { [measureName: string]: number } };
+}
+
+export function getDistributionBalanceMeasures(
+  measures: { [featureName: string]: { [measureName: string]: number } },
+  featureName: string
+): { [measureName: string]: number } {
+  if (featureName in measures) {
+    return measures[featureName];
+  }
+
+  return {};
+}
+
+export interface IFeatureBalanceMeasures {
+  measures?: {
+    [featureName: string]: {
+      [classKey: string]: { [measureName: string]: number };
+    };
+  };
+}
+
+export function getFeatureBalanceMeasures(
+  measures: {
+    [featureName: string]: {
+      [classKey: string]: { [measureName: string]: number };
+    };
+  },
+  featureName: string,
+  classA: string,
+  classB: string
+): { [measureName: string]: number } {
+  const classes = measures[featureName];
+  if (classes) {
+    let classKey = `${classA}__${classB}`;
+    if (classKey in classes) {
+      return classes[classKey];
+    }
+
+    classKey = `${classB}__${classA}`;
+    if (classKey in classes) {
+      const measures = classes[classKey];
+      for (const measure in measures) {
+        // Measures are available for the string "classB__classA" but not for "classA__classB"
+        // Therefore, we inverse the values of each measure to get the correct values for classA and classB
+        measures[measure] *= -1;
+      }
+
+      return measures;
+    }
+  }
+
+  return {};
 }
--- libs/dataset-explorer/src/lib/DataBalanceTab.tsx
+++ libs/dataset-explorer/src/lib/DataBalanceTab.tsx
@@ -6,6 +6,12 @@ import {
   MissingParametersPlaceholder,
   ModelAssessmentContext
 } from "@responsible-ai/core-ui";
+import {
+  getAggregateBalanceMeasures,
+  getDistributionBalanceMeasures,
+  getFeatureBalanceMeasures
+} from "libs/core-ui/src/lib/Interfaces/DataBalanceInterfaces";
+import { Stack, StackItem } from "office-ui-fabric-react";
 import React from "react";
 
 import { dataBalanceTabStyles } from "./DataBalanceTab.styles";
@@ -49,12 +55,101 @@ export class DataBalanceTab extends React.Component<
 
     return (
       <div className={classNames.page}>
-        <h1>Hi</h1>
-        {
-          this.context.dataset.dataBalanceMeasures?.aggregateBalanceMeasures
-            ?.measures?.atkinsonIndex
-        }
-        <h1>Done</h1>
+        <h1>Aggregate Balance Measures</h1>
+        <Stack>
+          <StackItem>
+            {`atkinsonIndex --> ${
+              getAggregateBalanceMeasures(
+                this.context.dataset.dataBalanceMeasures
+                  ?.aggregateBalanceMeasures?.measures ?? {}
+              ).atkinsonIndex
+            }`}
+          </StackItem>
+          <StackItem>
+            {`theilLIndex --> ${
+              getAggregateBalanceMeasures(
+                this.context.dataset.dataBalanceMeasures
+                  ?.aggregateBalanceMeasures?.measures ?? {}
+              ).theilLIndex
+            }`}
+          </StackItem>
+          <StackItem>
+            {`theilTIndex --> ${
+              getAggregateBalanceMeasures(
+                this.context.dataset.dataBalanceMeasures
+                  ?.aggregateBalanceMeasures?.measures ?? {}
+              ).theilTIndex
+            }`}
+          </StackItem>
+        </Stack>
+        <h1>Distribution Balance Measures</h1>
+        <Stack>
+          <StackItem>
+            {`race wassersteinDist --> ${
+              getDistributionBalanceMeasures(
+                this.context.dataset.dataBalanceMeasures
+                  ?.distributionBalanceMeasures?.measures ?? {},
+                "race"
+              ).wassersteinDist
+            }`}
+          </StackItem>
+          <StackItem>
+            {`sex jsDist --> ${
+              getDistributionBalanceMeasures(
+                this.context.dataset.dataBalanceMeasures
+                  ?.distributionBalanceMeasures?.measures ?? {},
+                "sex"
+              ).jsDist
+            }`}
+          </StackItem>
+        </Stack>
+        <h1>Feature Balance Measures</h1>
+        <Stack>
+          <StackItem>
+            {`race Other Asian-Pac-Islander dp --> ${
+              getFeatureBalanceMeasures(
+                this.context.dataset.dataBalanceMeasures?.featureBalanceMeasures
+                  ?.measures ?? {},
+                "race",
+                "Other",
+                "Asian-Pac-Islander"
+              ).dp
+            }`}
+          </StackItem>
+          <StackItem>
+            {`race Asian-Pac-Islander Other dp --> ${
+              getFeatureBalanceMeasures(
+                this.context.dataset.dataBalanceMeasures?.featureBalanceMeasures
+                  ?.measures ?? {},
+                "race",
+                "Asian-Pac-Islander",
+                "Other"
+              ).dp
+            }`}
+          </StackItem>
+          <StackItem>
+            {`sex Male Female dp --> ${
+              getFeatureBalanceMeasures(
+                this.context.dataset.dataBalanceMeasures?.featureBalanceMeasures
+                  ?.measures ?? {},
+                "sex",
+                "Male",
+                "Female"
+              ).dp
+            }`}
+          </StackItem>
+          <StackItem>
+            {`sex Female Male dp --> ${
+              getFeatureBalanceMeasures(
+                this.context.dataset.dataBalanceMeasures?.featureBalanceMeasures
+                  ?.measures ?? {},
+                "sex",
+                "Female",
+                "Male"
+              ).dp
+            }`}
+          </StackItem>
+        </Stack>
       </div>
     );
   }
