095058f215d97964fded64bd30f7d63759b9e4ad
zhb000 <zhb@microsoft.com>
2022-01-24 13:24:51 -0800

    temnp

--- libs/core-ui/src/lib/util/getErrorBarChartOptions.ts
+++ libs/core-ui/src/lib/util/getErrorBarChartOptions.ts
@@ -9,63 +9,52 @@ import { ICausalAnalysisSingleData } from "../Interfaces/ICausalAnalysisData";
 export function getErrorBarChartOptions(
   data: ICausalAnalysisSingleData[]
 ): IHighchartsConfig {
-  console.log(data);
   return {
     chart: {
       type: "lowmedhigh",
       zoomType: "xy"
     },
+    legend: {},
     series: [
       {
-        data: [
-          0.05046395672759368, 0.000009022198590758107, -0.19240139322214506
-        ],
-        name: "Rainfall",
+        data: data.map((d) => d.point),
         tooltip: {
           pointFormat:
-            '<span style="font-weight: bold; color: {series.color}">{series.name}</span>: <b>{point.y:.1f} mm</b> '
+            '<span style="font-weight: bold; color: {series.color}">{series.name}</span>: <b>{point.y:.1f}</b> '
         },
         type: "scatter"
-      },
-      {
-        data: [
-          [0.024554703670239472, 0.07637320978494788],
-          [0.0000018156074109169128, 0.0000162287897705993],
-          [-0.2419328967749073, -0.1428698896693828]
-        ],
-        name: "Rainfall error",
-        tooltip: {
-          pointFormat: "(error range: {point.low}-{point.high} mm)<br/>"
-        },
-        type: "errorbar"
       }
+      // {
+      //   data: [
+      //     [0.024554703670239472, 0.07637320978494788],
+      //     [0.0000018156074109169128, 0.0000162287897705993],
+      //     [-0.2419328967749073, -0.1428698896693828]
+      //   ],
+      //   name: "Rainfall error",
+      //   tooltip: {
+      //     pointFormat: "(error range: {point.low}-{point.high} mm)<br/>"
+      //   },
+      //   type: "errorbar"
+      // }
     ],
     title: {
-      text: "Temperature vs Rainfall"
+      text: ""
     },
     tooltip: {
       shared: true
     },
-
     xAxis: [
       {
-        categories: ["fnlwgt", "capital-gain", "education-num(Yes)"]
+        categories: data.map((d) => d.feature)
       }
     ],
-
     yAxis: [
       {
         labels: {
-          format: "{value} mm",
-          style: {
-            color: "red"
-          }
+          format: "{value}"
         },
         title: {
-          style: {
-            color: "red"
-          },
-          text: "Rainfall"
+          text: ""
         }
       }
     ]
