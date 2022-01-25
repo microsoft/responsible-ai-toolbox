9156454f541512a753badce770f2147000e6b65b
Bo Zhang <71688188+zhb000@users.noreply.github.com>
2022-01-26 04:26:38 +0800

    Add Highchart  (#1168)
    * fluentUI
    
    * highcharts
    
    * test
    
    * temp
    
    * temp
    
    * temp
    
    * check
    
    * fix
    
    * fix merge
    
    * fix
    
    * remove dynamic import
    
    Signed-off-by: Ke xu <xuke@microsoft.com>
    
    * temp
    
    * check
    
    * test
    
    * temnp
    
    * Error bar with theme
    
    * build
    
    * build
    
    * fix
    
    * lint
    
    * lint
    
    Co-authored-by: Ke xu <xuke@microsoft.com>

---
+++ libs/core-ui/src/lib/Highchart/ICommonChartProps.ts
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { ITheme } from "@fluentui/react";
+
+import { IHighchartsConfig } from "./HighchartTypes";
+
+export interface ICommonChartProps {
+  className?: string;
+  fallback?: React.ReactNode;
+  configOverride?: IHighchartsConfig;
+  theme?: ITheme;
+}
