e1cdaaecc1cb0dbf4607279532dd72e2a315e337
Roman Lutz <romanlutz13@gmail.com>
2022-02-04 11:33:28 -0500

    Add multiclass classification dataset & set up basic model-assessment tests (#1191)
    * add wine dataset for multiclass classification
    
    * add basic tests for model overview section for binary and multiclass classification as well as regression
    
    * lintfix
    
    * undo changes to notebook
    
    * update tests to reflect changes on main (addition of new metrics and string changes), simplify test setup
    
    * lintfix

---
+++ apps/dashboard-e2e/src/describer/modelAssessment/modelOverview/describeAxisConfigDialog.ts
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export function describeAxisConfigDialog(): void {
+  describe("Axis settings dialog", () => {
+    describe("Y Axis settings dialog", () => {
+      it("should display settings dialog", () => {
+        cy.get(
+          '#OverallMetricChart div[class*="rotatedVerticalBox"] button'
+        ).click();
+        cy.get("#AxisConfigPanel div.ms-Panel-main").should("exist");
+      });
+      it("should be able to hide settings", () => {
+        cy.get("#AxisConfigPanel button.ms-Panel-closeButton").click();
+        cy.get("#AxisConfigPanel div.ms-Panel-main").should("not.exist");
+      });
+      it("should change to different y-axis title", () => {
+        cy.get(
+          '#OverallMetricChart div[class*="rotatedVerticalBox"] button'
+        ).click();
+
+        cy.get("#AxisConfigPanel div[class*='ms-ChoiceFieldGroup'] label:eq(1)")
+          .invoke("text")
+          .then((text1) => {
+            cy.get(`#AxisConfigPanel label:contains(${text1})`).click();
+            cy.get("#AxisConfigPanel")
+              .find("button")
+              .contains("Select")
+              .click();
+            cy.get(
+              '#OverallMetricChart div[class*="rotatedVerticalBox"] button:eq(0)'
+            ).contains(text1);
+          });
+      });
+    });
+    describe("X Axis settings dialog", () => {
+      it("should display settings dialog", () => {
+        cy.get(
+          '#OverallMetricChart div[class*="horizontalAxis"] button'
+        ).click();
+        cy.get("#AxisConfigPanel div.ms-Panel-main").should("exist");
+      });
+      it("should be able to hide settings", () => {
+        cy.get("#AxisConfigPanel button.ms-Panel-closeButton").click();
+        cy.get("#AxisConfigPanel div.ms-Panel-main").should("not.exist");
+      });
+      it("should change to different x-axis title", () => {
+        cy.get(
+          '#OverallMetricChart div[class*="horizontalAxis"] button'
+        ).click();
+
+        cy.get("#AxisConfigPanel div[class*='ms-ChoiceFieldGroup'] label:eq(1)")
+          .invoke("text")
+          .then((text1) => {
+            cy.get(`#AxisConfigPanel label:contains(${text1})`).click();
+            cy.get("#AxisConfigPanel")
+              .find("button")
+              .contains("Select")
+              .click();
+            cy.get(
+              '#OverallMetricChart div[class*="horizontalAxis"] button:eq(0)'
+            ).contains(text1);
+          });
+      });
+    });
+  });
+}
