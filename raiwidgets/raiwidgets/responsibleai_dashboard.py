f7085c4d22e0d4ee02d1c40501c5c56d5cba72c0
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2022-02-24 02:07:50 -0800

    Add user defined cohort injection logic into raiwidgets (#1237)
    * Add pre-built cohort injection logic into raiwidgets
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * add conftest.py
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Create pytest fixtures raiwidgets tests (#1232)
    
    * Create pytest fixtures raiwidgets tests
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Remove unused imports
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Fix sorted imports
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- raiwidgets/raiwidgets/responsibleai_dashboard.py
+++ raiwidgets/raiwidgets/responsibleai_dashboard.py
@@ -24,10 +24,15 @@ class ResponsibleAIDashboard(Dashboard):
     :param locale: The language in which user wants to load and access the
         ResponsibleAI Dashboard. The default language is english ("en").
     :type locale: str
+    :param cohort_list:
+        List of cohorts defined by the user for the dashboard.
+    :type cohort_list: List[Cohort]
     """
     def __init__(self, analysis: RAIInsights,
-                 public_ip=None, port=None, locale=None):
-        self.input = ResponsibleAIDashboardInput(analysis)
+                 public_ip=None, port=None, locale=None,
+                 cohort_list=None):
+        self.input = ResponsibleAIDashboardInput(
+            analysis, cohort_list=cohort_list)
 
         super(ResponsibleAIDashboard, self).__init__(
             dashboard_type="ResponsibleAI",
--- raiwidgets/raiwidgets/responsibleai_dashboard_input.py
+++ raiwidgets/raiwidgets/responsibleai_dashboard_input.py
@@ -2,13 +2,17 @@
 # Licensed under the MIT License.
 
 import traceback
+from typing import List, Optional
 
+import numpy as np
 import pandas as pd
 
-from erroranalysis._internal.constants import display_name_to_metric
+from erroranalysis._internal.constants import ModelTask, display_name_to_metric
 from responsibleai import RAIInsights
 from responsibleai._input_processing import _convert_to_list
+from responsibleai.exceptions import UserConfigValidationException
 
+from ._cohort import Cohort
 from .constants import ErrorMessages
 from .error_handling import _format_exception
 from .interfaces import WidgetRequestResponseConstants
@@ -20,20 +24,73 @@ EXP_VIZ_ERR_MSG = ErrorMessages.EXP_VIZ_ERR_MSG
 class ResponsibleAIDashboardInput:
     def __init__(
             self,
-            analysis: RAIInsights):
+            analysis: RAIInsights,
+            cohort_list: Optional[List[Cohort]] = None):
         """Initialize the Explanation Dashboard Input.
 
         :param analysis:
             A RAIInsights object that represents an explanation.
         :type analysis: RAIInsights
+        :param cohort_list:
+            List of cohorts defined by the user for the dashboard.
+        :type cohort_list: List[Cohort]
         """
         self._analysis = analysis
         model = analysis.model
         self._is_classifier = _is_classifier(model)
         self.dashboard_input = analysis.get_data()
+
+        self._validate_cohort_list(cohort_list)
+        # Add cohort_list to dashboard_input
+        self.dashboard_input.cohortData = cohort_list
+
         self._feature_length = len(self.dashboard_input.dataset.feature_names)
         self._row_length = len(self.dashboard_input.dataset.features)
         self._error_analyzer = analysis.error_analysis._analyzer
+
+    def _validate_cohort_list(self, cohort_list=None):
+        if cohort_list is None:
+            return
+
+        if not isinstance(cohort_list, list):
+            raise UserConfigValidationException(
+                "cohort_list parameter should be a list.")
+
+        if not all(isinstance(entry, Cohort) for entry in cohort_list):
+            raise UserConfigValidationException(
+                "All entries in cohort_list should be of type Cohort.")
+
+        all_cohort_names = [cohort.name for cohort in cohort_list]
+        unique_cohort_names = np.unique(all_cohort_names).tolist()
+
+        if len(unique_cohort_names) != len(all_cohort_names):
+            raise UserConfigValidationException(
+                "Found cohorts with duplicate names. "
+                "All pre-defined cohorts need to have distinct names.")
+
+        test_data = pd.DataFrame(
+            data=self.dashboard_input.dataset.features,
+            columns=self.dashboard_input.dataset.feature_names)
+        if self.dashboard_input.dataset.task_type == \
+                ModelTask.CLASSIFICATION:
+            class_names_list = self.dashboard_input.dataset.class_names
+            true_y_array = self.dashboard_input.dataset.true_y
+            true_class_array = np.array(
+                [class_names_list[index] for index in true_y_array])
+            test_data[self.dashboard_input.dataset.target_column] = \
+                true_class_array
+        else:
+            test_data[self.dashboard_input.dataset.target_column] = \
+                self.dashboard_input.dataset.true_y
+
+        categorical_features = \
+            self.dashboard_input.dataset.categorical_features
+        for cohort in cohort_list:
+            cohort._validate_with_test_data(
+                test_data=test_data,
+                target_column=self.dashboard_input.dataset.target_column,
+                categorical_features=categorical_features,
+                is_classification=self._is_classifier)
 
     def on_predict(self, data):
         try:
