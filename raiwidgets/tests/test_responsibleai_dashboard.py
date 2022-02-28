274543c4ff7d86ca0524a8de4c7527da4d71ff74
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2022-02-28 07:41:32 -0800

    Add regression test for pre-defined cohorts in raiwidgets (#1249)
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- raiwidgets/tests/test_responsibleai_dashboard.py
+++ raiwidgets/tests/test_responsibleai_dashboard.py
@@ -40,8 +40,9 @@ class TestResponsibleAIDashboard:
         json.dumps(rai_widget.input.dashboard_input,
                    default=serialize_json_safe)
 
-    def test_responsibleai_adult(self, tmpdir, create_rai_insights_object):
-        ri = create_rai_insights_object
+    def test_responsibleai_adult_save_and_load(
+            self, tmpdir, create_rai_insights_object_classification):
+        ri = create_rai_insights_object_classification
 
         widget = ResponsibleAIDashboard(ri)
         self.validate_rai_dashboard_data(widget)
@@ -53,9 +54,73 @@ class TestResponsibleAIDashboard:
         widget_copy = ResponsibleAIDashboard(ri_copy)
         self.validate_rai_dashboard_data(widget_copy)
 
+    def test_responsibleai_housing_save_and_load(
+            self, tmpdir, create_rai_insights_object_regression):
+        ri = create_rai_insights_object_regression
+
+        widget = ResponsibleAIDashboard(ri)
+        self.validate_rai_dashboard_data(widget)
+
+        save_dir = tmpdir.mkdir('save-dir')
+        ri.save(save_dir)
+        ri_copy = ri.load(save_dir)
+
+        widget_copy = ResponsibleAIDashboard(ri_copy)
+        self.validate_rai_dashboard_data(widget_copy)
+
+    def test_responsibleai_housing_with_pre_defined_cohorts(
+            self, create_rai_insights_object_regression):
+        ri = create_rai_insights_object_regression
+
+        cohort_filter_continuous_1 = CohortFilter(
+            method=CohortFilterMethods.METHOD_LESS,
+            arg=[30.5],
+            column='HouseAge')
+        cohort_filter_continuous_2 = CohortFilter(
+            method=CohortFilterMethods.METHOD_GREATER,
+            arg=[3.0],
+            column='AveRooms')
+
+        user_cohort_continuous = Cohort(name='Cohort Continuous')
+        user_cohort_continuous.add_cohort_filter(cohort_filter_continuous_1)
+        user_cohort_continuous.add_cohort_filter(cohort_filter_continuous_2)
+
+        cohort_filter_index = CohortFilter(
+            method=CohortFilterMethods.METHOD_LESS,
+            arg=[20],
+            column='Index')
+
+        user_cohort_index = Cohort(name='Cohort Index')
+        user_cohort_index.add_cohort_filter(cohort_filter_index)
+
+        cohort_filter_predicted_y = CohortFilter(
+            method=CohortFilterMethods.METHOD_LESS,
+            arg=[5.0],
+            column='Predicted Y')
+
+        user_cohort_predicted_y = Cohort(name='Cohort Predicted Y')
+        user_cohort_predicted_y.add_cohort_filter(cohort_filter_predicted_y)
+
+        cohort_filter_true_y = CohortFilter(
+            method=CohortFilterMethods.METHOD_GREATER,
+            arg=[1.0],
+            column='True Y')
+
+        user_cohort_true_y = Cohort(name='Cohort True Y')
+        user_cohort_true_y.add_cohort_filter(cohort_filter_true_y)
+
+        widget = ResponsibleAIDashboard(
+            ri,
+            cohort_list=[user_cohort_continuous,
+                         user_cohort_index,
+                         user_cohort_predicted_y,
+                         user_cohort_true_y])
+
+        self.validate_rai_dashboard_data(widget)
+
     def test_responsibleai_adult_with_pre_defined_cohorts(
-            self, create_rai_insights_object):
-        ri = create_rai_insights_object
+            self, create_rai_insights_object_classification):
+        ri = create_rai_insights_object_classification
 
         cohort_filter_continuous_1 = CohortFilter(
             method=CohortFilterMethods.METHOD_LESS,
@@ -95,8 +160,8 @@ class TestResponsibleAIDashboard:
         self.validate_rai_dashboard_data(widget)
 
     def test_responsibleai_adult_with_ill_defined_cohorts(
-            self, create_rai_insights_object):
-        ri = create_rai_insights_object
+            self, create_rai_insights_object_classification):
+        ri = create_rai_insights_object_classification
 
         cohort_filter_continuous_1 = CohortFilter(
             method=CohortFilterMethods.METHOD_LESS,
@@ -123,8 +188,8 @@ class TestResponsibleAIDashboard:
                 ri, cohort_list=[user_cohort_continuous, {}])
 
     def test_responsibleai_adult_duplicate_cohort_names(
-            self, create_rai_insights_object):
-        ri = create_rai_insights_object
+            self, create_rai_insights_object_classification):
+        ri = create_rai_insights_object_classification
 
         cohort_filter_continuous_1 = CohortFilter(
             method=CohortFilterMethods.METHOD_LESS,
