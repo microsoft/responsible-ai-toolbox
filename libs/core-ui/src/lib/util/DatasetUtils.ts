837edc31e352fae39a87553ffc77ce7aa419faae
Roman Lutz <romanlutz13@gmail.com>
2022-03-15 21:35:30 -0400

    lintfix

--- libs/core-ui/src/lib/util/DatasetUtils.ts
+++ libs/core-ui/src/lib/util/DatasetUtils.ts
@@ -79,60 +79,60 @@ export function constructCols(
 ): IColumn[] {
   const columns: IColumn[] = [];
   columns.push({
+    columnActionsMode: ColumnActionsMode.disabled,
     fieldName: "0",
     isResizable: true,
     key: "column0",
     maxWidth: 100,
     minWidth: 50,
-    name: "Index",
-    columnActionsMode: ColumnActionsMode.disabled
+    name: "Index"
   });
   let index = 1;
   if (hasColors) {
     columns.push({
+      columnActionsMode: ColumnActionsMode.disabled,
       fieldName: `${index}`,
       isResizable: true,
       key: "color",
       maxWidth: 100,
       minWidth: 50,
-      name: "Color",
-      columnActionsMode: ColumnActionsMode.disabled
+      name: "Color"
     });
     index++;
   }
   if (!isCustomPointsView && jointDataset.hasTrueY) {
     columns.push({
+      columnActionsMode: ColumnActionsMode.disabled,
       fieldName: `${index}`,
       isResizable: true,
       key: `column${index}`,
       maxWidth: 100,
       minWidth: 50,
-      name: "TrueY",
-      columnActionsMode: ColumnActionsMode.disabled
+      name: "TrueY"
     });
     index++;
   }
   if (!isCustomPointsView && jointDataset.hasPredictedY) {
     columns.push({
+      columnActionsMode: ColumnActionsMode.disabled,
       fieldName: `${index}`,
       isResizable: true,
       key: `column${index}`,
       maxWidth: 100,
       minWidth: 50,
-      name: "PredictedY",
-      columnActionsMode: ColumnActionsMode.disabled
+      name: "PredictedY"
     });
     index++;
   }
   for (let i = 0; i < viewedCols; i++) {
     columns.push({
+      columnActionsMode: ColumnActionsMode.disabled,
       fieldName: `${index}`,
       isResizable: true,
       key: `column${index}`,
       maxWidth: 200,
       minWidth: 100,
-      name: featureNames[i],
-      columnActionsMode: ColumnActionsMode.disabled
+      name: featureNames[i]
     });
     index += 1;
   }
