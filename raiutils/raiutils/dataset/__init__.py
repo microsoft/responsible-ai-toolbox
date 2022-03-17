46e025912f4db5ddf27a72303941f34bf3170389
Ilya Matiach <ilmat@microsoft.com>
2022-03-17 10:48:27 -0400

    add new RAI Utils package for common utilities shared across RAI packages

---
+++ raiutils/raiutils/dataset/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+"""Module for defining common utilities related to datasets."""
+from .dataset import fetch_dataset
+
+__all__ = ['fetch_dataset']
