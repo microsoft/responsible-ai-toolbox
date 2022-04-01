80e71f73868e31b8e6d16527c677ea87e4a46008
tongyu-microsoft <tongy@microsoft.com>
2022-04-01 10:45:17 -0700

    add error message for counterfactual

--- libs/core-ui/src/lib/Interfaces/ICounterfactualData.ts
+++ libs/core-ui/src/lib/Interfaces/ICounterfactualData.ts
@@ -3,6 +3,7 @@
 
 export interface ICounterfactualData {
   cfs_list: Array<Array<Array<string | number>>>;
+  error_message?: string;
   feature_names: string[];
   feature_names_including_target: string[];
   summary_importance?: number[];
--- libs/counterfactuals/src/lib/CounterfactualPanel.tsx
+++ libs/counterfactuals/src/lib/CounterfactualPanel.tsx
@@ -21,7 +21,9 @@ import {
   TooltipDelay,
   DirectionalHint,
   IconButton,
-  ITooltipProps
+  ITooltipProps,
+  MessageBar,
+  MessageBarType
 } from "office-ui-fabric-react";
 import React from "react";
 
@@ -81,6 +83,13 @@ export class CounterfactualPanel extends React.Component<
         onRenderFooterContent={this.renderClose}
       >
         <Stack tokens={{ childrenGap: "m1" }}>
+          <Stack.Item>
+            {this.props.data?.error_message && (
+              <MessageBar messageBarType={MessageBarType.error}>
+                {this.props.data.error_message}
+              </MessageBar>
+            )}
+          </Stack.Item>
           <Stack.Item className={classes.counterfactualList}>
             <CounterfactualList
               selectedIndex={this.props.selectedIndex}
