d217a2795fefd498469a94fd6d58fa8c7afb204e
Roman Lutz <romanlutz13@gmail.com>
2022-04-04 02:40:55 -0400

    Add a feature flag for the new model overview experience (#1306)
    * add feature flag for new model overview experience and turn it off by default
    
    * remove useless constructor

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/TabsView/TabsView.tsx
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/TabsView/TabsView.tsx
@@ -153,7 +153,7 @@ export class TabsView extends React.PureComponent<
                       }
                     </Text>
                   </div>
-                  <ModelOverview />
+                  <ModelOverview showNewModelOverviewExperience={false} />
                 </>
               )}
               {t.key === GlobalTabKeys.DataExplorerTab && (
