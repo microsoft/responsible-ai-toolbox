876c9c2a8688c365c064ad6c85776641317245c9
Roman Lutz <romanlutz13@gmail.com>
2022-04-04 11:10:33 -0400

    refactor heatmap code into a common class

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/DatasetCohortStatsTable.tsx
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/DatasetCohortStatsTable.tsx
@@ -3,15 +3,10 @@
 
 import {
   defaultModelAssessmentContext,
-  ModelAssessmentContext,
-  HeatmapHighChart,
-  JointDataset,
-  generateMetrics
-} from "@responsible-ai/core-ui";
-import { localization } from "@responsible-ai/localization";
+  ModelAssessmentContext} from "@responsible-ai/core-ui";
 import { IDropdownOption } from "office-ui-fabric-react";
 import React from "react";
-import { generateCohortsStatsTable, wrapYAxisLabels } from "./StatsTableUtils";
+import { CohortStatsHeatmap } from "./CohortStatsHeatmap";
 
 interface IDatasetCohortStatsTableProps {
   selectableMetrics: IDropdownOption[];
@@ -28,122 +23,12 @@ export class DatasetCohortStatsTable extends React.Component<
   public context: React.ContextType<typeof ModelAssessmentContext> =
     defaultModelAssessmentContext;
 
-  constructor(props: IDatasetCohortStatsTableProps) {
-    super(props);
-    this.state = {
-      selectedMetrics: [],
-      isFeaturePickerLimitExceededDialogOpen: false
-    };
-  }
-
   public render(): React.ReactNode {
-    const columns: string[] = [
-      localization.ModelAssessment.ModelOverview.countColumnHeader
-    ];
-    columns.push(
-      ...this.props.selectableMetrics
-        .filter((element) =>
-          this.props.selectedMetrics.includes(element.key.toString())
-        )
-        .map((element) => {
-          return element.text;
-        })
-    );
-
-    // generate table contents for dataset cohorts
-    const datasetCohortLabeledStatistics = generateMetrics(
-      this.context.jointDataset,
-      this.context.errorCohorts.map((errorCohort) =>
-        errorCohort.cohort.unwrap(JointDataset.IndexLabel)
-      ),
-      this.context.modelMetadata.modelType
-    );
-
-    let datasetCohortItems = generateCohortsStatsTable(
-      this.context.errorCohorts,
-      this.props.selectableMetrics,
-      datasetCohortLabeledStatistics,
-      this.props.selectedMetrics
-    );
-
     return (
-      <HeatmapHighChart
-        configOverride={{
-          chart: { type: "heatmap", spacingLeft: 50 },
-          title: {
-            text: localization.ModelAssessment.ModelOverview
-              .dataCohortsHeatmapHeader,
-            align: "left"
-          },
-          xAxis: {
-            categories: columns,
-            opposite: true
-          },
-          yAxis: {
-            categories: this.context.errorCohorts.map(
-              (errorCohort) => errorCohort.cohort.name
-            ),
-            labels: {
-              align: "left",
-              reserveSpace: true,
-              // format labels to cap the line length at 20 characters
-              formatter: function () {
-                return wrapYAxisLabels(this.value, true);
-              }
-            }
-          },
-          series: [
-            {
-              name: "Metrics",
-              colorKey: "colorValue",
-              data: datasetCohortItems,
-              type: "heatmap",
-              dataLabels: {
-                enabled: true
-              },
-              borderWidth: 1
-            }
-          ],
-          colorAxis: {
-            min: 0,
-            max: 1,
-            minColor: "#FFFFFF",
-            maxColor: "#1634F6"
-          },
-          legend: {
-            enabled: false
-          },
-          tooltip: {
-            formatter: function () {
-              if (
-                this.point.y === undefined ||
-                this.point.value === undefined
-              ) {
-                return undefined;
-              }
-              if (this.point.x === 0) {
-                // Count column
-                return (
-                  "Cohort " +
-                  this.series["yAxis"].categories[this.point.y] +
-                  " contains " +
-                  this.point.value +
-                  " instances."
-                );
-              } else {
-                // Metric columns
-                return (
-                  "The model's " +
-                  this.series["xAxis"].categories[this.point.x].toLowerCase() +
-                  " on cohort " +
-                  this.series["yAxis"].categories[this.point.y] +
-                  " is " +
-                  this.point.value
-                );
-              }
-            }
-          }
-        }}
+      <CohortStatsHeatmap
+        cohorts={this.context.errorCohorts}
+        selectableMetrics={this.props.selectableMetrics}
+        selectedMetrics={this.props.selectedMetrics}
       />
     );
   }
--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/DisaggregatedAnalysisTable.tsx
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/DisaggregatedAnalysisTable.tsx
@@ -4,10 +4,8 @@
 import {
   defaultModelAssessmentContext,
   ModelAssessmentContext,
-  HeatmapHighChart,
-  JointDataset,
-  generateMetrics,
-  ErrorCohort} from "@responsible-ai/core-ui";
+  ErrorCohort
+} from "@responsible-ai/core-ui";
 import { localization } from "@responsible-ai/localization";
 import {
   ActionButton,
@@ -15,8 +13,8 @@ import {
   IDropdown
 } from "office-ui-fabric-react";
 import React from "react";
+import { CohortStatsHeatmap } from "./CohortStatsHeatmap";
 
-import { generateCohortsStatsTable, wrapYAxisLabels } from "./StatsTableUtils";
 
 interface IDisaggregatedAnalysisTableProps {
   selectableMetrics: IDropdownOption[];
@@ -25,7 +23,7 @@ interface IDisaggregatedAnalysisTableProps {
   featureBasedCohorts: ErrorCohort[];
 }
 
-interface IDisaggregatedAnalysisTableState {}
+class IDisaggregatedAnalysisTableState {}
 
 export class DisaggregatedAnalysisTable extends React.Component<
   IDisaggregatedAnalysisTableProps,
@@ -36,140 +34,26 @@ export class DisaggregatedAnalysisTable extends React.Component<
     defaultModelAssessmentContext;
   private featureDropdownRef = React.createRef<IDropdown>();
 
-  constructor(props: IDisaggregatedAnalysisTableProps) {
-    super(props);
-    this.state = {
-      selectedMetrics: [],
-      selectedFeatures: [],
-      isFeaturePickerLimitExceededDialogOpen: false
-    };
-  }
-
   public render(): React.ReactNode {
-    const columns: string[] = [
-      localization.ModelAssessment.ModelOverview.countColumnHeader
-    ];
-    columns.push(
-      ...this.props.selectableMetrics
-        .filter((element) =>
-          this.props.selectedMetrics.includes(element.key.toString())
-        )
-        .map((element) => {
-          return element.text;
-        })
-    );
-
-    const featureBasedCohortLabeledStatistics = generateMetrics(
-      this.context.jointDataset,
-      this.props.featureBasedCohorts.map((errorCohort) =>
-        errorCohort.cohort.unwrap(JointDataset.IndexLabel)
-      ),
-      this.context.modelMetadata.modelType
-    );
-
-    let featureBasedCohortItems = generateCohortsStatsTable(
-      this.props.featureBasedCohorts,
-      this.props.selectableMetrics,
-      featureBasedCohortLabeledStatistics,
-      this.props.selectedMetrics
-    );
-
     return (
       <>
         {this.props.selectedFeatures.length === 0 && (
           <ActionButton
-            styles={{}}
             onClick={() => {
               this.featureDropdownRef.current?.focus(true);
             }}
           >
-            Select features and fairness metrics to generate the disaggregated
-            analysis.
+            {
+              localization.ModelAssessment.ModelOverview
+                .disaggregatedAnalysisFeatureSelectionPlaceholder
+            }
           </ActionButton>
         )}
         {this.props.selectedFeatures.length > 0 && (
-          <HeatmapHighChart
-            configOverride={{
-              chart: {
-                type: "heatmap",
-                height: this.props.featureBasedCohorts.length * 40 + 120
-              },
-              title: {
-                text: localization.ModelAssessment.ModelOverview
-                  .disaggregatedAnalysisHeatmapHeader,
-                align: "left"
-              },
-              xAxis: {
-                categories: columns,
-                opposite: true
-              },
-              yAxis: {
-                categories: this.props.featureBasedCohorts.map(
-                  (errorCohort) => errorCohort.cohort.name
-                ),
-                labels: {
-                  align: "left",
-                  reserveSpace: true,
-                  // format labels to cap the line length at 20 characters
-                  formatter: function () {
-                    return wrapYAxisLabels(this.value, false);
-                  }
-                }
-              },
-              series: [
-                {
-                  name: "Metrics",
-                  colorKey: "colorValue",
-                  data: featureBasedCohortItems,
-                  type: "heatmap",
-                  dataLabels: {
-                    enabled: true
-                  },
-                  borderWidth: 1
-                }
-              ],
-              colorAxis: {
-                min: 0,
-                max: 1,
-                minColor: "#FFFFFF",
-                maxColor: "#1634F6"
-              },
-              legend: {
-                enabled: false
-              },
-              tooltip: {
-                formatter: function () {
-                  if (
-                    this.point.y === undefined ||
-                    this.point.value === undefined
-                  ) {
-                    return undefined;
-                  }
-                  if (this.point.x === 0) {
-                    // Count column
-                    return (
-                      "Cohort " +
-                      this.series["yAxis"].categories[this.point.y] +
-                      " contains " +
-                      this.point.value +
-                      " instances."
-                    );
-                  } else {
-                    // Metric columns
-                    return (
-                      "The model's " +
-                      this.series["xAxis"].categories[
-                        this.point.x
-                      ].toLowerCase() +
-                      " on cohort " +
-                      this.series["yAxis"].categories[this.point.y] +
-                      " is " +
-                      this.point.value
-                    );
-                  }
-                }
-              }
-            }}
+          <CohortStatsHeatmap
+            cohorts={this.props.featureBasedCohorts}
+            selectableMetrics={this.props.selectableMetrics}
+            selectedMetrics={this.props.selectedMetrics}
           />
         )}
       </>
