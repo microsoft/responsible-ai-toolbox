4ebc275b4cdab233bf62069ffa16b663baed7e79
Roman Lutz <romanlutz13@gmail.com>
2022-04-06 02:35:54 -0400

    Add dataset cohort table to new ModelOverview experience (#1314)
    * add only dataset cohort table, update wrapping code
    
    * lintfix
    
    * lintfix
    
    * build error fix

---
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/StatsTableUtils.ts
@@ -0,0 +1,116 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { ErrorCohort, ILabeledStatistic } from "@responsible-ai/core-ui";
+import { PointOptionsObject } from "highcharts";
+import { IDropdownOption } from "office-ui-fabric-react";
+
+export function generateCohortsStatsTable(
+  cohorts: ErrorCohort[],
+  selectableMetrics: IDropdownOption[],
+  labeledStatistics: ILabeledStatistic[][],
+  selectedMetrics: string[]
+) {
+  const items: PointOptionsObject[] = cohorts.map(
+    (errorCohort, cohortIndex) => {
+      return {
+        colorValue: 0,
+        value: errorCohort.cohortStats.totalCohort,
+
+        x: 0,
+        // metric index for Count column
+        y: cohortIndex
+      };
+    }
+  );
+  selectableMetrics
+    .filter((element: IDropdownOption) =>
+      selectedMetrics.includes(element.key.toString())
+    )
+    .forEach((metricOption: IDropdownOption, metricIndex: number) => {
+      // first determine min and max values
+      let metricMin = Number.MAX_SAFE_INTEGER;
+      let metricMax = Number.MIN_SAFE_INTEGER;
+      cohorts.forEach((_errorCohort, cohortIndex) => {
+        const stat = labeledStatistics[cohortIndex].find(
+          (stat) => stat.key === metricOption.key
+        );
+        if (stat) {
+          if (stat.stat > metricMax) {
+            metricMax = stat.stat;
+          }
+          if (stat.stat < metricMin) {
+            metricMin = stat.stat;
+          }
+        }
+      });
+      // use min and max to normalize the colors
+      const metricMinMaxDiff = metricMax - metricMin;
+      cohorts.forEach((_errorCohort, cohortIndex) => {
+        const stat = labeledStatistics[cohortIndex].find(
+          (stat) => stat.key === metricOption.key
+        );
+        if (stat && !Number.isNaN(stat.stat)) {
+          items.push({
+            colorValue: (stat.stat - metricMin) / metricMinMaxDiff,
+            value: Number(stat.stat.toFixed(3)),
+            x: metricIndex + 1,
+            y: cohortIndex
+          });
+        } else {
+          // not a numeric value (NaN), so just put null
+          items.push({
+            color: "#808080",
+            value: Number.NaN,
+            x: metricIndex + 1,
+            y: cohortIndex // gray
+          });
+        }
+      });
+    });
+  return items;
+}
+
+export function wrapYAxisLabels(label: string, wrapOnWhitespace = false) {
+  const maxLineLength = 40;
+  const maxLines = 2;
+  const lineWrapHtmlTag = "<br />";
+  const ellipsis = "...";
+
+  // check if there are suitable spots for a linewrap
+  // if not just wrap after maxLineLength characters
+  let slicingIndex = maxLineLength;
+  const closingParenthesis = ") ";
+  const openingParenthesis = " (";
+  const whitespace = " ";
+  const searchString = new Set(
+    label.slice(slicingIndex - maxLineLength / 2, slicingIndex)
+  );
+
+  if (searchString.has(closingParenthesis)) {
+    // option 1: wrap after closing parenthesis
+    slicingIndex =
+      label.indexOf(closingParenthesis, slicingIndex - maxLineLength / 2) + 2;
+  } else if (searchString.has(openingParenthesis)) {
+    // option 2: wrap before opening parenthesis
+    slicingIndex =
+      label.indexOf(openingParenthesis, slicingIndex - maxLineLength / 2) + 1;
+  } else if (wrapOnWhitespace && searchString.has(whitespace)) {
+    // option 3: wrap after maxLineLength characters
+    slicingIndex =
+      label.indexOf(whitespace, slicingIndex - maxLineLength / 2) + 1;
+  }
+  label =
+    label.slice(0, slicingIndex) +
+    lineWrapHtmlTag +
+    label.slice(slicingIndex, label.length);
+
+  if (label.length > maxLineLength * maxLines + lineWrapHtmlTag.length) {
+    label =
+      label.slice(
+        0,
+        maxLineLength * maxLines - ellipsis.length + lineWrapHtmlTag.length
+      ) + ellipsis;
+  }
+  return label;
+}
