ce99f55c7d836c273c4f7ad528982537b29d9fe4
Ilya Matiach <ilmat@microsoft.com>
2022-04-07 10:13:47 -0400

    Disable save as new cohort button if nothing is selected in error tree

---
+++ apps/widget-e2e/src/integration/modelAssessment/responsibleaitoolboxMulticlassDnnModelDebugging/errorAnalysis.spec.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { describeErrorAnalysis } from "../../../describer/modelAssessment/errorAnalysis/describeErrorAnalysis";
+
+describeErrorAnalysis("MulticlassDnnModelDebugging");
