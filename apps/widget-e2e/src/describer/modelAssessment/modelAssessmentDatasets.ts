daae911964d0c3dd5b162a571d6aca394cc5859a
Ilya Matiach <ilmat@microsoft.com>
2022-04-11 12:27:31 -0400

    Disable save as new cohort button if nothing is selected in error tree (#1327)

--- apps/widget-e2e/src/describer/modelAssessment/modelAssessmentDatasets.ts
+++ apps/widget-e2e/src/describer/modelAssessment/modelAssessmentDatasets.ts
@@ -24,6 +24,9 @@ const modelAssessmentDatasets = {
       defaultXAxis: "Index",
       defaultYAxis: "age"
     },
+    errorAnalysisData: {
+      hasErrorAnalysisComponent: true
+    },
     featureImportanceData: {
       datapoint: 500,
       dropdownRowName: "Row 34",
@@ -92,6 +95,9 @@ const modelAssessmentDatasets = {
       defaultXAxis: "Index",
       defaultYAxis: "age"
     },
+    errorAnalysisData: {
+      hasErrorAnalysisComponent: false
+    },
     featureImportanceData: {
       datapoint: 89,
       hasFeatureImportanceComponent: false
@@ -144,6 +150,9 @@ const modelAssessmentDatasets = {
       datasetBarLabel: ["0 - 17", "18 - 35", "36 - 52", "53 - 70", "71 - 88"],
       defaultXAxis: "Index",
       defaultYAxis: "age"
+    },
+    errorAnalysisData: {
+      hasErrorAnalysisComponent: true
     },
     featureImportanceData: {
       datapoint: 89,
@@ -212,6 +221,9 @@ const modelAssessmentDatasets = {
       ],
       defaultXAxis: "Index",
       defaultYAxis: "LotFrontage"
+    },
+    errorAnalysisData: {
+      hasErrorAnalysisComponent: true
     },
     featureImportanceData: {
       datapoint: 730,
@@ -311,6 +323,9 @@ const modelAssessmentDatasets = {
       defaultXAxis: "Index",
       defaultYAxis: "LotFrontage"
     },
+    errorAnalysisData: {
+      hasErrorAnalysisComponent: false
+    },
     featureImportanceData: {
       hasFeatureImportanceComponent: false
     },
@@ -367,6 +382,9 @@ const modelAssessmentDatasets = {
       datasetBarLabel: ["0 - 17", "18 - 35", "36 - 52", "53 - 70", "71 - 88"],
       defaultXAxis: "Index",
       defaultYAxis: "alcohol"
+    },
+    errorAnalysisData: {
+      hasErrorAnalysisComponent: true
     },
     featureImportanceData: {
       datapoint: 500,
