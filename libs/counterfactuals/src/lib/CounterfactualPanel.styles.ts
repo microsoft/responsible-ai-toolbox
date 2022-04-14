ec5e9ab902538c5f6cde8df4a8938c450de05647
vinutha karanth <vinutha.karanth@gmail.com>
2022-04-14 13:22:21 -0700

    lintfix
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>

--- libs/counterfactuals/src/lib/CounterfactualPanel.styles.ts
+++ libs/counterfactuals/src/lib/CounterfactualPanel.styles.ts
@@ -90,10 +90,10 @@ export const counterfactualPanelStyles: () => IProcessedStyleSet<ICounterfactual
             paddingLeft: 24,
             paddingRight: 24
           },
-          ".scrollableContent": { height: "100%", paddingTop: 1 },
           ".ms-Panel-navigation": {
             justifyContent: "space-between"
-          }
+          },
+          ".scrollableContent": { height: "100%", paddingTop: 1 }
         }
       },
       positiveNumber: {
