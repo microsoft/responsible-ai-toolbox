c965c21684b50ac42098853a8f9f1e6780e916e6
Roman Lutz <romanlutz13@gmail.com>
2022-04-15 23:55:53 -0400

    add a few unit tests

--- libs/core-ui/src/lib/util/calculateBoxData.ts
+++ libs/core-ui/src/lib/util/calculateBoxData.ts
@@ -19,38 +19,52 @@ export function calculateBoxPlotDataFromErrorCohort(
 export function calculateBoxPlotData(
   data: number[],
   index?: number
-): IHighchartBoxData {
+): IHighchartBoxData | undefined {
   data.sort((number1: number, number2: number) => {
     return number1 - number2;
   });
+  if (data.length === 0) {
+    return undefined;
+  }
   const firstQuartile = getPercentile(data, 25);
   const median = getPercentile(data, 50);
   const thirdQuartile = getPercentile(data, 75);
-  const interquartileRange = thirdQuartile - firstQuartile;
-  // calculate fences as min and max allowed values to be inside the box
-  const lowerFence = firstQuartile - interquartileRange * 1.5;
-  const upperFence = thirdQuartile + interquartileRange * 1.5;
-  const nonOutliers = data.filter(
-    (element) => element >= lowerFence && element <= upperFence
-  );
-  const outliers = data.filter(
-    (element) => element < lowerFence || element > upperFence
-  );
-  return {
-    high: nonOutliers[nonOutliers.length - 1],
-    low: nonOutliers[0],
-    median,
-    outliers,
-    q1: firstQuartile,
-    q3: thirdQuartile,
-    x: index
-  };
+  if (
+    firstQuartile !== undefined &&
+    median !== undefined &&
+    thirdQuartile !== undefined
+  ) {
+    const interquartileRange = thirdQuartile - firstQuartile;
+    // calculate fences as min and max allowed values to be inside the box
+    const lowerFence = firstQuartile - interquartileRange * 1.5;
+    const upperFence = thirdQuartile + interquartileRange * 1.5;
+    const nonOutliers = data.filter(
+      (element) => element >= lowerFence && element <= upperFence
+    );
+    const outliers = data.filter(
+      (element) => element < lowerFence || element > upperFence
+    );
+    return {
+      high: nonOutliers[nonOutliers.length - 1],
+      low: nonOutliers[0],
+      median,
+      outliers,
+      q1: firstQuartile,
+      q3: thirdQuartile,
+      x: index
+    };
+  }
+  return undefined;
 }
 
-function getPercentile(sortedData: number[], percentile: number) {
+export function getPercentile(sortedData: number[], percentile: number) {
+  if (percentile <= 0 || percentile >= 100 || sortedData.length === 0) {
+    return undefined;
+  }
   const index = (percentile / 100) * sortedData.length;
   let result;
   if (Math.floor(index) === index) {
+    // take average of the two adjacent numbers
     result = (sortedData[index - 1] + sortedData[index]) / 2;
   } else {
     result = sortedData[Math.floor(index)];
