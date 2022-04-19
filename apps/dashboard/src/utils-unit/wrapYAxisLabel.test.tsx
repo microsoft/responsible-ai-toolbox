495f3af9af9c15758334712c78e3208299dfbb20
Roman Lutz <romanlutz13@gmail.com>
2022-04-18 21:20:48 -0400

    fix test case

--- apps/dashboard/src/utils-unit/wrapYAxisLabel.test.tsx
+++ apps/dashboard/src/utils-unit/wrapYAxisLabel.test.tsx
@@ -11,7 +11,7 @@ describe("wrapYAxisLabels", () => {
     ${"This is a test description that's meant to be wrapped"}                                                    | ${"This is a test description that's meant<br /> to be wrapped"}
     ${"Not this one, though!"}                                                                                    | ${"Not this one, though!"}
     ${"Thisone,ontheotherhand,doesn'treallyofferanywhitespacewrapoptionsoitjustwraps"}                            | ${"Thisone,ontheotherhand,doesn'treallyoffe<br />ranywhitespacewrapoptionsoitjustwraps"}
-    ${"Then again, some descriptions are so long that we really need to cut them off to avoid overlapping text."} | ${"Then again, some descriptions are so<br/> long that we really need to cut them off..."}
+    ${"Then again, some descriptions are so long that we really need to cut them off to avoid overlapping text."} | ${"Then again, some descriptions are so<br /> long that we really need to cut them off..."}
   `("should wrap text as expected", ({ input, expectedResult }) => {
     expect(wrapYAxisLabels(input)).toBe(expectedResult);
   });
