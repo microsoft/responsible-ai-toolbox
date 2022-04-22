1568f94b911ea406908cb51989e8eaefeb98e29f
Roman Lutz <romanlutz13@gmail.com>
2022-04-22 15:57:41 -0400

    Add a summary for disaggregated analysis showing ratio and difference (#1367)
    * add fairness metric table, update existing tables (order, N/A values, grid axis label lengths)
    
    * lintfix
    
    * merge fixes
    
    * addressed eslint issue for null value by creating a dedicated file, localized N/A, updated tooltip text to be wrapped (+ associated unit tests)
    
    * keep feature flag

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/DatasetCohortStatsTable.tsx
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/DatasetCohortStatsTable.tsx
@@ -3,12 +3,15 @@
 
 import {
   defaultModelAssessmentContext,
+  generateMetrics,
+  JointDataset,
   ModelAssessmentContext
 } from "@responsible-ai/core-ui";
 import { IDropdownOption } from "office-ui-fabric-react";
 import React from "react";
 
 import { CohortStatsHeatmap } from "./CohortStatsHeatmap";
+import { generateCohortsStatsTable } from "./StatsTableUtils";
 
 interface IDatasetCohortStatsTableProps {
   selectableMetrics: IDropdownOption[];
@@ -26,8 +29,25 @@ export class DatasetCohortStatsTable extends React.Component<
     defaultModelAssessmentContext;
 
   public render(): React.ReactNode {
+    // generate table contents
+    const cohortLabeledStatistics = generateMetrics(
+      this.context.jointDataset,
+      this.context.errorCohorts.map((errorCohort) =>
+        errorCohort.cohort.unwrap(JointDataset.IndexLabel)
+      ),
+      this.context.modelMetadata.modelType
+    );
+
+    const items = generateCohortsStatsTable(
+      this.context.errorCohorts,
+      this.props.selectableMetrics,
+      cohortLabeledStatistics,
+      this.props.selectedMetrics
+    ).items;
+
     return (
       <CohortStatsHeatmap
+        items={items}
         cohorts={this.context.errorCohorts}
         selectableMetrics={this.props.selectableMetrics}
         selectedMetrics={this.props.selectedMetrics}
--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/StatsTableUtils.ts
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/StatsTableUtils.ts
@@ -5,6 +5,7 @@ import {
   BinaryClassificationMetrics,
   classificationTask,
   ErrorCohort,
+  HighchartsNull,
   ILabeledStatistic,
   RegressionMetrics
 } from "@responsible-ai/core-ui";
@@ -12,12 +13,24 @@ import { localization } from "@responsible-ai/localization";
 import { PointOptionsObject } from "highcharts";
 import { IDropdownOption } from "office-ui-fabric-react";
 
+export interface IFairnessStats {
+  max: number;
+  min: number;
+  maxCohortName: string;
+  minCohortName: string;
+  difference: number;
+  ratio: number | undefined;
+}
+
 export function generateCohortsStatsTable(
   cohorts: ErrorCohort[],
   selectableMetrics: IDropdownOption[],
   labeledStatistics: ILabeledStatistic[][],
   selectedMetrics: string[]
 ) {
+  // The "count" metric has to be treated separately
+  // since it's not handled like other metrics, but
+  // is part of the ErrorCohort object.
   const items: PointOptionsObject[] = cohorts.map(
     (errorCohort, cohortIndex) => {
       return {
@@ -30,6 +43,31 @@ export function generateCohortsStatsTable(
       };
     }
   );
+  let countMax = Number.MIN_SAFE_INTEGER;
+  let countMin = Number.MAX_SAFE_INTEGER;
+  let countMinCohortName = "";
+  let countMaxCohortName = "";
+  cohorts.forEach((errorCohort) => {
+    const cohortCount = errorCohort.cohortStats.totalCohort;
+    if (cohortCount > countMax) {
+      countMax = cohortCount;
+      countMaxCohortName = errorCohort.cohort.name;
+    }
+    if (cohortCount < countMin) {
+      countMin = cohortCount;
+      countMinCohortName = errorCohort.cohort.name;
+    }
+  });
+  const fairnessStats: IFairnessStats[] = [
+    {
+      difference: countMax - countMin,
+      max: countMax,
+      maxCohortName: countMaxCohortName,
+      min: countMin,
+      minCohortName: countMinCohortName,
+      ratio: countMin / countMax
+    }
+  ];
   selectableMetrics
     .filter((element: IDropdownOption) =>
       selectedMetrics.includes(element.key.toString())
@@ -37,22 +75,39 @@ export function generateCohortsStatsTable(
     .forEach((metricOption: IDropdownOption, metricIndex: number) => {
       // first determine min and max values
       let metricMin = Number.MAX_SAFE_INTEGER;
+      let metricMinCohortName = "";
       let metricMax = Number.MIN_SAFE_INTEGER;
-      cohorts.forEach((_errorCohort, cohortIndex) => {
+      let metricMaxCohortName = "";
+      cohorts.forEach((errorCohort, cohortIndex) => {
         const labeledStat = labeledStatistics[cohortIndex].find(
           (labeledStat) => labeledStat.key === metricOption.key
         );
         if (labeledStat) {
           if (labeledStat.stat > metricMax) {
             metricMax = labeledStat.stat;
+            metricMaxCohortName = errorCohort.cohort.name;
           }
           if (labeledStat.stat < metricMin) {
             metricMin = labeledStat.stat;
+            metricMinCohortName = errorCohort.cohort.name;
           }
         }
       });
       // use min and max to normalize the colors
       const metricMinMaxDiff = metricMax - metricMin;
+      let metricMinMaxRatio: number | undefined = undefined;
+      if (metricMax !== 0) {
+        metricMinMaxRatio = metricMin / metricMax;
+      }
+      const metricFairnessStats: IFairnessStats = {
+        difference: metricMinMaxDiff,
+        max: metricMax,
+        maxCohortName: metricMaxCohortName,
+        min: metricMin,
+        minCohortName: metricMinCohortName,
+        ratio: metricMinMaxRatio
+      };
+      fairnessStats.push(metricFairnessStats);
       cohorts.forEach((_errorCohort, cohortIndex) => {
         const labeledStat = labeledStatistics[cohortIndex].find(
           (labeledStat) => labeledStat.key === metricOption.key
@@ -83,56 +138,75 @@ export function generateCohortsStatsTable(
                 width: 10
               }
             },
-            value: Number.NaN,
+            // null is treated as a special value by highcharts
+            // sadly there's no alternative (undefined doesn't work)
+            value: HighchartsNull,
             x: metricIndex + 1,
             y: cohortIndex
           });
         }
       });
     });
-  return items;
+  return { fairnessStats, items };
 }
 
-export function wrapYAxisLabels(label: string, wrapOnWhitespace = true) {
-  const maxLineLength = 40;
-
-  if (label.length <= maxLineLength) {
-    // label is short enough to fit on one line
-    return label;
+export function wrapText(
+  text: string,
+  maxLineLength = 40,
+  maxLines = 2,
+  lineStart = 0,
+  currentLine = 0
+) {
+  if (text.length <= lineStart + maxLineLength) {
+    // label is short enough to fit on current line
+    return text;
   }
 
-  const maxLines = 2;
+  const wrapStartingPosition = lineStart + maxLineLength / 2;
   const lineWrapHtmlTag = "<br />";
   const ellipsis = "...";
+
+  if (currentLine === maxLines - 1 && text.length > lineStart + maxLineLength) {
+    return (
+      text.slice(0, lineStart + maxLineLength - ellipsis.length) + ellipsis
+    );
+  }
 
   // check if there are suitable spots for a linewrap
   // if not just wrap after maxLineLength characters
   // consider suitable wrapping spots based on whitespace
-  // starting at half the line length
-  const startingPosition = maxLineLength / 2;
-  let slicingIndex = maxLineLength;
+  let slicingIndex = lineStart + maxLineLength;
   const whitespace = " ";
 
-  if (wrapOnWhitespace) {
-    // find last whitespace in the first line
-    for (let index = maxLineLength - 1; index >= startingPosition; index -= 1) {
-      if (label[index] === whitespace) {
-        slicingIndex = index;
-        break;
-      }
+  // find last whitespace in the current line
+  for (
+    let index = lineStart + maxLineLength - 1;
+    index >= wrapStartingPosition;
+    index -= 1
+  ) {
+    if (text[index] === whitespace) {
+      slicingIndex = index;
+      break;
     }
   }
-  label =
-    label.slice(0, slicingIndex) +
+  text =
+    text.slice(0, slicingIndex) +
     lineWrapHtmlTag +
-    label.slice(slicingIndex, label.length);
+    text.slice(slicingIndex, text.length);
 
-  const maxLabelLength = maxLineLength * maxLines + lineWrapHtmlTag.length;
-  if (label.length > maxLabelLength) {
-    // cut off label and add ellipsis at the end
-    label = label.slice(0, maxLabelLength - ellipsis.length) + ellipsis;
+  // check if remainer is still too long for next line
+  if (text.length - slicingIndex + lineWrapHtmlTag.length > maxLineLength) {
+    // if so call recursively with adjusted window
+    text = wrapText(
+      text,
+      maxLineLength,
+      maxLines,
+      slicingIndex + lineWrapHtmlTag.length,
+      currentLine + 1
+    );
   }
-  return label;
+
+  return text;
 }
 
 export function getSelectableMetrics(
