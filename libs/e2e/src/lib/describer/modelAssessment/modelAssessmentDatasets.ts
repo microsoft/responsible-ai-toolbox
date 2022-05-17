6dee1ab21d8a195dbbb56a82430ceaa9654f499d
Roman Lutz <romanlutz13@gmail.com>
2022-05-17 16:52:57 -0400

    Update postbuild branch with changes from main (#1434)
    * fix error on machines with pyspark installed where passed dataframe is not spark pandas (#1415)
    
    * add postbuild branch trigger (#1417)
    
    * Fix causal UI strings according to classification/regression tasks (#1419)
    
    * Fix causal UI strings according to classification/regression tasks
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Fix lint error
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Fix UI test
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Fix description for model overview (#1425)
    
    * fix description for model overview
    
    * keep new description for new model overview
    
    * fix failing to create error report when filter_features is empty list (#1421)
    
    * filter out missing values from what if dropdown to prevent explanation dashboard from crashing (#1418)
    
    * Remove |Set Value| blurb in case it is not availble in counterfactual panel (#1426)
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Add y-axis description to counterfactual feature importance chart (#1423)
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    Co-authored-by: xuke444 <40614413+xuke444@users.noreply.github.com>
    
    * Add the user class name to causal UI strings (#1422)
    
    * Fix causal UI strings according to classification/regression tasks
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Fix lint error
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Fix UI test
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Add the user class name to causal UI strings
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    Co-authored-by: xuke444 <40614413+xuke444@users.noreply.github.com>
    
    * fix math.min / max for array size more than 10^7 (#1427)
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>
    
    * upgrade pytest and lightgbm to try to fix random pytest segfault test failures (#1424)
    
    s
    
    * fix flaky notebook causing build failures by adding retry logic (#1431)
    
    * Upper bound SciKit-Learn to address freeze in causal (#1432)
    
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    Replaces #1429  to address #1430 . Causal analysis is getting stuck with the latest release of SciKit-Learn. This contains:
    
    - Test case which gets stuck with SciKit-Learn 1.1.0
    - Upper bound on SciKit-Learn in `requirements.txt`
    
    ## Checklist
    
    <!--- Make sure to satisfy all the criteria listed below. -->
    
    - [x] I have added screenshots above for all UI changes.
    - [x] Documentation was updated if it was needed.
    - [x] New tests were added or changes were manually verified.
    
    
    Signed-off-by: Richard Edgar <riedgar@microsoft.com>
    
    Co-authored-by: Ilya Matiach <ilmat@microsoft.com>
    Co-authored-by: Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
    Co-authored-by: xuke444 <40614413+xuke444@users.noreply.github.com>
    Co-authored-by: Richard Edgar <riedgar@microsoft.com>

--- libs/e2e/src/lib/describer/modelAssessment/modelAssessmentDatasets.ts
+++ libs/e2e/src/lib/describer/modelAssessment/modelAssessmentDatasets.ts
@@ -92,6 +92,10 @@ const modelAssessmentDatasets = {
   },
   DiabetesDecisionMaking: {
     causalAnalysisData: {
+      binaryDescription:
+        "On average in this sample, turning on this feature will cause the predictions of the target to increase/decrease by X units.",
+      continuousDescription:
+        "On average in this sample, increasing this feature by 1 unit will cause the predictions of the target to increase/decrease by X units.",
       featureListInCausalTable: ["s2(num)", "bmi(num)", "bp(num)"],
       hasCausalAnalysisComponent: true
     },
@@ -318,6 +322,10 @@ const modelAssessmentDatasets = {
   },
   HousingDecisionMaking: {
     causalAnalysisData: {
+      binaryDescription:
+        "On average in this sample, turning on this feature will cause the predictions of the target to increase/decrease by X units.",
+      continuousDescription:
+        "On average in this sample, increasing this feature by 1 unit will cause the predictions of the target to increase/decrease by X units.",
       featureListInCausalTable: [
         "GarageCars(num)",
         "OverallQual(num)",
