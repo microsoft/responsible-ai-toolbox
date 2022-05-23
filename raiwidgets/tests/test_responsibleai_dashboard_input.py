6c832fa96804cd1fa07d3387ba7acb810b9804cf
Gaurav Gupta <gaugup@microsoft.com>
2022-05-23 16:58:15 -0700

    Add unit tests for ResponsibleAIDashboardInput
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- raiwidgets/tests/test_responsibleai_dashboard_input.py
+++ raiwidgets/tests/test_responsibleai_dashboard_input.py
@@ -5,20 +5,47 @@ from unittest.mock import patch
 
 from raiwidgets.responsibleai_dashboard_input import \
     ResponsibleAIDashboardInput
+from raiwidgets.interfaces import WidgetRequestResponseConstants
 
 
 class TestResponsibleAIDashboardInput:
-    def test_model_analysis_adult(
+    def test_model_analysis_adult_on_predict_success(
             self, create_rai_insights_object_classification):
         ri = create_rai_insights_object_classification
         knn = ri.model
         test_data = ri.test
 
         dashboard_input = ResponsibleAIDashboardInput(ri)
-        with patch.object(knn, "predict_proba") as predict_mock:
-            test_pred_data = test_data.head(1).drop("Income", axis=1).values
-            dashboard_input.on_predict(
-                test_pred_data)
+        test_pred_data = test_data.head(1).drop("Income", axis=1).values
+        flask_server_prediction_output = dashboard_input.on_predict(
+            test_pred_data)
+        knn_prediction = knn.predict_proba(test_pred_data)
 
-            assert (predict_mock.call_args[0]
-                    [0].values == test_pred_data).all()
+        assert knn_prediction is not None
+        assert flask_server_prediction_output is not None
+        assert WidgetRequestResponseConstants.data in flask_server_prediction_output 
+        assert (flask_server_prediction_output['data'] == knn_prediction).all()
+
+    def test_model_analysis_adult_on_predict_failure(
+            self, create_rai_insights_object_classification):
+        ri = create_rai_insights_object_classification
+        test_data = ri.test
+
+        dashboard_input = ResponsibleAIDashboardInput(ri)
+        test_pred_data = test_data.head(1).values
+        flask_server_prediction_output = dashboard_input.on_predict(
+            test_pred_data)
+
+        assert flask_server_prediction_output is not None
+        assert WidgetRequestResponseConstants.error in flask_server_prediction_output
+        assert "Model threw exception while predicting..." in flask_server_prediction_output[WidgetRequestResponseConstants.error]
+        assert len(flask_server_prediction_output[WidgetRequestResponseConstants.data]) == 0
+
+    def test_model_analysis_adult_importances(
+            self, create_rai_insights_object_classification):
+        ri = create_rai_insights_object_classification
+
+        dashboard_input = ResponsibleAIDashboardInput(ri)
+        flask_server_prediction_output = dashboard_input.importances()
+        assert flask_server_prediction_output is not None
+        assert WidgetRequestResponseConstants.data in flask_server_prediction_output 
