54911ee8039f7de4cf2b96fa77ba11dd13681fdd
Gaurav Gupta <gaugup@microsoft.com>
2022-06-10 09:57:36 -0700

    Undo models.py change
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- raiutils/raiutils/models/__init__.py
+++ raiutils/raiutils/models/__init__.py
@@ -2,6 +2,6 @@
 # Licensed under the MIT License.
 
 """Module for defining common utilities related to models."""
-from .models import SKLearn, is_classifier
+from .model_utils import SKLearn, is_classifier
 
 __all__ = ['is_classifier', 'SKLearn']
