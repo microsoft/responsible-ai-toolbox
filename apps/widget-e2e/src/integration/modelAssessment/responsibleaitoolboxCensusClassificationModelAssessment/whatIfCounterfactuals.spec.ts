76897eb8561aa5c667e62938fc970e69d98bed0d
Roman Lutz <romanlutz13@gmail.com>
2022-06-15 23:41:36 -0400

    merge follow-up

--- apps/widget-e2e/src/integration/modelAssessment/responsibleaitoolboxCensusClassificationModelAssessment/whatIfCounterfactuals.spec.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-import { describeWhatIf, modelAssessmentDatasets } from "@responsible-ai/e2e";
-
-const datasetShape =
-  modelAssessmentDatasets.CensusClassificationModelAssessment;
-describeWhatIf(datasetShape, "CensusClassificationModelAssessment");
