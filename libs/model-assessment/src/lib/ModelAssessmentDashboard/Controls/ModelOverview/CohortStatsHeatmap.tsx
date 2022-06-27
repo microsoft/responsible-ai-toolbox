6d2328bed5244903ba45791fbef6887b74840860
Roman Lutz <romanlutz13@gmail.com>
2022-06-27 16:20:42 -0400

    Model overview: use unique key to avoid heatmap color mismatch (#1517)
    * use unique key
    
    * adjust key to be easier to read and understand

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/CohortStatsHeatmap.tsx
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/CohortStatsHeatmap.tsx
@@ -23,7 +23,9 @@ interface ICohortStatsHeatmapProps {
   id: string;
 }
 
-class ICohortStatsHeatmapState {}
+interface ICohortStatsHeatmapState {
+  key: number;
+}
 
 export class CohortStatsHeatmap extends React.Component<
   ICohortStatsHeatmapProps,
@@ -32,6 +34,30 @@ export class CohortStatsHeatmap extends React.Component<
   public static contextType = ModelAssessmentContext;
   public context: React.ContextType<typeof ModelAssessmentContext> =
     defaultModelAssessmentContext;
+
+  public constructor(props: ICohortStatsHeatmapProps) {
+    super(props);
+    this.state = { key: 0 };
+  }
+
+  public componentDidUpdate(prevProps: ICohortStatsHeatmapProps) {
+    const cohortsChanged =
+      prevProps.cohorts.length !== this.props.cohorts.length ||
+      prevProps.cohorts.some(
+        (errorCohort, cohortIndex) =>
+          this.props.cohorts[cohortIndex].cohort.getCohortID() !==
+          errorCohort.cohort.getCohortID()
+      );
+    const metricsChanged =
+      prevProps.selectedMetrics.length !== this.props.selectedMetrics.length ||
+      prevProps.selectedMetrics.some(
+        (metric, metricIndex) =>
+          metric !== this.props.selectedMetrics[metricIndex]
+      );
+    if (cohortsChanged || metricsChanged) {
+      this.setState({ key: this.state.key + 1 });
+    }
+  }
 
   public render(): React.ReactNode {
     const columns: string[] = [
@@ -58,6 +84,7 @@ export class CohortStatsHeatmap extends React.Component<
 
     return (
       <HeatmapHighChart
+        key={`heatmap${this.state.key}`}
         id={this.props.id}
         configOverride={{
           chart: {
