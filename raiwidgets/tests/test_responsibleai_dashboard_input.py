1b607b5199cb7e8a1d51b04e63068e539d55212f
Gaurav Gupta <gaugup@microsoft.com>
2022-07-21 16:00:33 -0700

    Add multiclass classification test in raiwidgets
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- raiwidgets/tests/test_responsibleai_dashboard_input.py
+++ raiwidgets/tests/test_responsibleai_dashboard_input.py
@@ -220,3 +220,23 @@ class TestResponsibleAIDashboardInputRegression:
         assert "Failed to generate causal what-if," in \
             flask_server_prediction_output[
                 WidgetRequestResponseConstants.error]
+
+
+class TestResponsibleAIDashboardInputMultiClassClassification:
+    def test_rai_dashboard_input_iris_on_predict_success(
+            self, create_rai_insights_object_multiclass_classification):
+        ri = create_rai_insights_object_multiclass_classification
+        rf = ri.model
+        test_data = ri.test
+
+        dashboard_input = ResponsibleAIDashboardInput(ri)
+        test_pred_data = test_data.head(1).drop("target", axis=1).values
+        flask_server_prediction_output = dashboard_input.on_predict(
+            test_pred_data)
+        rf_prediction = rf.predict_proba(test_pred_data)
+
+        assert rf_prediction is not None
+        assert flask_server_prediction_output is not None
+        assert WidgetRequestResponseConstants.data in \
+            flask_server_prediction_output
+        assert (flask_server_prediction_output['data'] == rf_prediction).all()
