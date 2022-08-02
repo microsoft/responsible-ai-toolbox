071f7e7e946fddbdf063f40366dff897ce094ea2
Ruby Zhu <zhenzhu@microsoft.com>
2022-08-02 15:53:19 -0700

    add comments
    Signed-off-by: Ruby Zhu <zhenzhu@microsoft.com>

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/FeaturesUtils.ts
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/FeaturesUtils.ts
@@ -1,7 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 import { localization } from "@responsible-ai/localization";
+
 import { IFeatureConfigurationRow } from "./FeatureConfigurationFlyout";
 
 export function shortFeatureGroups(items: IFeatureConfigurationRow[]) {
+  // if feature groups is more than 10, keep the top 10 groups, for the rest show as Other
   items.forEach((item) => {
     if (item.groups.length > 10) {
       const groups = item.groups.slice(0, 10);
