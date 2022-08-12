128d92db832f8af5269e750e753e247e1b959155
Ilya Matiach <ilmat@microsoft.com>
2022-08-12 12:57:33 -0400

    add UI tests for RAI text dashboard

---
+++ apps/dashboard-e2e/src/integration/interpret-text/newsgroupsBinaryData/individualFeatureImportance.spec.ts
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import { describeIndividualFeatureImportance } from "../../../describer/interpretText/individualFeatureImportance/describeIndividualFeatureImportance";
+
+describeIndividualFeatureImportance("newsgroupBinaryData");
