22c68a11b657b7cd2ec9beeb347bf87cad1e16b7
Ilya Matiach <ilmat@microsoft.com>
2022-08-18 15:36:27 -0400

    enable new Model Overview experience in OSS RAI Dashboard by default (#1647)

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/FeatureFlights.ts
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/FeatureFlights.ts
@@ -1,15 +1,11 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-export const newModelOverviewExperienceFlight = "newModelOverviewExperience";
 export const dataBalanceExperienceFlight = "dataBalanceExperience";
 export const featureFlightSeparator = "&";
 
 // add more entries for new feature flights
-export const featureFlights = [
-  newModelOverviewExperienceFlight,
-  dataBalanceExperienceFlight
-];
+export const featureFlights = [dataBalanceExperienceFlight];
 
 export function parseFeatureFlights(featureFlights?: string): string[] {
   if (featureFlights) {
