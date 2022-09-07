431770878a8b4478a1fe533024480a614a240815
Ilya Matiach <ilmat@microsoft.com>
2022-09-07 16:42:42 -0400

    add mini-table to feature importances view (#1682)

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/IndividualFeatureImportanceView/IndividualFeatureImportanceView.tsx
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/IndividualFeatureImportanceView/IndividualFeatureImportanceView.tsx
@@ -1,29 +1,70 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { Stack } from "@fluentui/react";
+import { IObjectWithKey, Label, Stack, Text } from "@fluentui/react";
 import {
   defaultModelAssessmentContext,
   ModelAssessmentContext
 } from "@responsible-ai/core-ui";
+import { TableView } from "@responsible-ai/dataset-explorer";
+import { localization } from "@responsible-ai/localization";
 import React from "react";
 
 import { IIndividualFeatureImportanceProps } from "./IndividualFeatureImportanceProps";
+import { IIndividualFeatureImportanceState } from "./IndividualFeatureImportanceState";
+import {
+  horizontalComponentTokens,
+  individualFeatureImportanceStyles,
+  verticalComponentTokens
+} from "./IndividualFeatureImportanceView.styles";
 import { TabularLocalImportancePlots } from "./TabularLocalImportancePlots";
 import { TextLocalImportancePlots } from "./TextLocalImportancePlots";
 
-export class IndividualFeatureImportanceView extends React.Component<IIndividualFeatureImportanceProps> {
+export class IndividualFeatureImportanceView extends React.Component<
+  IIndividualFeatureImportanceProps,
+  IIndividualFeatureImportanceState
+> {
   public static contextType = ModelAssessmentContext;
   public context: React.ContextType<typeof ModelAssessmentContext> =
     defaultModelAssessmentContext;
+
+  public constructor(props: IIndividualFeatureImportanceProps) {
+    super(props);
+    this.state = {
+      allSubsetSelectedItems: []
+    };
+  }
 
   public render(): React.ReactNode {
     const hasTextImportances =
       !!this.context.modelExplanationData?.precomputedExplanations
         ?.textFeatureImportance;
-
+    const classNames = individualFeatureImportanceStyles();
     return (
-      <Stack tokens={{ padding: "l1" }}>
+      <Stack tokens={verticalComponentTokens}>
+        <Stack horizontal tokens={horizontalComponentTokens}>
+          <Label className={classNames.boldText}>
+            {
+              localization.ModelAssessment.IndividualFeatureImportanceView
+                .SmallInstanceSelection
+            }
+          </Label>
+          <Text variant="large">
+            {
+              localization.ModelAssessment.IndividualFeatureImportanceView
+                .SmallTableText
+            }
+          </Text>
+        </Stack>
+        <TableView
+          features={this.props.features}
+          jointDataset={this.props.jointDataset}
+          selectedCohort={this.props.selectedCohort}
+          modelType={this.props.modelType}
+          onAllSelectedItemsChange={this.onAllSubsetSelectedItemsChange}
+          telemetryHook={this.props.telemetryHook}
+          subsetSelectedItems={this.props.allSelectedItems}
+        />
         {!hasTextImportances && (
           <TabularLocalImportancePlots
             features={this.context.modelMetadata.featureNames}
@@ -35,7 +76,7 @@ export class IndividualFeatureImportanceView extends React.Component<IIndividual
             onWeightChange={this.props.onWeightChange}
             selectedCohort={this.context.selectedErrorCohort}
             modelType={this.props.modelType}
-            selectedItems={this.props.allSelectedItems}
+            selectedItems={this.state.allSubsetSelectedItems}
             telemetryHook={this.props.telemetryHook}
           />
         )}
@@ -52,4 +93,10 @@ export class IndividualFeatureImportanceView extends React.Component<IIndividual
       </Stack>
     );
   }
+
+  private onAllSubsetSelectedItemsChange = (
+    allSubsetSelectedItems: IObjectWithKey[]
+  ): void => {
+    this.setState({ allSubsetSelectedItems });
+  };
 }
