2ddc41fb8db884a8a8b56603b9c9d5f81b7d6a90
vinutha karanth <vinutha.karanth@gmail.com>
2022-10-28 15:56:44 -0700

    matrix style update
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>

--- libs/error-analysis/src/lib/ErrorAnalysisDashboard/Controls/Matrix/MatrixLegend/MatrixLegend.styles.ts
+++ libs/error-analysis/src/lib/ErrorAnalysisDashboard/Controls/Matrix/MatrixLegend/MatrixLegend.styles.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 import { IStyle, mergeStyleSets, IProcessedStyleSet } from "@fluentui/react";
+import { flexMdDown } from "@responsible-ai/core-ui";
 
 import { metricStyles, textStyles } from "../../../Styles/CommonStyles.styles";
 
@@ -12,6 +13,7 @@ export interface IMatrixLegendStyles {
   metricBarRed: IStyle;
   smallHeader: IStyle;
   valueBlack: IStyle;
+  metricLegendStack: IStyle;
 }
 
 export const matrixLegendStyles: () => IProcessedStyleSet<IMatrixLegendStyles> =
@@ -27,6 +29,7 @@ export const matrixLegendStyles: () => IProcessedStyleSet<IMatrixLegendStyles> =
       metricBarGreen: commonMetricStyles.metricBarGreen,
       metricBarRed: commonMetricStyles.metricBarRed,
       smallHeader: commonTextStyles.smallHeader,
-      valueBlack: commonMetricStyles.valueBlack
+      valueBlack: commonMetricStyles.valueBlack,
+      metricLegendStack: flexMdDown
     });
   };
