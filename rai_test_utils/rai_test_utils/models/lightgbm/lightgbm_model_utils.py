9438ac60905131beba8a0a5decb32db33f9d4508
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2022-10-28 18:11:24 -0700

    Add rai_test_utils package (#1777)
    * Add rai_test_utils package
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Lint fix and namespace changes
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Fix duplicated cohorts not retaining the selection in combobox (#1785)
    
    * fix and e2e
    
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>
    
    * update filter
    
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>
    
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>
    
    * release raiwidgets and responsibleai 0.23.0 (#1787)
    
    * fix release github action for raiwidgets and responsibleai by making it same as in gated build (#1788)
    
    * Add e2e for dependency plot Avg of abs value option (#1768)
    
    * fix dep plot
    
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>
    
    * add e2e
    
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>
    
    * lintfix
    
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>
    
    * lintfix
    
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>
    
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>
    
    * update e2e visit function (#1792)
    
    * Fix code review comments
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    Signed-off-by: vinutha karanth <vinutha.karanth@gmail.com>
    Co-authored-by: Vinutha Karanth <vinutha.karanth@gmail.com>
    Co-authored-by: Ilya Matiach <ilmat@microsoft.com>
    Co-authored-by: tongy-msft <91754176+tongyu-microsoft@users.noreply.github.com>

---
+++ rai_test_utils/rai_test_utils/models/lightgbm/lightgbm_model_utils.py
@@ -0,0 +1,12 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+from lightgbm import LGBMClassifier
+
+
+def create_lightgbm_classifier(X, y):
+    lgbm = LGBMClassifier(boosting_type='gbdt', learning_rate=0.1,
+                          max_depth=5, n_estimators=200, n_jobs=1,
+                          random_state=777)
+    model = lgbm.fit(X, y)
+    return model
---
+++ rai_test_utils/rai_test_utils/models/sklearn/sklearn_model_utils.py
@@ -0,0 +1,82 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+import numpy as np
+import pandas as pd
+from sklearn import svm
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import (FunctionTransformer, OneHotEncoder,
+                                   StandardScaler)
+
+
+def create_sklearn_random_forest_classifier(X, y):
+    rfc = RandomForestClassifier(n_estimators=10, max_depth=4,
+                                 random_state=777)
+    model = rfc.fit(X, y)
+    return model
+
+
+def create_sklearn_svm_classifier(X, y, probability=True):
+    clf = svm.SVC(gamma=0.001, C=100., probability=probability,
+                  random_state=777)
+    model = clf.fit(X, y)
+    return model
+
+
+def create_kneighbors_classifier(X, y):
+    knn = KNeighborsClassifier()
+    model = knn.fit(X, y)
+    return model
+
+
+def create_sklearn_logistic_regressor(X, y, pipeline=False):
+    lin = LogisticRegression(solver='liblinear', random_state=777)
+    if pipeline:
+        lin = Pipeline([('lin', lin)])
+    model = lin.fit(X, y)
+    return model
+
+
+def create_sklearn_random_forest_regressor(X, y):
+    rfc = RandomForestRegressor(n_estimators=10, max_depth=4,
+                                random_state=777)
+    model = rfc.fit(X, y)
+    return model
+
+
+def create_titanic_pipeline(X_train, y_train):
+    def conv(X):
+        if isinstance(X, pd.Series):
+            return X.values
+        return X
+
+    many_to_one_transformer = \
+        FunctionTransformer(lambda x: conv(x.sum(axis=1)).reshape(-1, 1))
+    many_to_many_transformer = \
+        FunctionTransformer(lambda x: np.hstack(
+            (conv(np.prod(x, axis=1)).reshape(-1, 1),
+                conv(np.prod(x, axis=1)**2).reshape(-1, 1))
+        ))
+    transformations = ColumnTransformer([
+        ("age_fare_1", Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='median')),
+            ('scaler', StandardScaler())
+        ]), ["age", "fare"]),
+        ("age_fare_2", many_to_one_transformer, ["age", "fare"]),
+        ("age_fare_3", many_to_many_transformer, ["age", "fare"]),
+        ("embarked", Pipeline(steps=[
+            ("imputer",
+                SimpleImputer(strategy='constant', fill_value='missing')),
+            ("encoder", OneHotEncoder(sparse=False))]), ["embarked"]),
+        ("sex_pclass", OneHotEncoder(sparse=False), ["sex", "pclass"])
+    ])
+    clf = Pipeline(steps=[('preprocessor', transformations),
+                          ('classifier',
+                           LogisticRegression(solver='lbfgs'))])
+    clf.fit(X_train, y_train)
+    return clf
