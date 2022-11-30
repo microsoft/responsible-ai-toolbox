4180e938cc6db9961cc2c0c35a98adf566871fbd
Roman Lutz <romanlutz13@gmail.com>
2022-11-30 16:19:11 -0500

    Simplify route generation logic for testing with flights (#1530)
    * remove noFlights special case
    
    * make model assessment the default
    
    * allow empty string
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>
    
    * update snap shot
    
    Signed-off-by: Ke Xu <xuke@microsoft.com>
    
    Co-authored-by: Ke Xu <xuke@microsoft.com>
    Co-authored-by: xuke444 <40614413+xuke444@users.noreply.github.com>

--- apps/dashboard/src/app/App.tsx
+++ apps/dashboard/src/app/App.tsx
@@ -19,7 +19,7 @@ import { App as ModelAssessment } from "../model-assessment/App";
 
 import { AppHeader } from "./AppHeader";
 import { applications, IApplications, applicationKeys } from "./applications";
-import { IAppSetting, noFlights, routeKey } from "./IAppSetting";
+import { IAppSetting, routeKey } from "./IAppSetting";
 import { themes } from "./themes";
 
 interface IAppState extends Required<IAppSetting> {
@@ -29,6 +29,7 @@ interface IAppState extends Required<IAppSetting> {
 
 export class App extends React.Component<IAppSetting, IAppState> {
   public static route = generateRoute(routeKey);
+
   public constructor(props: IAppSetting) {
     super(props);
     this.state = this.getState({ ...this.props, iteration: 0 });
@@ -141,11 +142,7 @@ export class App extends React.Component<IAppSetting, IAppState> {
                   this.state.version
                 ]
               }
-              featureFlights={
-                this.state.featureFlights === noFlights
-                  ? []
-                  : parseFeatureFlights(this.state.featureFlights)
-              }
+              featureFlights={parseFeatureFlights(this.state.featureFlights)}
             />
           )}
           {this.state.application === "modelAssessmentText" && (
@@ -160,11 +157,7 @@ export class App extends React.Component<IAppSetting, IAppState> {
                   this.state.version
                 ]
               }
-              featureFlights={
-                this.state.featureFlights === noFlights
-                  ? []
-                  : parseFeatureFlights(this.state.featureFlights)
-              }
+              featureFlights={parseFeatureFlights(this.state.featureFlights)}
             />
           )}
           {this.state.application === "modelAssessmentVision" && (
@@ -179,11 +172,7 @@ export class App extends React.Component<IAppSetting, IAppState> {
                   this.state.version
                 ]
               }
-              featureFlights={
-                this.state.featureFlights === noFlights
-                  ? []
-                  : parseFeatureFlights(this.state.featureFlights)
-              }
+              featureFlights={parseFeatureFlights(this.state.featureFlights)}
             />
           )}
         </div>
@@ -206,14 +195,14 @@ export class App extends React.Component<IAppSetting, IAppState> {
       props.application as keyof IApplications
     );
     const application: keyof IApplications =
-      idx < 0 ? "interpret" : applicationKeys[idx];
+      idx < 0 ? "modelAssessment" : applicationKeys[idx];
     return {
       application,
       dataset:
         !props.dataset || !applications[application].datasets[props.dataset]
           ? Object.keys(applications[application].datasets)[0]
           : props.dataset,
-      featureFlights: props.featureFlights ?? noFlights,
+      featureFlights: props.featureFlights ?? "",
       iteration: props.iteration + 1,
       language:
         !props.language || !Language[props.language]
