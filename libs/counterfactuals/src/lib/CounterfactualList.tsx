6e2f5d077eeca7be1baa8592c620c3c2477ef19c
tongy <tongy@microsoft.com>
2022-12-05 15:55:30 -0800

    fix wrong cf predict value

--- libs/counterfactuals/src/lib/CounterfactualList.tsx
+++ libs/counterfactuals/src/lib/CounterfactualList.tsx
@@ -163,8 +163,13 @@ export class CounterfactualList extends React.Component<
     const data = _.cloneDeep(items[idx]);
     Object.keys(data).forEach((k) => {
       data[k] = data[k] === "-" ? items[0][k] : data[k];
-      const keyIndex =
-        this.props.data?.feature_names_including_target.indexOf(k);
+      const metaDict = this.context.jointDataset.metaDict;
+      const columnKey = Object.keys(metaDict).find(
+        (item) => metaDict[item].label === k
+      );
+      if (!columnKey) {
+        return;
+      }
       if (typeof data[k] === "string") {
         const dropdownOption = getCategoricalOption(
           this.context.jointDataset,
@@ -173,9 +178,9 @@ export class CounterfactualList extends React.Component<
         const optionIndex = dropdownOption?.data.categoricalOptions.findIndex(
           (feature: IComboBoxOption) => feature.key === data[k]
         );
-        this.props.setCustomRowProperty(`Data${keyIndex}`, true, optionIndex);
+        this.props.setCustomRowProperty(columnKey, true, optionIndex);
       } else {
-        this.props.setCustomRowProperty(`Data${keyIndex}`, false, data[k]);
+        this.props.setCustomRowProperty(columnKey, false, data[k]);
       }
     });
     data.row = localization.formatString(
