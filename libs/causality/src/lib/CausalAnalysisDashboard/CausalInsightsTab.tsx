d608965ec00bf4fd264a1613e8e77407bbd3de87
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2022-12-12 17:52:50 -0800

    Disable local causal chart for large data support (#1845)
    * Disable local causal chart for large data support
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    * Fix lint
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>
    
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- libs/causality/src/lib/CausalAnalysisDashboard/CausalInsightsTab.tsx
+++ libs/causality/src/lib/CausalAnalysisDashboard/CausalInsightsTab.tsx
@@ -9,7 +9,8 @@ import {
   ITelemetryEvent,
   ModelAssessmentContext,
   TelemetryEventName,
-  TelemetryLevels
+  TelemetryLevels,
+  ifEnableLargeData
 } from "@responsible-ai/core-ui";
 import { localization } from "@responsible-ai/localization";
 import React from "react";
@@ -64,10 +65,12 @@ export class CausalInsightsTab extends React.PureComponent<
                 itemKey={CausalAnalysisOptions.Aggregate}
                 headerText={localization.CausalAnalysis.MainMenu.aggregate}
               />
-              <PivotItem
-                itemKey={CausalAnalysisOptions.Individual}
-                headerText={localization.CausalAnalysis.MainMenu.individual}
-              />
+              {!ifEnableLargeData(this.context.dataset) && (
+                <PivotItem
+                  itemKey={CausalAnalysisOptions.Individual}
+                  headerText={localization.CausalAnalysis.MainMenu.individual}
+                />
+              )}
               <PivotItem
                 itemKey={CausalAnalysisOptions.Treatment}
                 headerText={localization.CausalAnalysis.MainMenu.treatment}
