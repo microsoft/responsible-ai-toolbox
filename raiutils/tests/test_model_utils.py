9fee57229d7298821224df010501e896fb43821c
Roman Lutz <romanlutz13@gmail.com>
2023-02-02 00:26:53 -0500

    Move ModelTask to raiutils and create forecasting utils (#1935)

--- raiutils/tests/test_model_utils.py
+++ raiutils/tests/test_model_utils.py
@@ -1,7 +1,8 @@
 # Copyright (c) Microsoft Corporation
 # Licensed under the MIT License.
 
-from raiutils.models import is_classifier
+from raiutils.models import (is_classifier, is_forecaster,
+                             is_quantile_forecaster)
 
 
 class Classifier:
@@ -17,11 +18,29 @@ class Regressor:
         pass
 
 
+class Forecaster:
+    def forecast(self):
+        pass
+
+    def forecast_quantiles(self):
+        pass
+
+
 class TestIsClassifier:
     def test_classifier(self):
         classifier = Classifier()
         assert is_classifier(classifier)
+        assert not is_forecaster(classifier)
+        assert not is_quantile_forecaster(classifier)
 
     def test_regressor(self):
         regressor = Regressor()
         assert not is_classifier(regressor)
+        assert not is_forecaster(regressor)
+        assert not is_quantile_forecaster(regressor)
+
+    def test_forecaster(self):
+        forecaster = Forecaster()
+        assert not is_classifier(forecaster)
+        assert is_forecaster(forecaster)
+        assert is_quantile_forecaster(forecaster)
