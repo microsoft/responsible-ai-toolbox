f1b7da7307b43c61bf2665415c48019b5493a058
Ilya Matiach <ilmat@microsoft.com>
2023-02-24 10:42:31 -0500

    add ResponsibleAI Text package as initial commit

---
+++ responsibleai_text/version.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+name = 'responsibleai_text'
+_major = '0'
+_minor = '0'
+_patch = '13'
+version = '{}.{}.{}'.format(_major, _minor, _patch)
---
+++ tests/test_rai_text_insights.py
@@ -0,0 +1,49 @@
+# Copyright (c) Microsoft Corporation
+# Licensed under the MIT License.
+
+from responsibleai_text import RAITextInsights, ModelTask
+from common_text_utils import (
+    load_covid19_emergency_event_dataset,
+    load_emotion_dataset, load_squad_dataset,
+    create_multilabel_pipeline,
+    create_text_classification_pipeline, ANSWERS, EMOTION,
+    create_question_answering_pipeline, COVID19_EVENTS_LABELS)
+from rai_text_insights_validator import validate_rai_text_insights
+
+
+class TestRAITextInsights(object):
+
+    def test_rai_insights_emotion_classification(self):
+        data = load_emotion_dataset()
+        pred = create_text_classification_pipeline()
+        task_type = ModelTask.TEXT_CLASSIFICATION
+        run_rai_insights(pred, data, data[:3], EMOTION, task_type)
+
+    def test_rai_insights_question_answering(self):
+        data = load_squad_dataset()
+        pred = create_question_answering_pipeline()
+        task_type = ModelTask.QUESTION_ANSWERING
+        run_rai_insights(pred, data, data[:5], ANSWERS, task_type)
+
+    def test_rai_insights_multilabel(self):
+        data = load_covid19_emergency_event_dataset()
+        pred = create_multilabel_pipeline()
+        task_type = ModelTask.MULTILABEL_TEXT_CLASSIFICATION
+        labels = COVID19_EVENTS_LABELS
+        run_rai_insights(pred, data, data[:5], labels, task_type)
+
+
+def run_rai_insights(model, train_data, test_data,
+                     target_column, task_type):
+    rai_insights = RAITextInsights(model, train_data, test_data,
+                                   target_column,
+                                   task_type=task_type)
+    rai_insights.explainer.add()
+    if task_type != ModelTask.QUESTION_ANSWERING:
+        rai_insights.error_analysis.add()
+    rai_insights.compute()
+    rai_insights.get_data()
+    # Validate
+    validate_rai_text_insights(
+        rai_insights, train_data, test_data,
+        target_column, task_type)
