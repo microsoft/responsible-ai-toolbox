b919dc67f02df59bc6de43b3016e8abc1939074c
Roman Lutz <romanlutz13@gmail.com>
2023-02-24 14:46:53 -0500

    turn off EA

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/TabsView/TabsView.tsx
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/TabsView/TabsView.tsx
@@ -205,6 +205,7 @@ export class TabsView extends React.PureComponent<
                 </>
               )}
               {t.key === GlobalTabKeys.ErrorAnalysisTab &&
+                this.context.dataset.task_type !== DatasetTaskType.Forecasting &&
                 this.props.errorAnalysisData?.[0] && (
                   <>
                     <Stack
