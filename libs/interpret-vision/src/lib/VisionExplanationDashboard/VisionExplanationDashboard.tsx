6bb45f2585c93abe373e0786168cc103239043c0
Natalie Isak <natalieisak@microsoft.com>
2023-04-21 16:06:27 -0400

    movign things around

--- libs/interpret-vision/src/lib/VisionExplanationDashboard/VisionExplanationDashboard.tsx
+++ libs/interpret-vision/src/lib/VisionExplanationDashboard/VisionExplanationDashboard.tsx
@@ -4,31 +4,23 @@
 import {
   IDropdownOption,
   Stack,
-  PivotItem,
-  Slider,
-  Separator,
-  Text,
-  mergeStyles
+  PivotItem
 } from "@fluentui/react";
 import {
   defaultModelAssessmentContext,
   IVisionListItem,
   ModelAssessmentContext
 } from "@responsible-ai/core-ui";
-import { localization } from "@responsible-ai/localization";
 import React from "react";
 
-import { CohortToolBar } from "./Controls/CohortToolBar";
 import { Flyout } from "./Controls/Flyout";
 import { FlyoutObjectDetection } from "./Controls/FlyoutObjectDetection";
 import { imageListStyles } from "./Controls/ImageList.styles";
-import { PageSizeSelectors } from "./Controls/PageSizeSelectors";
-import { Pivots } from "./Controls/Pivots";
 import { TabsView } from "./Controls/TabsView";
-import { ToolBar } from "./Controls/ToolBar";
 import { IVisionExplanationDashboardProps } from "./Interfaces/IVisionExplanationDashboardProps";
 import { IVisionExplanationDashboardState } from "./Interfaces/IVisionExplanationDashboardState";
 import { visionExplanationDashboardStyles } from "./VisionExplanationDashboard.styles";
+import { VisionExplanationDashboardCommon } from "./VisionExplanationDashboardCommon"
 import {
   preprocessData,
   getItems,
@@ -79,100 +71,14 @@ export class VisionExplanationDashboard extends React.Component<
         grow
         tokens={{ childrenGap: "l1", padding: "m 40px" }}
       >
-        <Stack.Item>
-          <Pivots
-            selectedKey={this.state.selectedKey}
-            onLinkClick={this.handleLinkClick}
-          />
-        </Stack.Item>
-        <Stack.Item>
-          <Separator styles={{ root: { width: "100%" } }} />
-        </Stack.Item>
-        <Stack.Item>
-          <ToolBar
-            cohorts={this.props.cohorts}
-            searchValue={this.state.searchValue}
-            onSearch={this.onSearch}
-            selectedCohort={this.props.selectedCohort}
-            setSelectedCohort={this.props.setSelectedCohort}
-          />
-        </Stack.Item>
-        <Stack.Item>
-          <Stack
-            horizontal
-            horizontalAlign="space-between"
-            verticalAlign="start"
-          >
-            <Stack.Item>
-              <Slider
-                max={80}
-                min={20}
-                className={classNames.slider}
-                label={localization.InterpretVision.Dashboard.thumbnailSize}
-                defaultValue={50}
-                showValue={false}
-                onChange={this.onSliderChange}
-                disabled={
-                  this.state.selectedKey ===
-                  VisionDatasetExplorerTabOptions.ClassView
-                }
-              />
-            </Stack.Item>
-            {this.state.selectedKey !==
-            VisionDatasetExplorerTabOptions.ImageExplorerView ? (
-              <Stack.Item>
-                <PageSizeSelectors
-                  selectedKey={this.state.selectedKey}
-                  onNumRowsSelect={this.onNumRowsSelect}
-                  onPageSizeSelect={this.onPageSizeSelect}
-                />
-              </Stack.Item>
-            ) : (
-              <Stack
-                horizontal
-                tokens={{ childrenGap: "l1" }}
-                verticalAlign="center"
-              >
-                <Stack.Item>
-                  <Text>
-                    {localization.InterpretVision.Dashboard.predictedLabel}
-                  </Text>
-                </Stack.Item>
-                <Stack.Item
-                  className={mergeStyles(
-                    imageStyles.errorIndicator,
-                    classNames.legendIndicator
-                  )}
-                >
-                  <Text className={imageStyles.labelPredicted}>
-                    {localization.InterpretVision.Dashboard.legendFailure}
-                  </Text>
-                </Stack.Item>
-                <Stack.Item
-                  className={mergeStyles(
-                    imageStyles.successIndicator,
-                    classNames.legendIndicator
-                  )}
-                >
-                  <Text className={imageStyles.labelPredicted}>
-                    {localization.InterpretVision.Dashboard.legendSuccess}
-                  </Text>
-                </Stack.Item>
-              </Stack>
-            )}
-          </Stack>
-        </Stack.Item>
-        {this.state.selectedKey ===
-          VisionDatasetExplorerTabOptions.TableView && (
-          <Stack.Item>
-            <CohortToolBar
-              addCohort={this.addCohortWrapper}
-              cohorts={this.props.cohorts}
-              selectedIndices={this.state.selectedIndices}
-            />
-          </Stack.Item>
-        )}
-        <Stack.Item>
+      <Stack.Item>
+      <VisionExplanationDashboardCommon 
+        thisdashboard={this}
+        imageStyles={imageStyles}
+        classNames={classNames}
+      />
+      </Stack.Item>
+      <Stack.Item>
           <TabsView
             addCohort={this.addCohortWrapper}
             errorInstances={this.state.errorInstances}
@@ -207,117 +113,29 @@ export class VisionExplanationDashboard extends React.Component<
       grow
       tokens={{ childrenGap: "l1", padding: "m 40px" }}
     >
-      <Stack.Item>
-        <Pivots
-          selectedKey={this.state.selectedKey}
-          onLinkClick={this.handleLinkClick}
-        />
-      </Stack.Item>
-      <Stack.Item>
-        <Separator styles={{ root: { width: "100%" } }} />
-      </Stack.Item>
-      <Stack.Item>
-        <ToolBar
-          cohorts={this.props.cohorts}
-          searchValue={this.state.searchValue}
-          onSearch={this.onSearch}
-          selectedCohort={this.props.selectedCohort}
-          setSelectedCohort={this.props.setSelectedCohort}
-        />
-      </Stack.Item>
-      <Stack.Item>
-        <Stack
-          horizontal
-          horizontalAlign="space-between"
-          verticalAlign="start"
-        >
-          <Stack.Item>
-            <Slider
-              max={80}
-              min={20}
-              className={classNames.slider}
-              label={localization.InterpretVision.Dashboard.thumbnailSize}
-              defaultValue={50}
-              showValue={false}
-              onChange={this.onSliderChange}
-              disabled={
-                this.state.selectedKey ===
-                VisionDatasetExplorerTabOptions.ClassView
-              }
-            />
-          </Stack.Item>
-          {this.state.selectedKey !==
-          VisionDatasetExplorerTabOptions.ImageExplorerView ? (
-            <Stack.Item>
-              <PageSizeSelectors
-                selectedKey={this.state.selectedKey}
-                onNumRowsSelect={this.onNumRowsSelect}
-                onPageSizeSelect={this.onPageSizeSelect}
-              />
-            </Stack.Item>
-          ) : (
-            <Stack
-              horizontal
-              tokens={{ childrenGap: "l1" }}
-              verticalAlign="center"
-            >
-              <Stack.Item>
-                <Text>
-                  {localization.InterpretVision.Dashboard.predictedLabel}
-                </Text>
-              </Stack.Item>
-              <Stack.Item
-                className={mergeStyles(
-                  imageStyles.errorIndicator,
-                  classNames.legendIndicator
-                )}
-              >
-                <Text className={imageStyles.labelPredicted}>
-                  {localization.InterpretVision.Dashboard.legendFailure}
-                </Text>
-              </Stack.Item>
-              <Stack.Item
-                className={mergeStyles(
-                  imageStyles.successIndicator,
-                  classNames.legendIndicator
-                )}
-              >
-                <Text className={imageStyles.labelPredicted}>
-                  {localization.InterpretVision.Dashboard.legendSuccess}
-                </Text>
-              </Stack.Item>
-            </Stack>
-          )}
-        </Stack>
-      </Stack.Item>
-      {this.state.selectedKey ===
-        VisionDatasetExplorerTabOptions.TableView && (
+      <VisionExplanationDashboardCommon 
+        thisdashboard={this}
+        imageStyles={imageStyles}
+        classNames={classNames}
+      />
         <Stack.Item>
-          <CohortToolBar
+          <TabsView
             addCohort={this.addCohortWrapper}
-            cohorts={this.props.cohorts}
-            selectedIndices={this.state.selectedIndices}
+            errorInstances={this.state.errorInstances}
+            successInstances={this.state.successInstances}
+            imageDim={this.state.imageDim}
+            numRows={this.state.numRows}
+            otherMetadataFieldNames={this.state.otherMetadataFieldNames}
+            pageSize={this.state.pageSize}
+            searchValue={this.state.searchValue}
+            selectedItem={this.state.selectedItem}
+            selectedKey={this.state.selectedKey}
+            onItemSelect={this.onItemSelect}
+            updateSelectedIndices={this.updateSelectedIndices}
+            selectedCohort={this.props.selectedCohort}
+            setSelectedCohort={this.props.setSelectedCohort}
           />
         </Stack.Item>
-      )}
-      <Stack.Item>
-        <TabsView
-          addCohort={this.addCohortWrapper}
-          errorInstances={this.state.errorInstances}
-          successInstances={this.state.successInstances}
-          imageDim={this.state.imageDim}
-          numRows={this.state.numRows}
-          otherMetadataFieldNames={this.state.otherMetadataFieldNames}
-          pageSize={this.state.pageSize}
-          searchValue={this.state.searchValue}
-          selectedItem={this.state.selectedItem}
-          selectedKey={this.state.selectedKey}
-          onItemSelect={this.onItemSelect}
-          updateSelectedIndices={this.updateSelectedIndices}
-          selectedCohort={this.props.selectedCohort}
-          setSelectedCohort={this.props.setSelectedCohort}
-        />
-      </Stack.Item>
       <Stack.Item>
         <Flyout
           explanations={this.state.computedExplanations}
@@ -331,25 +149,25 @@ export class VisionExplanationDashboard extends React.Component<
     </Stack>
     );
   }
-  private updateSelectedIndices = (indices: number[]): void => {
+  public updateSelectedIndices = (indices: number[]): void => {
     this.setState({ selectedIndices: indices });
   };
-  private addCohortWrapper = (name: string, switchCohort: boolean): void => {
+  public addCohortWrapper = (name: string, switchCohort: boolean): void => {
     this.context.addCohort(
       getCohort(name, this.state.selectedIndices, this.context.jointDataset),
       switchCohort
     );
   };
-  private onPanelClose = (): void => {
+  public onPanelClose = (): void => {
     this.setState({ panelOpen: !this.state.panelOpen });
   };
-  private onSearch = (
+  public onSearch = (
     _event?: React.ChangeEvent<HTMLInputElement>,
     newValue?: string
   ): void => {
     this.setState({ searchValue: newValue || "" });
   };
-  private onItemSelect = (item: IVisionListItem): void => {
+  public onItemSelect = (item: IVisionListItem): void => {
     this.setState({ panelOpen: !this.state.panelOpen, selectedItem: item });
     const index = item.index;
     const { computedExplanations, loadingExplanation } = this.state;
@@ -377,7 +195,7 @@ export class VisionExplanationDashboard extends React.Component<
         });
     }
   };
-  private onItemSelectObjectDetection = (item: IVisionListItem, selectedObject = -1): void => {
+  public onItemSelectObjectDetection = (item: IVisionListItem, selectedObject = -1): void => {
     this.setState({ panelOpen: true, selectedItem: item });
     const { computedExplanations, loadingExplanation } = this.state;
     if (selectedObject !== -1) {
@@ -413,7 +231,7 @@ export class VisionExplanationDashboard extends React.Component<
   };
   /* For onSliderChange, the max imageDims per tab (400 and 100) are selected arbitrary to look like the Figma. 
   For handleLinkClick, the default are half the max values chosen in onSliderChange. */
-  private onSliderChange = (value: number): void => {
+  public onSliderChange = (value: number): void => {
     if (
       this.state.selectedKey ===
       VisionDatasetExplorerTabOptions.ImageExplorerView
@@ -423,19 +241,19 @@ export class VisionExplanationDashboard extends React.Component<
       this.setState({ imageDim: Math.floor((value / 100) * 100) });
     }
   };
-  private onNumRowsSelect = (
+  public onNumRowsSelect = (
     _event: React.FormEvent<HTMLDivElement>,
     item: IDropdownOption | undefined
   ): void => {
     this.setState({ numRows: Number(item?.text) });
   };
-  private onPageSizeSelect = (
+  public onPageSizeSelect = (
     _event: React.FormEvent<HTMLDivElement>,
     item: IDropdownOption | undefined
   ): void => {
     this.setState({ pageSize: Number(item?.text) });
   };
-  private handleLinkClick = (item?: PivotItem): void => {
+  public handleLinkClick = (item?: PivotItem): void => {
     if (item && item.props.itemKey !== undefined) {
       this.setState({ selectedKey: item.props.itemKey });
       switch (item.props.itemKey) {
