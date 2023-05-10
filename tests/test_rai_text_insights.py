6c2db2ff45b18446fdcb8c4234e89886b73cc5ba
Ilya Matiach <ilmat@microsoft.com>
2023-05-10 18:41:25 -0400

    add metadata support to RAITextInsights (#19)

--- tests/test_rai_text_insights.py
+++ tests/test_rai_text_insights.py
@@ -8,6 +8,7 @@ from common_text_utils import (ANSWERS, COVID19_EVENTS_LABELS, EMOTION,
                                load_covid19_emergency_event_dataset,
                                load_emotion_dataset, load_squad_dataset)
 from rai_text_insights_validator import validate_rai_text_insights
+from responsibleai.feature_metadata import FeatureMetadata
 
 from responsibleai_text import ModelTask, RAITextInsights
 
@@ -26,6 +27,15 @@ class TestRAITextInsights(object):
         task_type = ModelTask.QUESTION_ANSWERING
         run_rai_insights(pred, data, data[:5], ANSWERS, task_type)
 
+    def test_rai_insights_question_answering_metadata(self):
+        data = load_squad_dataset(with_metadata=True)
+        pred = create_question_answering_pipeline()
+        task_type = ModelTask.QUESTION_ANSWERING
+        feature_metadata = FeatureMetadata()
+        feature_metadata.categorical_features = ['title']
+        run_rai_insights(pred, data, data[:5], ANSWERS, task_type,
+                         feature_metadata)
+
     def test_rai_insights_multilabel(self):
         data = load_covid19_emergency_event_dataset()
         pred = create_multilabel_pipeline()
@@ -33,9 +43,20 @@ class TestRAITextInsights(object):
         labels = COVID19_EVENTS_LABELS
         run_rai_insights(pred, data, data[:5], labels, task_type)
 
+    def test_rai_insights_multilabel_metadata(self):
+        data = load_covid19_emergency_event_dataset(with_metadata=True)
+        pred = create_multilabel_pipeline()
+        task_type = ModelTask.MULTILABEL_TEXT_CLASSIFICATION
+        labels = COVID19_EVENTS_LABELS
+        feature_metadata = FeatureMetadata()
+        feature_metadata.categorical_features = ['language', 'country']
+        run_rai_insights(pred, data, data[:5], labels, task_type,
+                         feature_metadata)
+
 
 def run_rai_insights(model, train_data, test_data,
-                     target_column, task_type):
+                     target_column, task_type,
+                     feature_metadata=None):
     if task_type == ModelTask.TEXT_CLASSIFICATION:
         classes = train_data[target_column].unique()
         classes.sort()
@@ -44,7 +65,8 @@ def run_rai_insights(model, train_data, test_data,
     rai_insights = RAITextInsights(model, test_data,
                                    target_column,
                                    task_type=task_type,
-                                   classes=classes)
+                                   classes=classes,
+                                   feature_metadata=feature_metadata)
     rai_insights.explainer.add()
     if task_type != ModelTask.QUESTION_ANSWERING:
         rai_insights.error_analysis.add()
