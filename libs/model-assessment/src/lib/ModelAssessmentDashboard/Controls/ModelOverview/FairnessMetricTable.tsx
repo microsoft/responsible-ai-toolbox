a7ab1b9dc91cd9bb09b872e5ddfc2952d9b06dcf
Hannah Westra (SHE/HER) <hawestra@microsoft.com>
2023-05-31 08:54:03 -0700

    undo revert

--- libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/FairnessMetricTable.tsx
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/FairnessMetricTable.tsx
@@ -104,7 +104,10 @@ export class FairnessMetricTable extends React.Component<
                 data: items,
                 dataLabels: {
                   color: theme.semanticColors.bodyText,
-                  enabled: true
+                  enabled: true,
+                  style: {
+                    textOutline: "none"
+                  }
                 },
                 name: "Metrics",
                 type: "heatmap"
