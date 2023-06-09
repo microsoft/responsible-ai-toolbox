d80f8f0e3d06262056937064da9a70a33d6148d5
Gaurav Gupta <47334368+gaugup@users.noreply.github.com>
2023-06-08 23:33:14 -0700

    Add more dataset utilities in `rai_test_utils` (#2101)
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- rai_test_utils/rai_test_utils/utilities/utils.py
+++ rai_test_utils/rai_test_utils/utilities/utils.py
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation
 # Licensed under the MIT License.
 
+import os
 import uuid
 
 
@@ -17,3 +18,47 @@ def is_valid_uuid(id: str):
         return True
     except ValueError:
         return False
+
+
+def retrieve_dataset(dataset, **kwargs):
+    """Retrieve a dataset.
+
+    :param dataset: The dataset to retrieve.
+    :type dataset: str
+    :return: The dataset.
+    :rtype: object
+    """
+    # if data not extracted, download zip and extract
+    outdirname = 'datasets.4.27.2021'
+    if not os.path.exists(outdirname):
+        try:
+            from urllib import urlretrieve
+        except ImportError:
+            from urllib.request import urlretrieve
+        import zipfile
+        zipfilename = outdirname + '.zip'
+        urlretrieve(
+            'https://publictestdatasets.blob.core.windows.net/data/' +
+            zipfilename, zipfilename
+        )
+        with zipfile.ZipFile(zipfilename, 'r') as unzip:
+            unzip.extractall('.')
+    extension = os.path.splitext(dataset)[1]
+    filepath = os.path.join(outdirname, dataset)
+    if extension == '.npz':
+        # sparse format file
+        from scipy.sparse import load_npz
+        return load_npz(filepath)
+    elif extension == '.svmlight':
+        from sklearn import datasets
+        return datasets.load_svmlight_file(filepath)
+    elif extension == '.json':
+        import json
+        with open(filepath, encoding='utf-8') as f:
+            dataset = json.load(f)
+        return dataset
+    elif extension == '.csv':
+        import pandas as pd
+        return pd.read_csv(filepath, **kwargs)
+    else:
+        raise Exception('Unrecognized file extension: ' + extension)
