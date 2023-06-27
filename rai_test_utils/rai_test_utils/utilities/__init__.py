6537c281b351b946ef4e0934d5523bfa754e3919
Gaurav Gupta <gaugup@microsoft.com>
2023-06-27 13:11:49 -0700

    Delete the downloaded zip and folder in `retrieve_dataset`
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- rai_test_utils/rai_test_utils/utilities/__init__.py
+++ rai_test_utils/rai_test_utils/utilities/__init__.py
@@ -3,9 +3,10 @@
 
 """Namespace for utility functions used in tests."""
 
-from .utils import is_valid_uuid, retrieve_dataset
+from .utils import DOWNLOADED_DATASET_DIR, is_valid_uuid, retrieve_dataset
 
 __all__ = [
     "is_valid_uuid",
-    "retrieve_dataset"
+    "retrieve_dataset",
+    "DOWNLOADED_DATASET_DIR"
 ]
