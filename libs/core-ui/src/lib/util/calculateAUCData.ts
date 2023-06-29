6b4fc7d56bf5359e6bcdbeef67a6b154311ed938
Hannah Westra (SHE/HER) <hawestra@microsoft.com>
2023-06-28 19:33:53 -0700

    pipe ROC data

--- libs/core-ui/src/lib/util/calculateAUCData.ts
+++ libs/core-ui/src/lib/util/calculateAUCData.ts
@@ -3,7 +3,7 @@
 
 import { localization } from "@responsible-ai/localization";
 import { SeriesOptionsType } from "highcharts";
-import { orderBy, sortBy, sum, zipWith } from "lodash";
+import { orderBy, sortBy, zipWith } from "lodash";
 
 import { IDataset } from "../Interfaces/IDataset";
 
@@ -39,8 +39,8 @@ export function calculateROCData(
   trueY: number[]
 ): IROCData {
   const rocData: IROCData = {
-    falsePositiveRates: [],
-    truePositiveRates: []
+    falsePositiveRates: [0],
+    truePositiveRates: [0]
   };
 
   const yData = zipWith(
@@ -49,36 +49,44 @@ export function calculateROCData(
     (probY, trueY) => {
       return {
         // TODO: confirm that it's probability that y == 1
-        probabilityY: probY[1],
+        probabilityY: probY[0],
         trueY
       };
     }
   );
-  const totalPositives = sum(trueY);
-  const totalNegatives = trueY.length - totalPositives;
   const sortedY = sortBy(yData, ["probabilityY"]);
   // TODO: remove duplicates of values
-  let i = 0;
-  while (i < sortedY.length) {
+  let i = sortedY.length - 1;
+  while (i >= 0) {
     let truePositives = 0;
+    let trueNegatives = 0;
     let falsePositives = 0;
+    let falseNegatives = 0;
+    const threshold = sortedY[i].probabilityY;
     for (const y of sortedY) {
-      if (y.trueY) {
-        // need some check for predicted ???
-        if (y.probabilityY >= sortedY[i].probabilityY) {
+      // if the probability of predicting the positive label is greater than the
+      // threshold and the positive label was predicted than it's a true positive.
+      // otherwise, it's a false positive
+      if (y.probabilityY >= threshold) {
+        if (y.trueY) {
           truePositives++;
         } else {
           falsePositives++;
         }
+      } else if (y.trueY) {
+        falseNegatives++;
+      } else {
+        trueNegatives++;
       }
     }
+    const totalPositives = truePositives + falseNegatives || 1;
+    const totalNegatives = falsePositives + trueNegatives || 1;
     const tpr = truePositives / totalPositives;
     const fpr = falsePositives / totalNegatives;
     rocData.truePositiveRates.push(tpr);
     rocData.falsePositiveRates.push(fpr);
-    i++;
+    i--;
   }
-  console.log(rocData);
   return rocData;
 }
 
@@ -87,7 +95,6 @@ export function calculateAUCData(dataset: IDataset): SeriesOptionsType[] {
     // TODO: show warning message
     return [...getStaticROCData()];
   }
-  console.log(dataset.true_y);
   const rocData = calculateROCData(
     // TODO: remove cast
     dataset.probability_y as unknown as number[],
@@ -109,82 +116,4 @@ export function calculateAUCData(dataset: IDataset): SeriesOptionsType[] {
     ...getStaticROCData()
   ];
   return allData as SeriesOptionsType[];
-}
-
-// export function getROCData(trueLabels: number[], predictedProbabilities: number[]): any[] {
-//   let truePositive = 0;
-//   let falsePositive = 0;
-//   const truePositiveRates = [];
-//   const falsePositiveRates = [];
-//   let prevF = Number.NEGATIVE_INFINITY;
-//   // Based on: Tom Fawcett's "An introduction to ROC analysis.", http://academic.research.microsoft.com/Paper/2173957
-//   const sortedIndices = predictedProbabilities.sort().map((_, idx) => idx);
-
-//   for (let elem = 0; elem < predictedProbabilities.length; elem++) {
-//     const trueLabel = trueLabels[sortedIndices[elem]];
-//     const predLabel = predictedProbabilities[sortedIndices[elem]];
-//   }
-
-// }
-
-export function computeAUC(
-  trueLabels: number[],
-  predictedProbabilities: number[]
-): number {
-  // Based on: Tom Fawcett's "An introduction to ROC analysis.", http://academic.research.microsoft.com/Paper/2173957
-  const sortedIndices = predictedProbabilities.sort().map((_, idx) => idx);
-
-  let area = 0;
-  let truePositive = 0;
-  let falsePositive = 0;
-  let prevTruePositive = 0;
-  let prevFalsePositive = 0;
-  let fPrev = Number.NEGATIVE_INFINITY;
-
-  for (let elem = 0; elem < predictedProbabilities.length; elem++) {
-    const trueLabel = trueLabels[sortedIndices[elem]];
-    const predLabel = predictedProbabilities[sortedIndices[elem]];
-
-    if (!predLabel || !trueLabel) {
-      continue;
-    }
-    if (Math.abs(predLabel - fPrev) > Number.EPSILON) {
-      area += trapz(
-        falsePositive,
-        prevFalsePositive,
-        truePositive,
-        prevTruePositive
-      );
-      fPrev = predLabel;
-      prevFalsePositive = falsePositive;
-      prevTruePositive = truePositive;
-    }
-
-    if (trueLabel > 0) {
-      truePositive += 1;
-    }
-
-    if (trueLabel <= 0) {
-      falsePositive += 1;
-    }
-  }
-
-  area += trapz(
-    falsePositive,
-    prevFalsePositive,
-    truePositive,
-    prevTruePositive
-  );
-
-  // RS: Account for numerical inaccuracies in integration that might cause AUC to be > 1
-  return Math.min(
-    1,
-    truePositive * falsePositive !== 0
-      ? area / (truePositive * falsePositive)
-      : 0
-  );
-}
-
-function trapz(x1: number, x2: number, y1: number, y2: number): number {
-  return 0.5 * Math.abs(x1 - x2) * (y1 + y2);
 }
