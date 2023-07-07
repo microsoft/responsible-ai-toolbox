7b0d2cc0b4ef56991512d22745a38d12b1eb0719
Hannah Westra (SHE/HER) <hawestra@microsoft.com>
2023-07-07 13:03:14 -0700

    working version plus tests

--- libs/core-ui/src/lib/util/calculateAUCData.test.ts
+++ libs/core-ui/src/lib/util/calculateAUCData.test.ts
@@ -1,22 +1,148 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { binarizeData } from "./calculateAUCData";
+import { binarizeData, calculatePerClassROCData } from "./calculateAUCData";
 
 describe("Test binarizeData", () => {
   it("should binarize numbers", () => {
     const result = binarizeData([1, 3, 4, 0], [0, 1, 2, 3, 4]);
-    expect(result).toEqual([[]]);
+    expect(result).toEqual([
+      [0, 1, 0, 0, 0],
+      [0, 0, 0, 1, 0],
+      [0, 0, 0, 0, 1],
+      [1, 0, 0, 0, 0]
+    ]);
   });
   it("should binarize strings", () => {
     const result = binarizeData(
       ["one", "two", "three"],
       ["three", "one", "two"]
     );
-    expect(result).toEqual([[]]);
+    expect(result).toEqual([
+      [0, 1, 0],
+      [0, 0, 1],
+      [1, 0, 0]
+    ]);
   });
   it("should binarize binary data", () => {
     const result = binarizeData([1, 0, 1, 0], [0, 1]);
-    expect(result).toEqual([[]]);
+    expect(result).toEqual([
+      [0, 1],
+      [1, 0],
+      [0, 1],
+      [1, 0]
+    ]);
+  });
+});
+describe("Test calculatePerClassROCData", () => {
+  it("generate x,y data corresponding to fpr and tpr respectively", () => {
+    const result = calculatePerClassROCData(
+      [0.33, 0.32, 0.34, 0.29, 0.12, 0.41, 0.4, 0.39],
+      [0, 1, 1, 0, 1, 0, 0, 0]
+    );
+    expect(result).toEqual({
+      points: [
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 1 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 1, y: 0.6666666666666666 },
+        { x: 0.8, y: 0.6666666666666666 },
+        { x: 0.8, y: 0.6666666666666666 },
+        { x: 0.8, y: 0.6666666666666666 },
+        { x: 0.8, y: 0.3333333333333333 },
+        { x: 0.6, y: 0.3333333333333333 },
+        { x: 0.6, y: 0 },
+        { x: 0.6, y: 0 },
+        { x: 0.6, y: 0 },
+        { x: 0.6, y: 0 },
+        { x: 0.6, y: 0 },
+        { x: 0.4, y: 0 },
+        { x: 0.2, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 },
+        { x: 0, y: 0 }
+      ]
+    });
   });
 });
--- libs/core-ui/src/lib/util/calculateAUCData.ts
+++ libs/core-ui/src/lib/util/calculateAUCData.ts
@@ -3,13 +3,16 @@
 
 import { localization } from "@responsible-ai/localization";
 import { SeriesOptionsType } from "highcharts";
-import { orderBy, sortBy, sum, unzip, zipWith } from "lodash";
+import { range, unzip } from "lodash";
 
 import { IDataset } from "../Interfaces/IDataset";
 
+interface IPoint {
+  x: number;
+  y: number;
+}
 export interface IROCData {
-  falsePositiveRates: number[];
-  truePositiveRates: number[];
+  points: IPoint[];
 }
 
 function getStaticROCData(): SeriesOptionsType[] {
@@ -34,64 +37,45 @@ function getStaticROCData(): SeriesOptionsType[] {
   ];
 }
 
-function calculatePerClassROCData(
+export function calculatePerClassROCData(
   probabilityY: number[],
-  binTrueY: boolean[]
+  binY: number[]
 ): IROCData {
   const rocData: IROCData = {
-    falsePositiveRates: [],
-    truePositiveRates: []
+    points: []
   };
-
-  const yData = zipWith(probabilityY, binTrueY, (probY, trueY) => {
-    return {
-      probabilityY: probY,
-      trueY
-    };
-  });
-  const sortedY = sortBy(yData, ["probabilityY"]);
-  let prevProba = -Infinity;
+  const thresholds = range(0, 1, 0.01);
   let truePositives = 0;
   let falsePositives = 0;
-  const totalPositives = sum(binTrueY);
-  const totalNegatives = binTrueY.length - totalPositives;
-  let i = sortedY.length - 1;
-  while (i >= 0) {
-    // only add the point, if the probability has changed
-    if (sortedY[i].probabilityY !== prevProba) {
-      addROCPoint(
-        truePositives,
-        falsePositives,
-        totalPositives,
-        totalNegatives,
-        rocData
-      );
-      prevProba = sortedY[i].probabilityY;
-    }
-    const threshold = sortedY[i].probabilityY;
-    truePositives = 0;
-    falsePositives = 0;
-    for (const y of sortedY) {
+  let trueNegatives = 0;
+  let falseNegatives = 0;
+
+  for (const threshold of thresholds) {
+    for (const [index, yProba] of probabilityY.entries()) {
       // if the probability of predicting the positive label is greater than the
-      // threshold and the positive label was predicted than it's a true positive.
+      // threshold then it's a true positive.
       // otherwise, it's a false positive
-      if (y.probabilityY >= threshold) {
-        if (y.trueY) {
-          truePositives++;
+      if (yProba < threshold) {
+        if (binY[index]) {
+          falseNegatives++;
         } else {
-          falsePositives++;
+          trueNegatives++;
         }
+      } else if (binY[index]) {
+        truePositives++;
+      } else {
+        falsePositives++;
       }
     }
-    i--;
+    addROCPoint(
+      truePositives,
+      falsePositives,
+      trueNegatives,
+      falseNegatives,
+      rocData
+    );
+    truePositives = falsePositives = trueNegatives = falseNegatives = 0;
   }
-  addROCPoint(
-    truePositives,
-    falsePositives,
-    totalPositives,
-    totalNegatives,
-    rocData
-  );
 
   return rocData;
 }
@@ -99,32 +83,31 @@ function calculatePerClassROCData(
 function addROCPoint(
   truePositives: number,
   falsePositives: number,
-  totalPositives: number,
-  totalNegatives: number,
+  trueNegatives: number,
+  falseNegatives: number,
   rocData: IROCData
 ): void {
   // prevent division by 0
+  const totalNegatives = trueNegatives + falsePositives;
+  const totalPositives = truePositives + falseNegatives;
   const tpr = totalPositives === 0 ? 1 : truePositives / totalPositives;
   const fpr = totalNegatives === 0 ? 1 : falsePositives / totalNegatives;
-  rocData.truePositiveRates.push(tpr);
-  rocData.falsePositiveRates.push(fpr);
+  rocData.points.push({ x: fpr, y: tpr });
 }
 
 export function binarizeData(
   yData: string[] | number[] | number[][],
   classes: string[] | number[]
-): boolean[][] {
+): number[][] {
   // binarize labels in a one-vs-all fashion according to
-  const yBinData: boolean[][] = [];
-
+  const yBinData: number[][] = [];
   for (const yDatum of yData) {
     const binaryData = classes.map((c) => {
-      return c === yDatum;
+      return c === yDatum ? 1 : 0;
     });
     yBinData.push(binaryData);
   }
-  // transpose
-  return unzip(yBinData);
+  return yBinData;
 }
 
 // based on https://msdata.visualstudio.com/Vienna/_git/AzureMlCli?path=/src/azureml-metrics/azureml/metrics/_classification.py&version=GBmaster
@@ -134,36 +117,26 @@ export function calculateAUCData(dataset: IDataset): SeriesOptionsType[] {
     return [...getStaticROCData()];
   }
 
-  const binTrueY = binarizeData(dataset.true_y, dataset.class_names);
+  // temporary, replace with dataset.classnames
+  const cNames = [0, 1];
+  const binTrueY = binarizeData(dataset.true_y, cNames);
+  console.log(binTrueY);
+  // transpose in order to group class data together
+  const perClassBinY = unzip(binTrueY);
+  const perClassProba = unzip(dataset.probability_y);
   const data = [];
-  // TODO: class_names.length might not work ....
-  for (let i = 0; i < dataset.class_names.length; i++) {
-    const classROCData = calculatePerClassROCData(
-      dataset.probability_y[i],
-      binTrueY[i]
-    );
-    const classData = zipWith(
-      classROCData.falsePositiveRates,
-      classROCData.truePositiveRates,
-      (fpr, tpr) => {
-        return {
-          // TODO: check class_names length earlier ?
-          name: dataset.class_names ? dataset.class_names[i] : "",
-          x: fpr,
-          y: tpr
-        };
-      }
-    );
+  // loop through each class to calculate roc data per class
+  for (const [i, element] of perClassBinY.entries()) {
+    const classROCData = calculatePerClassROCData(perClassProba[i], element);
+    const classData = {
+      data: classROCData.points,
+      // TODO: check class_names length earlier ?
+      name: cNames ? cNames[i] : "",
+      type: "line"
+    };
     data.push(classData);
   }
 
-  const allData = [
-    {
-      data: orderBy(data, ["x"]),
-      name: "AUC",
-      type: "line"
-    },
-    ...getStaticROCData()
-  ];
+  const allData = [...data, ...getStaticROCData()];
   return allData as SeriesOptionsType[];
 }
