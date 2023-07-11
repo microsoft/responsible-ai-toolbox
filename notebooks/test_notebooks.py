c301d865d75c599b977372fb81e799217812c8bf
Advitya Gemawat <agemawat@microsoft.com>
2023-07-11 10:27:48 -0400

    python lint fixes

--- notebooks/test_notebooks.py
+++ notebooks/test_notebooks.py
@@ -258,9 +258,10 @@ def test_responsibleaidashboard_multiclass_dnn_model_debugging():
 
 
 @pytest.mark.notebooks
-def test_responsibleaidashboard_fridge_multilabel_image_classification_model_debugging():
+def test_responsibleaidashboard_fridge_multilabel_ic_model_debugging():
     nb_path = RESPONSIBLEAIDASHBOARD
-    nb_name = "responsibleaidashboard-fridge-multilabel-image-classification-model-debugging"
+    nb_name = "responsibleaidashboard-fridge-multilabel-\
+    image-classification-model-debugging"
 
     test_values = {}
     assay_one_notebook(nb_path, nb_name, test_values)
