54689bcfe5717ad0aea34b4a7aa8caed36f82ba0
Advitya Gemawat <agemawat@microsoft.com>
2023-07-17 11:39:24 -0400

    mo test for ic

---
+++ apps/widget-e2e/src/integration/modelAssessmentVision/responsibleaitoolboxFridgeImageClassificationModelDebugging/modelOverview.spec.ts
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+  describeVisionModelOverview,
+  modelAssessmentVisionDatasets
+} from "@responsible-ai/e2e";
+const datasetShape =
+  modelAssessmentVisionDatasets.FridgeImageClassificationModelDebugging;
+describeVisionModelOverview(datasetShape, "FridgeImageClassificationModelDebugging");
