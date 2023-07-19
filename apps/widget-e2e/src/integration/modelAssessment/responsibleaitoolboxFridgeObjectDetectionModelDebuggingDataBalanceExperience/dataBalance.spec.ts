479a5e4932a21650a6bed24551c525b34216e522
Advitya Gemawat <agemawat@microsoft.com>
2023-07-19 09:37:46 -0400

    spec refactor

---
+++ apps/widget-e2e/src/integration/modelAssessment/responsibleaitoolboxFridgeObjectDetectionModelDebuggingDataBalanceExperience/dataBalance.spec.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import {
+    describeDataBalance,
+    modelAssessmentDatasets
+} from "@responsible-ai/e2e";
+
+const datasetShape =
+    modelAssessmentDatasets.FridgeObjectDetectionModelDebugging;
+describeDataBalance(
+    datasetShape,
+    "FridgeObjectDetectionModelDebugging"
+);
