cdcf3d7dbac779ba32f523d49afcf09edb449076
Advitya Gemawat <agemawat@microsoft.com>
2023-07-26 09:50:24 -0400

    image click ckpt

--- libs/e2e/src/lib/describer/modelAssessment/visionDataExplorer/ensureAllVisionDataExplorerFlyoutElementsAfterSelectionArePresent.ts
+++ libs/e2e/src/lib/describer/modelAssessment/visionDataExplorer/ensureAllVisionDataExplorerFlyoutElementsAfterSelectionArePresent.ts
@@ -2,10 +2,18 @@
 // Licensed under the MIT License.
 
 import { Locators } from "../Constants";
+import { expect } from "chai";
 
 export function ensureAllVisionDataExplorerFlyoutElementsAfterSelectionArePresent(): void {
   cy.get(Locators.VisionDataExplorerImageExplorerViewButton).click();
 
   // TODO: click on flyout and get basic elements, then exit
-  //   cy.get(Locators.VisionDataExplorerImageExplorerViewErrorImage).click();
+  cy.get(Locators.VisionDataExplorerImageExplorerViewErrorImage)
+    .should("be.visible")
+    .and(($image) => {
+        // verifies the image is loaded
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+        expect($img[0].naturalHeight).to.be.greaterThan(0);
+    });
+  cy.get(Locators.VisionDataExplorerImageExplorerViewErrorImage).click();
 }
