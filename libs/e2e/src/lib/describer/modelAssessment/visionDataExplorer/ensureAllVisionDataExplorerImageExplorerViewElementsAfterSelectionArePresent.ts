c3282b9aa3f45a2e277aaaf833c6983705929ade
Advitya Gemawat <agemawat@microsoft.com>
2023-07-31 10:37:29 -0400

    test update

--- libs/e2e/src/lib/describer/modelAssessment/visionDataExplorer/ensureAllVisionDataExplorerImageExplorerViewElementsAfterSelectionArePresent.ts
+++ libs/e2e/src/lib/describer/modelAssessment/visionDataExplorer/ensureAllVisionDataExplorerImageExplorerViewElementsAfterSelectionArePresent.ts
@@ -11,19 +11,20 @@ export function ensureAllVisionDataExplorerImageExplorerViewElementsAfterSelecti
   cy.get(Locators.VisionDataExplorerLegendSuccess).should("exist");
 
   cy.get(Locators.VisionDataExplorerImageExplorerViewErrorInstances).should(
-    "exist"
-  );
-  cy.get(Locators.VisionDataExplorerImageExplorerViewErrorInstances).should(
     "include.text",
     "Error instances"
   );
+  cy.get(Locators.VisionDataExplorerImageExplorerViewSuccessImage)
+    .should("be.visible")
+    .and(($image) => {
+        // verifies the image is loaded
+        expect(($image[0] as HTMLImageElement).naturalWidth).to.be.greaterThan(0);
+        expect(($image[0] as HTMLImageElement).naturalHeight).to.be.greaterThan(0);
+    });
 //   cy.get(Locators.VisionDataExplorerImageExplorerViewErrorInstanceCount).should(
 //     "include.text",
 //     datasetShape.visionDataExplorerData?.errorInstances
 //   );
-  cy.get(Locators.VisionDataExplorerImageExplorerViewSuccessInstances).should(
-    "exist"
-  );
   cy.get(Locators.VisionDataExplorerImageExplorerViewSuccessInstances).should(
     "include.text",
     "Success instances"
