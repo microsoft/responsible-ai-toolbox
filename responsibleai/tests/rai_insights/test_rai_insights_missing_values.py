6b19019940d6584be8770d0bd9f75641f229754b
Gaurav Gupta <gaugup@microsoft.com>
2023-08-22 14:10:50 -0700

    fix linting
    Signed-off-by: Gaurav Gupta <gaugup@microsoft.com>

--- responsibleai/tests/rai_insights/test_rai_insights_missing_values.py
+++ responsibleai/tests/rai_insights/test_rai_insights_missing_values.py
@@ -120,8 +120,8 @@ class TestRAIInsightsMissingValues(object):
                         missing_value_combination == \
                             MISSING_VALUE.BOTH_TRAIN_TEST_MISSING_VALUES:
                         error_message = \
-                            "Categorical features workclass cannot have missing " + \
-                            "values for computing explanations. " + \
+                            "Categorical features workclass cannot have " + \
+                            "missing values for computing explanations. " + \
                             "Please check your training data."
                     else:
                         error_message = \
