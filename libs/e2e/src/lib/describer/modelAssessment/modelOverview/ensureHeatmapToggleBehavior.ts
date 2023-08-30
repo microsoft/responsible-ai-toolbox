58ba9c64f88103885b4fc2d31881b41dbe6bb1f3
Advitya Gemawat <agemawat@microsoft.com>
2023-08-30 16:10:54 -0400

    auto lint fixes

--- libs/e2e/src/lib/describer/modelAssessment/modelOverview/ensureHeatmapToggleBehavior.ts
+++ libs/e2e/src/lib/describer/modelAssessment/modelOverview/ensureHeatmapToggleBehavior.ts
@@ -3,41 +3,36 @@
 
 import { Locators } from "../Constants";
 
+export function ensureHeatmapToggleBehavior(heatmapTable: string): void {
+  cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).should("exist");
+  cy.get(heatmapTable).should("exist");
 
-export function ensureHeatmapToggleBehavior(
-    heatmapTable: string
-): void {
-    cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).should("exist");
-    cy.get(heatmapTable).should("exist");
+  // checks the toggle is on
+  cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).should(
+    "have.attr",
+    "aria-checked",
+    "true"
+  );
 
-    // checks the toggle is on
-    cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).should(
-        "have.attr",
-        "aria-checked",
-        "true"
-      );
+  // checks there are RGB colors in the heatmap table
+  cy.get(heatmapTable)
+    .find("path")
+    .filter('[fill*="rgb"]')
+    .should("have.length.greaterThan", 0);
 
-      // checks there are RGB colors in the heatmap table
-      cy.get(heatmapTable)
-        .find("path")
-        .filter('[fill*="rgb"]')
-        .should("have.length.greaterThan", 0);
+  // turn off the toggle
+  cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).click();
 
-      // turn off the toggle
-      cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).click();
+  // checks the toggle is off
+  cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).should(
+    "have.attr",
+    "aria-checked",
+    "false"
+  );
 
-      // checks the toggle is off
-      cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).should(
-        "have.attr",
-        "aria-checked",
-        "false"
-      );
+  // checks there are no RGB colors in the heatmap table
+  cy.get(heatmapTable).find("path").should("not.have.attr", 'fill*="rgb"');
 
-      // checks there are no RGB colors in the heatmap table
-      cy.get(heatmapTable)
-        .find("path")
-        .should("not.have.attr", 'fill*="rgb"');
-
-    // turn the toggle back on
-    cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).click();
+  // turn the toggle back on
+  cy.get(Locators.ModelOverviewHeatmapVisualDisplayToggle).click();
 }
