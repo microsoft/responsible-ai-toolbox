572263e2bf28384988cdb344eda765297387d0e0
Advitya Gemawat <agemawat@microsoft.com>
2023-08-30 09:52:51 -0400

    od widget test ckpt

--- libs/e2e/src/lib/describer/modelAssessment/modelOverview/ensureAllModelOverviewBasicElementsArePresent.ts
+++ libs/e2e/src/lib/describer/modelAssessment/modelOverview/ensureAllModelOverviewBasicElementsArePresent.ts
@@ -2,8 +2,11 @@
 // Licensed under the MIT License.
 
 import { Locators } from "../Constants";
+import { IModelAssessmentData } from "../IModelAssessmentData";
 
-export function ensureAllModelOverviewBasicElementsArePresent(): void {
+export function ensureAllModelOverviewBasicElementsArePresent(
+  datasetShape: IModelAssessmentData
+  ): void {
   cy.get(Locators.ModelOverviewHeader).should("exist");
   cy.get(Locators.ModelOverviewDescription).should("exist");
   cy.get(Locators.ModelOverviewCohortViewSelector).should("exist");
@@ -13,4 +16,11 @@ export function ensureAllModelOverviewBasicElementsArePresent(): void {
     3
   );
   cy.get(Locators.ModelOverviewMetricSelection).should("exist");
+
+  if (datasetShape.isObjectDetection) {
+    cy.get(Locators.ModelOverviewAggregateMethod).should("exist");
+    cy.get(Locators.ModelOverviewClassSelector).should("exist");
+    cy.get(Locators.ModelOverviewIoUThresholdSlider).should("exist");
+  }
+
 }
