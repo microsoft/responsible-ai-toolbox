9f24427cafeccc0f6621855ca445ad3607be652a
Roman Lutz <romanlutz13@gmail.com>
2023-08-31 11:48:19 -0400

    serialize pandas timestamps json safe

--- responsibleai/responsibleai/_internal/_served_model_wrapper.py
+++ responsibleai/responsibleai/_internal/_served_model_wrapper.py
@@ -1,6 +1,9 @@
 # Copyright (c) Microsoft Corporation
 # Licensed under the MIT License.
 
+from raiutils.data_processing import serialize_json_safe
+
+import json
 import requests
 
 
@@ -9,9 +12,13 @@ class ServedModelWrapper:
         self.port = port
     
     def forecast(self, X):
+        # request formatting according to mlflow docs
+        # https://mlflow.org/docs/latest/cli.html#mlflow-models-serve
         response = requests.post(
-            url=f"http://localhost:{self.port}/predict",
-            json={"X": X.to_json(orient='split')})
+            url=f"http://localhost:{self.port}/invocations",
+            data=json.dumps(
+                {"dataframe_split": X.to_dict(orient='split')},
+                default=serialize_json_safe))
         if response.status_code < 300:
             return response.content
         else:
