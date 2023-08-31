3ab3afbdb0df9914dc405e143aa51f20047ac015
Roman Lutz <romanlutz13@gmail.com>
2023-08-31 13:02:37 -0400

    json loads to read binary string

--- responsibleai/responsibleai/_internal/_served_model_wrapper.py
+++ responsibleai/responsibleai/_internal/_served_model_wrapper.py
@@ -14,13 +14,16 @@ class ServedModelWrapper:
     def forecast(self, X):
         # request formatting according to mlflow docs
         # https://mlflow.org/docs/latest/cli.html#mlflow-models-serve
+        # JSON safe serialization takes care of datetime columns
         response = requests.post(
             url=f"http://localhost:{self.port}/invocations",
             data=json.dumps(
                 {"dataframe_split": X.to_dict(orient='split')},
                 default=serialize_json_safe))
         if response.status_code < 300:
-            return response.content
+            # json.loads decodes byte string response.
+            # Response is a dictionary with a single entry 'predictions' 
+            return json.loads(response.content)['predictions']
         else:
             raise Exception(
                 "Could not retrieve predictions. "
