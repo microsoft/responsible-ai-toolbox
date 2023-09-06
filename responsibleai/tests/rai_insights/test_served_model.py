ad91447e2d20eed5ed5f7a6db4eac1f95e523af8
Roman Lutz <romanlutz13@gmail.com>
2023-09-06 11:43:30 -0400

    add a failing test

--- responsibleai/tests/rai_insights/test_served_model.py
+++ responsibleai/tests/rai_insights/test_served_model.py
@@ -3,15 +3,20 @@
 
 import json
 import os
+import pytest
 import random
 from responsibleai import RAIInsights, FeatureMetadata
 from tests.common_utils import (
     create_tiny_forecasting_dataset, RandomForecastingModel)
-from unittest.mock import MagicMock, patch, Mock
+from unittest.mock import patch, Mock
 
 
-@patch("requests.post")
-def test_served_model(mock_post):
+RAI_INSIGHTS_DIR_NAME = "rai_insights_test_served_model"
+
+
+# create a pytest fixture
+@pytest.fixture(scope="session")
+def rai_forecasting_insights_for_served_model():
     X_train, X_test, y_train, y_test = create_tiny_forecasting_dataset()
     train = X_train.copy()
     train["target"] = y_train
@@ -20,7 +25,6 @@ def test_served_model(mock_post):
     model = RandomForecastingModel()
 
     # create RAI Insights and save it
-    rai_insights_dir_name = "rai_insights_test_served_model"
     rai_insights = RAIInsights(
         model=model,
         train=train,
@@ -32,7 +36,12 @@ def test_served_model(mock_post):
             time_series_id_features=['id']
         ),
         forecasting_enabled=True)
-    rai_insights.save(rai_insights_dir_name)
+    rai_insights.save(RAI_INSIGHTS_DIR_NAME)
+
+
+@patch("requests.post")
+def test_served_model(mock_post, rai_forecasting_insights_for_served_model):
+    X_train, X_test, _, _ = create_tiny_forecasting_dataset()
 
     mock_post.return_value = Mock(
             status_code=200,
@@ -43,7 +52,28 @@ def test_served_model(mock_post):
 
     # set port number for served model before loading RAI Insights
     os.environ["RAI_MODEL_SERVING_PORT"] = "5123"
-    rai_insights = RAIInsights.load(rai_insights_dir_name)
+    rai_insights = RAIInsights.load(RAI_INSIGHTS_DIR_NAME)
     forecasts = rai_insights.model.forecast(X_test)
     assert len(forecasts) == len(X_test)
     assert mock_post.call_count == 1
+
+
+@patch("requests.post")
+def test_served_model_failed(mock_post, rai_forecasting_insights_for_served_model):
+    X_train, X_test, _, _ = create_tiny_forecasting_dataset()
+
+    response_content = "Could not connect to host since it actively refuses the connection."
+    mock_post.return_value = Mock(
+            status_code=400,
+            content=response_content
+        )
+
+    # set port number for served model before loading RAI Insights
+    os.environ["RAI_MODEL_SERVING_PORT"] = "5123"
+    rai_insights = RAIInsights.load(RAI_INSIGHTS_DIR_NAME)
+    with pytest.raises(Exception) as exc:
+        forecasts = rai_insights.model.forecast(X_test)
+        assert ("Could not retrieve predictions. "
+               "Model server returned status code 400 "
+               f"and the following response: {response_content}"
+               in exc.value.args[0])
