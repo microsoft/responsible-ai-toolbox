66b2d3831c858b5b05cdc310ecf4c372615546a3
Ilya Matiach <ilmat@microsoft.com>
2023-09-27 10:35:20 -0400

    fix choose cohorts close or cancel updating feature cohorts selection in flyout

---
+++ libs/model-assessment/src/lib/ModelAssessmentDashboard/Controls/ModelOverview/ChartConfigurationFlyoutState.tsx
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+export interface IChartConfigurationFlyoutState {
+  newlySelectedDatasetCohorts: number[];
+  newlySelectedFeatureBasedCohorts: number[];
+  datasetCohortViewIsNewlySelected: boolean;
+}
